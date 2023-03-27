0fce32d2e7a13d501ca3075f62d43866af940c52
w4ffl35 <w4ffl35@ml1.net>
2023-03-26 19:57:43 -0600

    fixes build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airunner',
-    version='1.8.9',
+    version='1.8.10',
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description="",
