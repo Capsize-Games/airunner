68aeb02da93d6748198c59ad12f0ba49d4a1f2e3
Joe Curlee <25737761+w4ffl35@users.noreply.github.com>
2023-03-31 15:14:53 -0600

    Removes some unused code

--- src/airunner/main.py
+++ src/airunner/main.py
@@ -20,29 +20,6 @@ from filters import FilterGaussianBlur, FilterBoxBlur, FilterUnsharpMask, Filter
     FilterColorBalance, FilterPixelArt
 import qdarktheme
 
-history_event_types = {
-    "draw": "Draw",
-    "erase": "Erase",
-    "clear": "Clear",
-    "load": "Load",
-}
-
-
-class ColorBalanceFilter(Filter):
-    name = "Color Balance"
-
-    def __init__(self, cyan_red=0, magenta_green=0, yellow_blue=0):
-        self.cyan_red = cyan_red
-        self.magenta_green = magenta_green
-        self.yellow_blue = yellow_blue
-
-    def filter(self, image):
-        # Apply enhancement
-        image = ImageEnhance.Color(image).enhance(1.0 + self.cyan_red)
-        image = ImageEnhance.Color(image).enhance(1.0 + self.magenta_green)
-        image = ImageEnhance.Color(image).enhance(1.0 + self.yellow_blue)
-        return image
-
 
 class History:
     event_history = []
