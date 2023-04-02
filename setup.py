b9c32adad7401cdefc9953c0a5afba2373bea984
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-04-02 20:02:34 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airunner',
-    version='1.8.21',
+    version='1.8.20',
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
