d9c5b8c9a87f5e8a3786e38bc10f83098f28f0bb
w4ffl35 <w4ffl35@ml1.net>
2023-04-08 16:31:16 -0600

    stop using the api which is hanging the build

--- build.py
+++ build.py
@@ -13,19 +13,6 @@ def install_latest(repo):
     os.system(f'python3 -m pip install {tar_url}')
 
 
-def get_latest_version_tag(repo):
-    url = f'https://api.github.com/repos/{repo}/releases/latest'
-    with urllib.request.urlopen(url) as response:
-        data = response.read().decode('utf-8')
-    data = json.loads(data)
-    tag_name = data["tag_name"]
-    # strip the v
-    if tag_name.startswith("v"):
-        tag_name = tag_name[1:]
-    return tag_name
-
-
-
 def clone(repo):
     # clone repo into /app
     os.system(f'git clone https://github.com/{repo}.git /app/{repo.split("/")[1]}')
@@ -41,6 +28,5 @@ install_latest("Capsize-Games/aihandler")
 os.chdir("/app/airunner")
 os.system("git checkout master")
 os.system("git pull")
-version = get_latest_version_tag("Capsize-Games/airunner")
 os.system("python3 -m pip uninstall nvidia-cublas-cu11 -y")
 os.system("python3 -m pip install bitsandbytes-cuda102 -y")
