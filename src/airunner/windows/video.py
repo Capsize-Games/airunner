c80a1e97165a441a3adc7ceb7a50002cfdf1c1f3
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-04-14 08:54:44 -0600

    adds support for txt2vid

---
+++ src/airunner/windows/video.py
@@ -0,0 +1,34 @@
+from PyQt6.QtWidgets import QVBoxLayout
+
+from airunner.windows.base_window import BaseWindow
+from PyQt6.QtMultimedia import QMediaPlayer
+from PyQt6.QtMultimediaWidgets import QVideoWidget
+from PyQt6.QtCore import QUrl
+
+
+class VideoPopup(BaseWindow):
+    template_name = "video"
+    window_title = "Video preview"
+
+    def __init__(self, settings_manager, file_path):
+        self.file_path = file_path
+        super().__init__(settings_manager)
+
+    def initialize_window(self):
+        # Create a video widget
+        self.video_widget = QVideoWidget()
+        self.video_widget.setGeometry(0, 0, 800, 600)
+
+        # Create a media player
+        self.media_player = QMediaPlayer()
+
+        # Set the video output
+        self.media_player.setVideoOutput(self.video_widget)
+
+        # Set the layout for the dialog
+        self.template.layout().addWidget(self.video_widget)
+
+        video_url = QUrl.fromLocalFile(self.file_path)
+        self.media_player.setSource(QUrl(video_url))
+        self.media_player.setLoops(QMediaPlayer.Loops.Infinite)
+        self.media_player.play()
