84a6714e7010d9f2cf28fc634ad164fcba642127
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-04-17 14:27:50 -0600

    fixes for windows build

--- src/airunner/windows/about.py
+++ src/airunner/windows/about.py
@@ -1,13 +1,7 @@
 from airunner.windows.base_window import BaseWindow
 # open the version file from the root of the project and get the VERSION variable string from it
-with open("../../version.py", "r") as f:
-    VERSION = f.read().split("=")[1].strip().replace('"', "")
-
 
 
 class AboutWindow(BaseWindow):
     template_name = "about"
     window_title = "About AI Runner"
-
-    def initialize_window(self):
-        self.template.title.setText(f"AI Runner {VERSION}")
