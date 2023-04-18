4f12a1fb5d9b21c39764045779ee57d00750b6fb
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-04-18 08:54:10 -0600

    fixes for windows deploy

---
+++ butler.windows.py
@@ -0,0 +1,6 @@
+import sys
+import os
+sys.path.append('Z:\\app\\airunner')
+version = open('Z:\\app\\VERSION').read().strip()
+print(f"Deploying {version} to itch.io")
+os.system(f"C:\\Python310\\Scripts\\butler.exe push Z:\\app\\dist\\airunner capsizegames/ai-runner:windows --userversion {version}")
