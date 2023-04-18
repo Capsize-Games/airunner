3c797904581ad336266f79c43f7b41f350e21726
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-04-18 07:10:20 -0600

    split data classes into separate files

---
+++ src/airunner/models/imagedata.py
@@ -0,0 +1,8 @@
+from PIL import Image
+from PyQt6.QtCore import QPoint
+
+
+class ImageData:
+    def __init__(self, position: QPoint, image: Image):
+        self.position = position
+        self.image = image
