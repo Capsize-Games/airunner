e46ba93f28c622de48a2ca646707b832ff35cd9c
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-04-28 17:57:02 -0600

    add a checkbox to treat multiple layers as single layer

--- src/airunner/mixins/embedding_mixin.py
+++ src/airunner/mixins/embedding_mixin.py
@@ -15,22 +15,17 @@ class EmbeddingMixin:
         return self._embedding_names
 
     def load_embeddings(self, tab):
-        # create a widget that can be added to scroll area
         container = QWidget()
         container.setLayout(QVBoxLayout())
-        print(self.embedding_names)
         for embedding_name in self.embedding_names:
             label = QLabel(embedding_name)
-            # add label to the contianer
             container.layout().addWidget(label)
-            # on double click of label insert it into the prompt
             label.mouseDoubleClickEvent = lambda event, _label=label: self.insert_into_prompt(_label.text())
         tab.embeddings.setWidget(container)
 
     def get_list_of_available_embedding_names(self):
         embeddings_folder = os.path.join(self.settings_manager.settings.model_base_path.get(), "embeddings")
         tokens = []
-        print("embeddings_folder", embeddings_folder)
         if os.path.exists(embeddings_folder):
             for f in os.listdir(embeddings_folder):
                 loaded_learned_embeds = torch.load(os.path.join(embeddings_folder, f), map_location="cpu")
@@ -41,6 +36,5 @@ class EmbeddingMixin:
         return tokens
 
     def insert_into_prompt(self, text):
-        # insert text into current tab prompt
         tab = self.window.tabWidget.currentWidget()
         tab.prompt.insertPlainText(text)
