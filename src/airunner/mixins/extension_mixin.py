e62dff2178ec1d278087d324b0a98564892901e4
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-04-29 13:53:46 -0600

    adds more injection methods for extensions

--- src/airunner/mixins/extension_mixin.py
+++ src/airunner/mixins/extension_mixin.py
@@ -9,6 +9,12 @@ class ExtensionMixin:
     """
     active_extensions = []
 
+    def initialize(self):
+        for tab_name in self.tabs.keys():
+            self.do_generator_tab_injection(self.tabs[tab_name], tab_name)
+        self.do_menubar_injection()
+        self.do_toolbar_injection()
+
     def get_extensions_from_path(self):
         """
         Initialize extensions by loading them from the extensions_directory.
@@ -40,3 +46,21 @@ class ExtensionMixin:
                             extension_class = getattr(module, "Extension")
                             extensions.append(extension_class(self.settings_manager))
         self.settings_manager.settings.active_extensions.set(extensions)
+
+    def do_generator_tab_injection(self, tab, tab_name):
+        """
+        Ibjects extensions into the generator tab widget.
+        :param tab_name:
+        :param tab:
+        :return:
+        """
+        for extension in self.settings_manager.settings.active_extensions.get():
+            extension.generator_tab_injection(tab, tab_name)
+
+    def do_menubar_injection(self):
+        for extension in self.settings_manager.settings.active_extensions.get():
+            extension.menubar_injection(self.window.menubar)
+
+    def do_toolbar_injection(self):
+        for extension in self.settings_manager.settings.active_extensions.get():
+            extension.toolbar_injection(self.window.horizontalFrame)
