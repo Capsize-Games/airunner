cf0cb43c33e2bea76044dea9a430ca805fdb8f75
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-13 08:50:16 -0600

    move filters into separate files in filters folder

---
+++ src/airunner/filters/filter_gaussian_blur.py
@@ -0,0 +1,12 @@
+from PIL.ImageFilter import GaussianBlur
+from airunner.filters.blur_filter import BlurFilter
+
+
+class FilterGaussianBlur(BlurFilter):
+    ui_name = "gaussian_blur_window"
+    window_title = "Gaussian Blur"
+    default_value = 50.0
+
+    @property
+    def filter(self):
+        return GaussianBlur(radius=self.blur_radius.get())
