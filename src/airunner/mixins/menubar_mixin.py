866061c387912aaeee119f932e55ea22319af214
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-16 12:02:54 -0600

    fixes setting reset

--- src/airunner/mixins/menubar_mixin.py
+++ src/airunner/mixins/menubar_mixin.py
@@ -20,6 +20,7 @@ class MenubarMixin:
         self.window.actionCopy.triggered.connect(self.copy_image)
         self.window.actionResize_on_Paste.triggered.connect(self.toggle_resize_on_paste)
         self.window.actionImage_to_new_layer.triggered.connect(self.toggle_image_to_new_layer)
+        self.window.actionReset_Settings.triggered.connect(self.reset_settings)
         self.initialize_filter_actions()
         self.window.actionResize_on_Paste.setChecked(self.settings_manager.settings.resize_on_paste.get() == True)
         self.window.actionImage_to_new_layer.setChecked(self.settings_manager.settings.image_to_new_layer.get() == True)
