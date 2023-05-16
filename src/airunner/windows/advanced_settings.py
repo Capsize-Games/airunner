b9eda76c87c7622524ee6b967b27de69b0d6783a
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-16 09:22:01 -0600

    removes cudnn_benchmark and torch compile from settings

--- src/airunner/windows/advanced_settings.py
+++ src/airunner/windows/advanced_settings.py
@@ -4,7 +4,7 @@ import platform
 
 class AdvancedSettings(BaseWindow):
     template_name = "advanced_settings"
-    window_title = "Advanced"
+    window_title = "Memory Settings"
 
     def initialize_window(self):
         settings = self.settings_manager.settings
@@ -13,11 +13,9 @@ class AdvancedSettings(BaseWindow):
             (self.template.use_enable_sequential_cpu_offload, settings.use_enable_sequential_cpu_offload),
             (self.template.use_attention_slicing, settings.use_attention_slicing),
             (self.template.use_tf32, settings.use_tf32),
-            (self.template.use_cudnn_benchmark, settings.use_cudnn_benchmark),
             (self.template.use_enable_vae_slicing, settings.use_enable_vae_slicing),
             (self.template.use_xformers, settings.use_xformers),
             (self.template.use_accelerated_transformers, settings.use_accelerated_transformers),
-            (self.template.use_torch_compile, settings.use_torch_compile),
             (self.template.use_tiled_vae, settings.use_tiled_vae),
             (self.template.enable_model_cpu_offload, settings.enable_model_cpu_offload),
         ]
@@ -25,8 +23,7 @@ class AdvancedSettings(BaseWindow):
             checkbox.setChecked(setting.get() == True)
             checkbox.stateChanged.connect(lambda val, setting=setting: setting.set(val == 2))
 
-        # on windows disable self.template.use_torch_compile
-        if platform.system() == "Windows":
-            self.template.use_torch_compile.setChecked(False)
-            self.template.use_torch_compile.setEnabled(False)
-            self.settings_manager.settings.use_torch_compile.set(False)
+        # TODO: torch compile once it is available on windows and in compiled python
+        # self.template.use_torch_compile.setChecked(False)
+        # self.template.use_torch_compile.setEnabled(False)
+        # self.settings_manager.settings.use_torch_compile.set(False)
