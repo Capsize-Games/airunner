2e8dff84e5d44ad015749334eb2685fa022ad0c4
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-17 15:25:11 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airunner',
-    version="1.12.0",
+    version="1.12.1",
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
@@ -15,6 +15,6 @@ setup(
     packages=find_packages("src"),
     python_requires=">=3.10.0",
     install_requires=[
-        "aihandler==1.12.1",
+        "aihandler==1.12.0",
     ]
 )
