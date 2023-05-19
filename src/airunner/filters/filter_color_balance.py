3499a4e88d182ddf817181dbbd9ce43eee1267ef
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-19 12:07:03 -0600

    Adds color balance filter

--- src/airunner/filters/filter_color_balance.py
+++ src/airunner/filters/filter_color_balance.py
@@ -15,9 +15,9 @@ class FilterColorBalance(FilterBase):
     @property
     def filter(self):
         return ColorBalanceFilter(
-            cyan_red=self.cyan_red.get() / 1000,
-            magenta_green=self.magenta_green.get() / 1000,
-            yellow_blue=self.yellow_blue.get() / 1000
+            cyan_red=self.cyan_red.get() / 1000.0,
+            magenta_green=self.magenta_green.get() / 1000.0,
+            yellow_blue=self.yellow_blue.get() / 1000.0
         )
 
     def show(self):
@@ -27,39 +27,41 @@ class FilterColorBalance(FilterBase):
         self.magenta_green.set(0)
         self.yellow_blue.set(0)
 
-        def update_filter():
-            self.parent.current_filter = self.filter
-            self.canvas.update()
-
         def color_balance_cyan_slider_change(val):
+            self.cyan_red.set(val)
             self.filter_window.cyan_spinbox.setValue(val / 1000.0)
             self.filter_window.cyan_spinbox.update()
-            update_filter()
+            self.preview_filter()
 
         def color_balance_magenta_slider_change(val):
+            self.magenta_green.set(val)
             self.filter_window.magenta_spinbox.setValue(val / 1000.0)
             self.filter_window.magenta_spinbox.update()
-            update_filter()
+            self.preview_filter()
 
         def color_balance_yellow_slider_change(val):
+            self.yellow_blue.set(val)
             self.filter_window.yellow_spinbox.setValue(val / 1000.0)
             self.filter_window.yellow_spinbox.update()
-            update_filter()
+            self.preview_filter()
 
         def color_balance_cyan_spinbox_change(val):
+            self.cyan_red.set(int(val * 1000.0))
             self.filter_window.cyan_slider.setValue(int(val * 1000.0))
             self.filter_window.cyan_slider.update()
-            update_filter()
+            self.preview_filter()
 
         def color_balance_magenta_spinbox_change(val):
+            self.magenta_green.set(int(val * 1000.0))
             self.filter_window.magenta_slider.setValue(int(val * 1000.0))
             self.filter_window.magenta_slider.update()
-            update_filter()
+            self.preview_filter()
 
         def color_balance_yellow_spinbox_change(val):
+            self.yellow_blue.set(int(val * 1000.0))
             self.filter_window.yellow_slider.setValue(int(val * 1000.0))
             self.filter_window.yellow_slider.update()
-            update_filter()
+            self.preview_filter()
 
         self.filter_window.cyan_slider.setValue(self.cyan_red.get())
         self.filter_window.cyan_slider.valueChanged.connect(lambda val: color_balance_cyan_slider_change(val))
@@ -79,5 +81,5 @@ class FilterColorBalance(FilterBase):
         # on ok button click, apply the filter
         self.filter_window.buttonBox.rejected.connect(self.cancel_filter)
         self.filter_window.buttonBox.accepted.connect(self.apply_filter)
-
+        self.preview_filter()
         self.filter_window.exec()
