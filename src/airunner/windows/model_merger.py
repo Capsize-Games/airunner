5ac0c2f1c2cfc9fc5563858d1beba51dbf47f2dc
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-19 07:49:26 -0600

    improved model merging
    use a tab widget to display models to merge

--- src/airunner/windows/model_merger.py
+++ src/airunner/windows/model_merger.py
@@ -12,58 +12,120 @@ class ModelMerger(BaseWindow):
     widgets = []
     total_models = 1
     models = []
+    model_type = "txt2img / img2img"
 
 
     def initialize_window(self):
-        self.models = load_default_models("outpaint")
+        model_types = ["txt2img / img2img", "inpaint / outpaint", "depth2img", "pix2pix"]
+        self.template.model_types.addItems(model_types)
+        self.template.model_types.currentIndexChanged.connect(self.change_model_type)
+
+        self.models = self.load_models()
         path = self.settings_manager.settings.outpaint_model_path.get()
         if not path or path == "":
             path = self.settings_manager.settings.model_base_path.get()
         self.models += load_models_from_path(path)
-        self.template.inpaint_models.addItems(self.models)
+        self.template.base_models.addItems(self.models)
 
         # get standard models from model_base_path
         path = self.settings_manager.settings.model_base_path.get()
         self.models = load_models_from_path(path)
         self.models.insert(0, "")
-
         # load the model_merger_model widget that will be used to add models
-        for n in range(self.total_models):
+        for n in range(len(self.widgets), self.total_models):
             self.add_model(self.models, n)
         layout = QVBoxLayout()
         self.template.models.setLayout(layout)
-        for widget in self.widgets:
-            self.template.models.layout().addWidget(widget)
         self.template.merge_button.clicked.connect(self.merge_models)
         self.template.addModel_button.clicked.connect(self.add_new_model)
+
+    def load_models(self):
+        if self.model_type == "txt2img / img2img":
+            action = "generate"
+        elif self.model_type == "inpaint / outpaint":
+            action = "outpaint"
+        elif self.model_type == "depth2img":
+            action = "depth2img"
+        elif self.model_type == "pix2pix":
+            action = "pix2pix"
+        return load_default_models(action)
+
+    def change_model_type(self, index):
+        self.model_type = self.template.model_types.currentText()
+        self.models = []
+        if self.model_type == "txt2img / img2img":
+            self.models = self.load_models()
+            path = self.settings_manager.settings.model_base_path.get()
+            self.models += load_models_from_path(path)
+        elif self.model_type == "inpaint / outpaint":
+            self.models = self.load_models()
+            path = self.settings_manager.settings.outpaint_model_path.get()
+            if not path or path == "":
+                path = self.settings_manager.settings.model_base_path.get()
+            self.models += load_models_from_path(path)
+        elif self.model_type == "depth2img":
+            self.models = self.load_models()
+            path = self.settings_manager.settings.depth2img_model_path.get()
+            if not path or path == "":
+                path = self.settings_manager.settings.model_base_path.get()
+            self.models += self.load_models()
+        elif self.model_type == "pix2pix":
+            self.models = load_default_models("pix2pix")
+            path = self.settings_manager.settings.pix2pix_model_path.get()
+            if not path or path == "":
+                path = self.settings_manager.settings.model_base_path.get()
+            self.models += self.load_models()
+        self.template.base_models.clear()
+        self.template.base_models.addItems(self.models)
     
     def add_new_model(self):
         self.total_models += 1
         self.add_model(self.models, self.total_models-1)
-        self.template.models.layout().addWidget(self.widgets[-1])
     
     def add_model(self, models, index):
         widget = uic.loadUi(os.path.join(f"pyqt/model_merger_model.ui"))
-        widget.model_label.setText(f"Model {index+1}")
         widget.models.addItems(models)
-        widget.model_weight_slider.setValue(50)
-        widget.model_weight_spinbox.setValue(0.5)
-        widget.model_weight_slider.valueChanged.connect(
-            lambda value, widget=widget: widget.model_weight_spinbox.setValue(value / 100)
+        widget.vae_weight_slider.setValue(50)
+        widget.vae_weight_spinbox.setValue(0.5)
+        widget.vae_weight_slider.valueChanged.connect(
+            lambda value, widget=widget: widget.vae_weight_spinbox.setValue(value / 100)
         )
-        widget.model_weight_spinbox.valueChanged.connect(
-            lambda value, widget=widget: widget.model_weight_slider.setValue(int(value * 100))
+        widget.vae_weight_spinbox.valueChanged.connect(
+            lambda value, widget=widget: widget.vae_weight_slider.setValue(int(value * 100))
+        )
+        widget.unet_weight_slider.setValue(50)
+        widget.unet_weight_spinbox.setValue(0.5)
+        widget.unet_weight_slider.valueChanged.connect(
+            lambda value, widget=widget: widget.unet_weight_spinbox.setValue(value / 100)
+        )
+        widget.unet_weight_spinbox.valueChanged.connect(
+            lambda value, widget=widget: widget.unet_weight_slider.setValue(int(value * 100))
+        )
+        widget.text_encoder_weight_slider.setValue(50)
+        widget.text_encoder_weight_spinbox.setValue(0.5)
+        widget.text_encoder_weight_slider.valueChanged.connect(
+            lambda value, widget=widget: widget.text_encoder_weight_spinbox.setValue(value / 100)
+        )
+        widget.text_encoder_weight_spinbox.valueChanged.connect(
+            lambda value, widget=widget: widget.text_encoder_weight_slider.setValue(int(value * 100))
         )
         widget.model_delete_button.clicked.connect(
             lambda _widget=widget: self.remove_model(widget)
         )
         self.widgets.append(widget)
+        # self.template.models is a QTabWidget
+        # add the widget as a new tab
+        self.template.models.addTab(widget, f"Model {index+1}")
     
     def remove_model(self, widget):
         if len(self.widgets) > 1:
             widget.deleteLater()
             self.widgets.remove(widget)
             self.total_models -= 1
+
+        # iterate over each tab in self.template.models and rename them
+        for n in range(self.template.models.count()):
+            self.template.models.setTabText(n, f"Model {n+1}")
 
     def merge_models(self):
         output_path = self.settings_manager.settings.outpaint_model_path.get()
@@ -76,9 +138,13 @@ class ModelMerger(BaseWindow):
         for widget in self.widgets:
             if widget.models.currentText() != "":
                 models.append(os.path.join(path, widget.models.currentText()))
-                weights.append(widget.model_weight_spinbox.value())
+                weights.append({
+                    "vae": widget.vae_weight_spinbox.value(),
+                    "unet": widget.unet_weight_spinbox.value(),
+                    "text_encoder": widget.text_encoder_weight_spinbox.value(),
+                })
 
-        model = self.template.inpaint_models.currentText()
+        model = self.template.base_models.currentText()
         section_name = "outpaint"
         if model in MODELS[section_name]:
             model_path = MODELS[section_name][model]["path"]
