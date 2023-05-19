650b0a7498983e1defb5c3d6bd1efca43d40cd0d
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-19 11:17:24 -0600

    adds saturation filter

--- src/airunner/filters/filter_saturation.py
+++ src/airunner/filters/filter_saturation.py
@@ -5,11 +5,11 @@ from airunner.filters.saturation_filter import SaturationFilter
 class FilterSaturation(FilterBase):
     ui_name = "saturation_window"
     window_title = "Saturation"
-    factor = 0
+    factor = 100.0
 
     @property
     def filter(self):
-        return SaturationFilter(factor=self.factor / 1000)
+        return SaturationFilter(factor=self.factor / 100)
 
     def show(self):
         super().show()
@@ -17,7 +17,7 @@ class FilterSaturation(FilterBase):
         self.filter_window.blur_slider.setValue(int(self.factor))
         self.filter_window.blur_slider.valueChanged.connect(
             lambda val: self.handle_blur_radius_slider_change(val))
-        self.filter_window.blur_spinbox.setValue(self.factor / 1000.0)
+        self.filter_window.blur_spinbox.setValue(self.factor / 100.0)
         self.filter_window.blur_spinbox.valueChanged.connect(
             lambda val: self.handle_blur_radius_spinbox_change(val))
 
@@ -25,16 +25,15 @@ class FilterSaturation(FilterBase):
         self.filter_window.buttonBox.rejected.connect(self.cancel_filter)
         self.filter_window.buttonBox.accepted.connect(self.apply_filter)
 
+        self.preview_filter()
         self.filter_window.exec()
 
     def handle_blur_radius_slider_change(self, val):
-        self.factor = float(val)
-        self.filter_window.blur_spinbox.setValue(val / 1000.0)
-        self.parent.current_filter = self.filter
-        self.canvas.update()
+        self.factor = val / 100.0
+        self.filter_window.blur_spinbox.setValue(self.factor)
+        self.preview_filter()
 
     def handle_blur_radius_spinbox_change(self, val):
-        self.factor = val / 100.0
-        self.filter_window.blur_slider.setValue(int(val * 1000.0))
-        self.parent.current_filter = self.filter
-        self.update_canvas()
+        self.factor = val * 100.0
+        self.filter_window.blur_slider.setValue(int(self.factor))
+        self.preview_filter()
