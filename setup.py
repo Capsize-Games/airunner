c45c401c78da51c0260167e48162b1643f5de708
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-19 18:44:42 -0600

    bumping patch version for diffusers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airunner',
-    version="1.13.0",
+    version="1.13.1",
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
