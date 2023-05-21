7e95ae23bd2f61aee9d094a1dedd7f7ec450a7af
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-21 11:43:18 -0600

    patch for lora missing in airunner settings

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airunner',
-    version="1.13.2",
+    version="1.13.4",
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
