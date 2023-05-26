60b9691bcde9fb5205151d3542eff9f5ffb829d8
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-26 10:50:18 -0600

    aihandler 1.14.5 for improved model loading and error handling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airunner',
-    version="1.14.5",
+    version="1.14.6",
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
@@ -15,6 +15,6 @@ setup(
     packages=find_packages("src"),
     python_requires=">=3.10.0",
     install_requires=[
-        "aihandler==1.14.4",
+        "aihandler==1.14.5",
     ]
 )
