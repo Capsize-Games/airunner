2690a61d1554a6afccd9bb67e6e980198b7a1ed8
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-26 09:03:12 -0600

    adds a prompt browser tool

---
+++ src/airunner/windows/prompt_browser.py
@@ -0,0 +1,35 @@
+from PyQt6 import uic
+from PyQt6.QtWidgets import QWidget, QVBoxLayout
+from airunner.windows.base_window import BaseWindow
+
+
+class PromptBrowser(BaseWindow):
+    template_name = "prompt_browser"
+    window_title = "Prompt Browser"
+
+    def initialize_window(self):
+        container = QWidget()
+        container.setLayout(QVBoxLayout())
+        self.prompts_manager = self.settings_manager
+        for prompt in self.prompts_manager.settings.prompts.get():
+            widget = uic.loadUi('pyqt/prompt_browser_prompt_widget.ui')
+            widget.prompt.setText(prompt['prompt'])
+            widget.negative_prompt.setText(prompt['negative_prompt'])
+            widget.load_button.clicked.connect(lambda val, _prompt=prompt: self.load_prompt(_prompt))
+            widget.delete_button.clicked.connect(lambda val, _prompt=prompt, _widget=widget: self.delete_prompt(_prompt, widget))
+            container.layout().addWidget(widget)
+
+            # self.template.scrollArea is a QScrollArea object
+            # we need to get the widget inside it to add our widget to it
+        self.template.scrollArea.setWidget(container)
+
+    def load_prompt(self, prompt):
+        self.app.update_prompt(prompt['prompt'])
+        self.app.update_negative_prompt(prompt['negative_prompt'])
+
+    def delete_prompt(self, prompt, widget):
+        prompts = self.prompts_manager.settings.prompts.get()
+        prompts.remove(prompt)
+        self.prompts_manager.settings.prompts.set(prompts)
+        self.prompts_manager.save_settings()
+        widget.deleteLater()
