2e8724b7e26318cb90d00cfc5a76eebae4c4c00a
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-26 15:03:14 -0600

    Fixes interrupt button

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airunner',
-    version="1.14.9",
+    version="1.14.10",
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
@@ -15,6 +15,6 @@ setup(
     packages=find_packages("src"),
     python_requires=">=3.10.0",
     install_requires=[
-        "aihandler==1.14.8",
+        "aihandler==1.14.9",
     ]
 )
