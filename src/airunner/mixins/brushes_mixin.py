8d2b8abaaa0acc8118c6f9559fc86a109ea5d568
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-06-01 18:19:59 -0600

    more brushes improvements and removes line opacity setting

--- src/airunner/mixins/brushes_mixin.py
+++ src/airunner/mixins/brushes_mixin.py
@@ -5,26 +5,12 @@ class BrushesMixin:
     def initialize(self):
         self.window.primary_color_button.clicked.connect(self.set_primary_color)
         self.window.secondary_color_button.clicked.connect(self.set_secondary_color)
-        self.window.primary_brush_opacity_slider.valueChanged.connect(self.set_primary_brush_opacity)
-        self.window.secondary_brush_opacity_slider.valueChanged.connect(self.set_secondary_brush_opacity)
-        self.window.primary_brush_opacity_slider.setValue(self.settings_manager.settings.primary_brush_opacity.get())
-        self.window.secondary_brush_opacity_slider.setValue(self.settings_manager.settings.secondary_brush_opacity.get())
         self.set_button_colors()
 
     def reset_settings(self):
-        self.window.primary_brush_opacity_slider.setValue(self.settings_manager.settings.primary_brush_opacity.get())
-        self.window.secondary_brush_opacity_slider.setValue(self.settings_manager.settings.secondary_brush_opacity.get())
         self.window.primary_color_button.setStyleSheet(f"background-color: {self.settings_manager.settings.primary_color.get()};")
         self.window.secondary_color_button.setStyleSheet(f"background-color: {self.settings_manager.settings.secondary_color.get()};")
         self.window.brush_size_slider.setValue(self.settings_manager.settings.size.get())
-
-    def set_primary_brush_opacity(self, value):
-        self.settings_manager.settings.primary_brush_opacity.set(int(value))
-        self.canvas.update()
-
-    def set_secondary_brush_opacity(self, value):
-        self.settings_manager.settings.secondary_brush_opacity.set(int(value))
-        self.canvas.update()
 
     def set_primary_color(self):
         # display a color picker
