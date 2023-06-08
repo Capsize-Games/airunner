8d67f630ee6dabc1dbb7af5c3f571d785e87cd57
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-06-08 08:07:56 -0600

    Improves brush tool

--- src/airunner/mixins/brushes_mixin.py
+++ src/airunner/mixins/brushes_mixin.py
@@ -4,12 +4,10 @@ from PyQt6.QtWidgets import QColorDialog
 class BrushesMixin:
     def initialize(self):
         self.window.primary_color_button.clicked.connect(self.set_primary_color)
-        self.window.secondary_color_button.clicked.connect(self.set_secondary_color)
         self.set_button_colors()
 
     def reset_settings(self):
         self.window.primary_color_button.setStyleSheet(f"background-color: {self.settings_manager.settings.primary_color.get()};")
-        self.window.secondary_color_button.setStyleSheet(f"background-color: {self.settings_manager.settings.secondary_color.get()};")
         self.window.brush_size_slider.setValue(self.settings_manager.settings.size.get())
 
     def set_primary_color(self):
@@ -19,17 +17,7 @@ class BrushesMixin:
             self.settings_manager.settings.primary_color.set(color.name())
             self.set_button_colors()
 
-    def set_secondary_color(self):
-        # display a color picker
-        color = QColorDialog.getColor()
-        if color.isValid():
-            self.settings_manager.settings.secondary_color.set(color.name())
-            self.set_button_colors()
-
     def set_button_colors(self):
         self.window.primary_color_button.setStyleSheet(
             f"background-color: {self.settings_manager.settings.primary_color.get()};"
-        )
-        self.window.secondary_color_button.setStyleSheet(
-            f"background-color: {self.settings_manager.settings.secondary_color.get()};"
         )
