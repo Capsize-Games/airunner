4c10e8d8453767ec410c0b8035e21a8711310865
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-06-10 10:26:27 -0600

    no longer treat ImageData as a list inside of LayerData
    simplifies usage of imagedata by assigning as single datapoint
    rather than a list of imagedata items.

--- src/airunner/models/layerdata.py
+++ src/airunner/models/layerdata.py
@@ -1,14 +1,10 @@
 import uuid
 from PyQt6.QtCore import QPoint
 
+from airunner.models.imagedata import ImageData
+
 
 class LayerData:
-    @property
-    def image(self):
-        if len(self.images) > 0:
-            return self.images[0]
-        return None
-
     def __init__(
         self,
         index: int,
@@ -23,7 +19,7 @@ class LayerData:
         self.opacity = opacity
         self.offset = offset
         self.lines = []
-        self.images = []
+        self.image_data = ImageData(QPoint(0, 0), None, 1.0)
         self.widgets = []
         self.uuid = uuid.uuid4()
 
@@ -42,7 +38,7 @@ class LayerData:
     def clear(self, index):
         self.index = index
         self.lines = []
-        self.images = []
+        self.image_data = None
         self.widgets = []
         self.visible = True
         self.opacity = 1.0
