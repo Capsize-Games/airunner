058813688fad5c9ca0e8b0ddd45268aa40ac6423
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-06-10 10:27:23 -0600

    adds image property reference and alpha channel backup for images in imagedata

--- src/airunner/models/imagedata.py
+++ src/airunner/models/imagedata.py
@@ -3,7 +3,19 @@ from PyQt6.QtCore import QPoint
 
 
 class ImageData:
-    def __init__(self, position: QPoint, image: Image):
+    @property
+    def image(self):
+        return self._image
+
+    @image.setter
+    def image(self, value):
+        self._image = value
+        if self._image and self.backup is None:
+            r, g, b, a = self._image.split()
+            self.backup = a
+
+    def __init__(self, position: QPoint, image: Image, opacity: float):
+        self.backup = None
         self.position = position
         self.image = image
         self.opacity = opacity * 255
