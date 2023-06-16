05c3ad5120e49808fb95312346f6dea5d84103f8
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-06-16 01:31:47 -0600

    upgrade accelerate

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airunner',
-    version="1.16.11",
+    version="1.16.12",
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
@@ -15,6 +15,6 @@ setup(
     packages=find_packages("src"),
     python_requires=">=3.10.0",
     install_requires=[
-        "aihandler==1.15.7",
+        "aihandler==1.15.8",
     ]
 )
