0f587bca2b5f6278387b02b0c4fc848f734ee03f
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-06-17 22:20:05 -0600

    blue filter improvements

--- src/airunner/filters/filter_box_blur.py
+++ src/airunner/filters/filter_box_blur.py
@@ -5,8 +5,8 @@ from airunner.filters.blur_filter import BlurFilter
 class FilterBoxBlur(BlurFilter):
     ui_name = "box_blur_window"
     window_title = "Box Blur"
-    default_value = 1.0
+    default_value = 0
 
     @property
     def filter(self):
-        return BoxBlur(radius=self.blur_radius.get())
+        return BoxBlur(radius=self.blur_radius.get()*20)
