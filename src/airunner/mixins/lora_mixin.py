acb86204f66ef29f4d854946c90d37bab2793f43
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-07-05 09:24:31 -0600

    2.0 updates

--- src/airunner/mixins/lora_mixin.py
+++ src/airunner/mixins/lora_mixin.py
@@ -2,17 +2,23 @@ import os
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QCheckBox
 from PyQt6 import QtCore
 
+from airunner.widgets.lora_widget import LoraWidget
+
 
 class LoraMixin:
     lora_loaded = False
-
     total_lora_by_section = {}
 
-    def toggle_all_lora(self, checked, tab):
-        for i in range(tab.lora_scroll_area.widget().layout().count()):
-            lora_widget = tab.lora_scroll_area.widget().layout().itemAt(i).widget()
-            if lora_widget:
-                lora_widget.enabledCheckbox.setChecked(checked)
+    @property
+    def loras(self):
+        available_loras = self.settings_manager.settings.available_loras.get()
+        if isinstance(available_loras, dict):
+            return []
+        return available_loras
+
+    @loras.setter
+    def loras(self, value):
+        self.settings_manager.settings.available_loras.set(value)
 
     def initialize(self):
         self.initialize_lora()
@@ -21,13 +27,8 @@ class LoraMixin:
         return tab not in ["upscale", "superresolution", "txt2vid"]
 
     def available_lora(self, action):
-        available_lora = self.settings_manager.settings.available_loras.get()
-        try:
-            loras = available_lora[action]
-        except KeyError:
-            loras = []
-        available_lora = []
-        for lora in loras:
+        available_lora = self.loras
+        for lora in available_lora:
             if lora["enabled"] and lora["scale"] > 0:
                 available_lora.append(lora)
         return available_lora
@@ -43,12 +44,7 @@ class LoraMixin:
             return
         for tab_name in self.tabs.keys():
             tab = self.tabs[tab_name]
-            if not self.tab_has_lora(tab_name):
-                tab.PromptTabsSection.removeTab(2)
-            else:
-                self.load_lora_tab(tab, tab_name)
-            if not self.tab_has_embeddings(tab_name):
-                tab.PromptTabsSection.removeTab(1)
+            self.load_lora_tab(tab, tab_name)
         self.initialize_lora_trigger_words()
 
     def available_loras(self, tab_name):
@@ -58,17 +54,15 @@ class LoraMixin:
             lora_path = os.path.join(base_path, lora_path)
         if not os.path.exists(lora_path):
             return []
-        available_lora = self.settings_manager.settings.available_loras.get()
-        if tab_name not in available_lora:
-            available_lora[tab_name] = []
+        available_lora = self.loras
         self.settings_manager.enable_save()
-        available_lora[tab_name] = self.get_list_of_available_loras(tab_name, lora_path, lora_names=available_lora[tab_name])
-        self.settings_manager.settings.available_loras.set(available_lora)
+        available_lora = self.get_list_of_available_loras(tab_name, lora_path, lora_names=available_lora)
+        self.loras = available_lora
         self.settings_manager.save_settings()
-        return available_lora[tab_name]
+        return available_lora
 
     def get_list_of_available_loras(self, tab_name, lora_path, lora_names=None):
-        self.total_lora_by_section[tab_name] = {
+        self.total_lora_by_section = {
             "total": 0,
             "enabled": 0
         }
@@ -87,19 +81,16 @@ class LoraMixin:
                 scale = 100.0
                 enabled = True
                 trigger_word = ""
-                # check if we have scale in self.settings_manager.settings.available_loras[tab_name]
-                available_lora = self.settings_manager.settings.available_loras.get()
-                if tab_name in available_lora:
-                    loras = available_lora[tab_name] or []
-                    for lora in loras:
-                        if lora["name"] == name:
-                            scale = lora["scale"]
-                            enabled = lora["enabled"]
-                            trigger_word = lora["trigger_word"] if trigger_word in lora else ""
-                            self.total_lora_by_section[tab_name]["total"] += 1
-                            if enabled:
-                                self.total_lora_by_section[tab_name]["enabled"] += 1
-                            break
+                available_lora = self.loras
+                for lora in available_lora:
+                    if lora["name"] == name:
+                        scale = lora["scale"]
+                        enabled = lora["enabled"]
+                        trigger_word = lora["trigger_word"] if trigger_word in lora else ""
+                        self.total_lora_by_section["total"] += 1
+                        if enabled:
+                            self.total_lora_by_section["enabled"] += 1
+                        break
                 new_loras.append({
                     "name": name,
                     "scale": scale,
@@ -134,12 +125,12 @@ class LoraMixin:
         container.setLayout(QVBoxLayout())
         available_loras = self.available_loras(tab_name)
         for lora in available_loras:
-            lora_widget = self.load_template("lora")
-            lora_widget.enabledCheckbox.setText(lora["name"])
+            lora_widget = LoraWidget(lora=lora)
+
             # lora_widget.label.setText(lora["name"])
-            enabled = lora["enabled"]
-            lora_widget.enabledCheckbox.setChecked(enabled)
+
             container.layout().addWidget(lora_widget)
+            # prevent lora.enabledCheckbox from overflowing
             # scale = lora["scale"]
             # lora_widget.scaleSlider.setValue(int(scale))
             # lora_widget.scaleSpinBox.setValue(scale / 100)
@@ -154,22 +145,18 @@ class LoraMixin:
         # add a vertical spacer to the end of the container
         container.layout().addStretch()
         # display tabs of tab.PromptTabsSection which is a QTabWidget
-        tab.lora_scroll_area.setWidget(container)
+        self.tool_menu_widget.lora_container_widget.lora_scroll_area.setWidget(container)
 
         # if all lora are checked set toggleAllLora
-        if tab_name in self.total_lora_by_section:
-            if self.total_lora_by_section[tab_name]["total"] == self.total_lora_by_section[tab_name]["enabled"]:
-                tab.toggleAllLora.setChecked(True)
+        # if tab_name in self.total_lora_by_section:
+        #     if self.total_lora_by_section[tab_name]["total"] == self.total_lora_by_section[tab_name]["enabled"]:
+        #         tab.toggleAllLora.setChecked(True)
 
         # set the tab name
         self.update_lora_tab_name(tab_name)
 
     def initialize_lora_trigger_words(self):
-        available_loras = self.settings_manager.settings.available_loras.get()
-        try:
-            available_loras = available_loras['txt2img']
-        except KeyError:
-            available_loras = []
+        available_loras = self.loras
 
         for lora in available_loras:
             trigger_word = lora["trigger_word"] if "trigger_word" in lora else ""
@@ -177,8 +164,8 @@ class LoraMixin:
                 tab = self.tabs[tab_name]
                 if not self.tab_has_lora(tab_name):
                     continue
-                for i in range(tab.lora_scroll_area.widget().layout().count()):
-                    lora_widget = tab.lora_scroll_area.widget().layout().itemAt(i).widget()
+                for i in range(self.tool_menu_widget.lora_container_widget.lora_scroll_area.widget().layout().count()):
+                    lora_widget = self.tool_menu_widget.lora_container_widget.lora_scroll_area.widget().layout().itemAt(i).widget()
                     if not lora_widget:
                         continue
                     if lora_widget.enabledCheckbox.text() == lora["name"]:
@@ -190,52 +177,50 @@ class LoraMixin:
                         break
 
     def handle_lora_trigger_word(self, lora, lora_widget, value):
-        available_loras = self.settings_manager.settings.available_loras.get()
-        for tab_name in self.tabs.keys():
-            if not self.tab_has_lora(tab_name):
-                continue
-            for n in range(len(available_loras[tab_name])):
-                if available_loras[tab_name][n]["name"] == lora["name"]:
-                    available_loras[tab_name][n]["trigger_word"] = value
-        self.settings_manager.settings.available_loras.set(available_loras)
+        available_loras = self.loras
+        for n in range(len(available_loras)):
+            if available_loras[n]["name"] == lora["name"]:
+                available_loras[n]["trigger_word"] = value
+        self.loras = available_loras
         self.settings_manager.save_settings()
 
     def toggle_lora(self, lora, value, tab_name):
-        available_loras = self.settings_manager.settings.available_loras.get()
-        for n in range(len(available_loras[tab_name])):
-            if available_loras[tab_name][n]["name"] == lora["name"]:
-                available_loras[tab_name][n]["enabled"] = value == 2
+        available_loras = self.loras
+        for n in range(len(available_loras)):
+            if available_loras[n]["name"] == lora["name"]:
+                available_loras[n]["enabled"] = value == 2
                 if value == 2:
-                    self.total_lora_by_section[tab_name]["enabled"] += 1
+                    self.total_lora_by_section["enabled"] += 1
                 else:
-                    self.total_lora_by_section[tab_name]["enabled"] -= 1
+                    self.total_lora_by_section["enabled"] -= 1
                 self.update_lora_tab_name(tab_name)
-        self.settings_manager.settings.available_loras.set(available_loras)
+        self.loras = available_loras
         self.settings_manager.save_settings()
 
     def update_lora_tab_name(self, tab_name):
-        if tab_name not in self.total_lora_by_section:
-            self.total_lora_by_section[tab_name] = {"total": 0, "enabled": 0}
-        self.tabs[tab_name].PromptTabsSection.setTabText(
-            2,
-            f'LoRA ({self.total_lora_by_section[tab_name]["enabled"]}/{self.total_lora_by_section[tab_name]["total"]})'
-        )
+        # if tab_name not in self.total_lora_by_section:
+        #     self.total_lora_by_section[tab_name] = {"total": 0, "enabled": 0}
+        # self.tabs[tab_name].PromptTabsSection.setTabText(
+        #     2,
+        #     f'LoRA ({self.total_lora_by_section[tab_name]["enabled"]}/{self.total_lora_by_section[tab_name]["total"]})'
+        # )
+        pass
 
     def handle_lora_slider(self, lora, lora_widget, value, tab_name):
-        available_loras = self.settings_manager.settings.available_loras.get()
+        available_loras = self.loras
         float_val = value / 100
-        for n in range(len(available_loras[tab_name])):
-            if available_loras[tab_name][n]["name"] == lora["name"]:
-                available_loras[tab_name][n]["scale"] = float_val
+        for n in range(len(available_loras)):
+            if available_loras[n]["name"] == lora["name"]:
+                available_loras[n]["scale"] = float_val
         lora_widget.scaleSpinBox.setValue(float_val)
-        self.settings_manager.settings.available_loras.set(available_loras)
+        self.loras = available_loras
         self.settings_manager.save_settings()
 
     def handle_lora_spinbox(self, lora, lora_widget, value, tab_name):
-        available_loras = self.settings_manager.settings.available_loras.get()
-        for n in range(len(available_loras[tab_name])):
-            if available_loras[tab_name][n]["name"] == lora["name"]:
-                available_loras[tab_name][n]["scale"] = value
+        available_loras = self.loras
+        for n in range(len(available_loras)):
+            if available_loras[n]["name"] == lora["name"]:
+                available_loras[n]["scale"] = value
         lora_widget.scaleSlider.setValue(int(value * 100))
-        self.settings_manager.settings.available_loras.set(available_loras)
+        self.loars = available_loras
         self.settings_manager.save_settings()
