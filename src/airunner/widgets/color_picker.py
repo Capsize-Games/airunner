d45becc0e167b8771cc8d2bf22d70f41f6af848b
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-07-09 13:23:43 -0600

    handle color picker widget customization

--- src/airunner/widgets/color_picker.py
+++ src/airunner/widgets/color_picker.py
@@ -1,5 +1,5 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QColorDialog
+from PyQt6.QtCore import Qt, QObject, QEvent
+from PyQt6.QtWidgets import QColorDialog, QGridLayout, QLineEdit
 
 
 class ColorPicker(QColorDialog):
@@ -10,3 +10,36 @@ class ColorPicker(QColorDialog):
             QColorDialog.ColorDialogOption.DontUseNativeDialog |
             QColorDialog.ColorDialogOption.NoButtons
         )
+        self.delete_elements(self)
+        try:
+            color_picker_widget = self.findChild(QObject, "color_picker")
+            # change this layout to qgridlayout
+            # remove color_picker_widget from its parent
+            color_picker_widget.setParent(None)
+            # delete all children from this widget
+            self.delete_elements(self)
+            # add grid_layout to this widget
+            while self.layout().count():
+                item = self.layout().takeAt(0)
+                if item.widget():
+                    item.widget().deleteLater()
+            self.layout().addWidget(color_picker_widget)
+        except Exception as e:
+            print(e)
+
+    def delete_elements(self, widget):
+        # iterate over all children in layout and print their label names
+        try:
+            for child in widget.children():
+                class_name = child.metaObject().className()
+                if class_name in [
+                    "QGridLayout", "QVBoxLayout",
+                    "QWellArray", "QPushButton", "QDialogButtonBox", "QLabel",
+                ]:
+                    child.deleteLater()
+                elif class_name == "QColorPicker":
+                    # set a label name
+                    child.setObjectName("color_picker")
+                self.delete_elements(child)
+        except Exception as e:
+            print(e)
