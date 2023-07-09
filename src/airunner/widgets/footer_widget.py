542de852999d0597a94c949fed5cefdd681fec33
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-07-09 13:50:26 -0600

    updates to footer styles

--- src/airunner/widgets/footer_widget.py
+++ src/airunner/widgets/footer_widget.py
@@ -7,9 +7,5 @@ class FooterWidget(BaseWidget):
     def set_stylesheet(self):
         color = "#ffffff" if \
             self.settings_manager.settings.dark_mode_enabled.get() else "#000000"
-        self.status_label.setStyleSheet(f"color: {color}; padding-top: 5px; padding-bottom: 5px; padding-left: 5px;")
-        self.canvas_position.setStyleSheet(f"color: {color}; padding-top: 5px; padding-bottom: 5px;")
-        self.widget.setStyleSheet(f"""
-            border-top: 1px solid #121212;
-            border-radius: 0px;
-        """)
+        self.status_label.setStyleSheet(f"color: {color};")
+        self.widget.setStyleSheet(self.app.css("footer_widget"))
