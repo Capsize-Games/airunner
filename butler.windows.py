4a971285d60df5b476facc8a275c61b690de5933
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-07-14 07:04:56 -0600

    switch butler channel for windows to windows-rc
    handle testing before final deploy

--- butler.windows.py
+++ butler.windows.py
@@ -3,4 +3,4 @@ import os
 sys.path.append('Z:\\app\\airunner')
 version = open('Z:\\app\\VERSION').read().strip()
 print(f"Deploying {version} to itch.io")
-os.system(f"C:\\Python310\\Scripts\\butler.exe push Z:\\app\\dist\\airunner capsizegames/ai-runner:windows --userversion {version}")
+os.system(f"C:\\Python310\\Scripts\\butler.exe push Z:\\app\\dist\\airunner capsizegames/ai-runner:windows-rc --userversion {version}")
