13677f00d275a382107cc91f9587f431d53dab79
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-08-17 03:05:29 -0600

    adds a model data class

---
+++ src/airunner/models/modeldata.py
@@ -0,0 +1,10 @@
+class ModelData:
+    name = ""
+    path = ""
+    branch = ""
+    version = ""
+    category = ""
+    enabled = False
+    pipeline_action = ""
+    pipeline_class = ""
+    url = ""
