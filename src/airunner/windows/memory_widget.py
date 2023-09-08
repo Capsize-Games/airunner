189dc616e9e046f68d4bcce8a977759e1b7024a7
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-09-08 08:45:46 -0600

    Adds a button to memory settings which allows optimization of settings

--- src/airunner/windows/memory_widget.py
+++ src/airunner/windows/memory_widget.py
@@ -19,8 +19,28 @@ class MemoryWidget(CustomWidget):
         for name, checkbox in checkbox_settings.items():
             checkbox.setChecked(self.settings_manager.settings.__getattribute__(name).get() is True)
             checkbox.stateChanged.connect(lambda val, _name=name: self.handle_state_change(val, _name))
+        self.optimize_memory_button.clicked.connect(self.optimize_memory)
 
         # TODO: torch compile once it is available on windows and in compiled python
+
+    def optimize_memory(self):
+        self.settings_manager.settings.use_last_channels.set(True)
+        self.settings_manager.settings.use_attention_slicing.set(False)
+        self.settings_manager.settings.use_tf32.set(False)
+        self.settings_manager.settings.use_enable_vae_slicing.set(True)
+        self.settings_manager.settings.use_accelerated_transformers.set(True)
+        self.settings_manager.settings.use_tiled_vae.set(True)
+        self.settings_manager.settings.enable_model_cpu_offload.set(False)
+        self.settings_manager.settings.use_enable_sequential_cpu_offload.set(False)
+
+        self.use_lastchannels.setChecked(True)
+        self.use_attention_slicing.setChecked(False)
+        self.use_tf32.setChecked(False)
+        self.use_enable_vae_slicing.setChecked(True)
+        self.use_accelerated_transformers.setChecked(True)
+        self.use_tiled_vae.setChecked(True)
+        self.enable_model_cpu_offload.setChecked(False)
+        self.use_enable_sequential_cpu_offload.setChecked(False)
 
     def handle_state_change(self, val, name):
         self.settings_manager.settings.__getattribute__(name).set(val == 2)
