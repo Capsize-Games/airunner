ebe74c8f8027bd64ab16a25be0a2e95f4cd6451d
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-09-08 07:59:48 -0600

    resize image to thumbnail updates

--- src/airunner/utils.py
+++ src/airunner/utils.py
@@ -40,13 +40,14 @@ def image_to_pixmap(image: Image, size=None):
 
     # scaale the image to the new width and height preserving the aspect ratio
     if size is not None:
-        aspect_ratio = image_width / image_height
-        if image_width > image_height:
-            image_width = size
-            image_height = int(image_width / aspect_ratio)
-        else:
-            image_height = size
-            image_width = int(image_height * aspect_ratio)
+        if image_width > 0 and image_height > 0:
+            aspect_ratio = image_width / image_height
+            if image_width > image_height:
+                image_width = size
+                image_height = int(image_width / aspect_ratio)
+            else:
+                image_height = size
+                image_width = int(image_height * aspect_ratio)
     image_copy = image.copy()
     image_copy = image_copy.resize((image_width, image_height))
     new_image = Image.new("RGB", (size, size))
