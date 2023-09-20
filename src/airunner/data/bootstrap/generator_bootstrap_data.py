242f87054c9e786a45a94302ca47f884ea100a99
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-09-20 08:42:21 -0600

    Moves prompt builder data to db. moves all bootstrap data to py files

---
+++ src/airunner/data/bootstrap/generator_bootstrap_data.py
@@ -0,0 +1,5 @@
+sections_bootstrap_data = sections = {
+    "stablediffusion": ["txt2img", "outpaint", "depth2img", "pix2pix", "upscale", "supersample", "txt2vid"],
+    "kandinsky": ["txt2img", "outpaint"],
+    "shapegif": ["txt2img"],
+}
