e30d50aefa080dcd190d9225d66585b1f8d6545d
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-09-20 08:52:05 -0600

    paths widget fixes

--- src/airunner/windows/paths_widget.py
+++ src/airunner/windows/paths_widget.py
@@ -1,33 +1,34 @@
 import os
 from functools import partial
 
-from PyQt6.QtWidgets import QFileDialog
+from PyQt6.QtWidgets import QFileDialog, QPushButton
 
 from airunner.utils import default_hf_cache_dir
 from airunner.windows.custom_widget import CustomWidget
 
 
 class PathsWidget(CustomWidget):
+    paths = [
+        "hf_cache",
+        "model_base",
+        "depth2img_model",
+        "pix2pix_model",
+        "outpaint_model",
+        "upscale_model",
+        "txt2vid_model",
+        "embeddings",
+        "lora",
+        "image",
+        "gif",
+        "video",
+    ]
     def __init__(self, **kwargs):
         super().__init__(**kwargs, filename="paths")
         self.initialize_window()
 
     def initialize_window(self):
-        elements = [
-            "hf_cache",
-            "model_base",
-            "depth2img_model",
-            "pix2pix_model",
-            "outpaint_model",
-            "upscale_model",
-            "txt2vid_model",
-            "embeddings",
-            "lora",
-            "image",
-            "gif",
-            "video",
-        ]
-        for element in elements:
+
+        for element in self.paths:
             getattr(self, f"{element}_button").clicked.connect(
                 partial(self.handle_browse_button, element))
             getattr(self, f"{element}_path").textChanged.connect(
@@ -36,6 +37,26 @@ class PathsWidget(CustomWidget):
             if element == "hf_cache" and val == "":
                 val = default_hf_cache_dir()
             getattr(self, f"{element}_path").setText(val)
+
+        self.auto_button.clicked.connect(self.auto)
+
+    def auto(self):
+        home = os.path.expanduser("~")
+        stable_diffusion_folder = os.path.join(home, "stablediffusion", "models")
+        for element in self.paths:
+            if element == "hf_cache":
+                continue
+            if element == "model_base":
+                folder = "txt2img"
+            else:
+                folder = element
+            folder_path = os.path.join(home, stable_diffusion_folder, folder)
+            setattr(
+                self.settings_manager.path_settings,
+                f"{element}_path",
+                folder_path
+            )
+            getattr(self, f"{element}_path").setText(folder_path)
 
     def handle_browse_button(self, element):
         settings_option = getattr(self.settings_manager.path_settings, f"{element}_path")
