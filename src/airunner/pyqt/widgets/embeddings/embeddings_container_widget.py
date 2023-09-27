c879884901a26e4bf120b5f80916b3d2d303776e
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-09-27 06:20:53 -0600

    modified and built all widgets and windows

---
+++ src/airunner/pyqt/widgets/embeddings/embeddings_container_widget.py
@@ -0,0 +1,5 @@
+from airunner.pyqt.widgets.base_widget import BaseWidget
+
+
+class EmbeddingsContainerWidget(BaseWidget):
+    name = "embeddings_container"
--- src/airunner/widgets/brush_widget.py
+++
@@ -1,5 +0,0 @@
-from airunner.widgets.base_widget import BaseWidget
-
-
-class BrushWidget(BaseWidget):
-    name = "brush_widget"
