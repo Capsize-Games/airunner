05e3cd6187d3315594742c37fab5cca7f502e172
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-09-29 15:33:49 -0600

    Remove obsolete filter files

--- src/airunner/filters/box_blur.py
+++
@@ -1,8 +0,0 @@
-from PIL.ImageFilter import BoxBlur
-
-from airunner.filters.base_filter import BaseFilter
-
-
-class BoxBlur(BaseFilter):
-    def apply_filter(self, image, do_reset):
-        return image.filter(BoxBlur(radius=self.radius))
