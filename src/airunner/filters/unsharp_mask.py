60c899eaa8544b1130e513e54c5af55a8320da40
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-09-29 15:33:49 -0600

    Remove obsolete filter files

--- src/airunner/filters/unsharp_mask.py
+++
@@ -1,8 +0,0 @@
-from PIL.ImageFilter import UnsharpMask
-
-from airunner.filters.base_filter import BaseFilter
-
-
-class UnsharpMask(BaseFilter):
-    def apply_filter(self, image, do_reset):
-        return image.filter(UnsharpMask(radius=self.radius))
