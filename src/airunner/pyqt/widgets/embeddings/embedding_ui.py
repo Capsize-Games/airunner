fe9901bd16da8109ace8778a944418e06ed2b892
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-09-29 15:45:17 -0600

    Rearrange some window files

--- src/airunner/pyqt/widgets/embeddings/embedding_ui.py
+++ src/airunner/pyqt/widgets/embeddings/embedding_ui.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/pyqt/widgets/embeddings/embedding.ui'
+# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/../../src/airunner/pyqt/widgets/embeddings/embedding.ui'
 #
 # Created by: PyQt6 UI code generator 6.4.2
 #
