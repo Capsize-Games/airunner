0c198984941a11e61ccc92ff87a0c1cbfd6cd872
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-09-30 19:00:28 -0600

    Adds new layer button functionality

--- src/airunner/widgets/layers/layer_container_widget.py
+++ src/airunner/widgets/layers/layer_container_widget.py
@@ -10,25 +10,26 @@ class LayerContainerWidget(BaseWidget):
         self.load_layer_widgets()
 
     def load_layer_widgets(self):
-        for layer_data in self.app.canvas.layers:
-            self.add_layer_widget(layer_data)
+        for index, layer_data in enumerate(self.app.canvas.layers):
+            self.add_layer_widget(layer_data, index)
 
-    def add_layer_widget(self, layer_data):
-        layer_widget = LayerWidget(self, layer_data=layer_data)
+    def add_layer_widget(self, layer_data, index):
+        layer_widget = LayerWidget(self, layer_data=layer_data, layer_index=index)
         self.ui.scrollAreaWidgetContents.layout().addWidget(layer_widget)
         layer_widget.show()
 
     def action_clicked_button_add_new_layer(self):
-        pass
+        layer_data, index = self.canvas.add_layer()
+        self.add_layer_widget(layer_data, index)
 
     def action_clicked_button_move_layer_up(self):
-        pass
+        self.canvas.move_layer_up(self.canvas.current_layer)
 
     def action_clicked_button_move_layer_down(self):
-        pass
+        self.canvas.move_layer_down(self.canvas.current_layer)
 
     def action_clicked_button_merge_selected_layers(self):
-        pass
+        self.canvas.merge_selected_layers()
 
     def action_clicked_button_delete_selected_layers(self):
-        pass
+        self.canvas.delete_selected_layers()
