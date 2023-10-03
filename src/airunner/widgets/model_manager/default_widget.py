a0ecab56f221d0ffad8fa24de93bd1aa6f77fdf5
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-10-03 11:52:04 -0600

    model manager now scans for models on load

--- src/airunner/widgets/model_manager/default_widget.py
+++ src/airunner/widgets/model_manager/default_widget.py
@@ -1,7 +1,42 @@
+from functools import partial
+
+from airunner.data.models import AIModel
+from airunner.utils import get_session
 from airunner.widgets.base_widget import BaseWidget
+from airunner.widgets.model_manager.model_widget import ModelWidget
 from airunner.widgets.model_manager.templates.default_ui import Ui_default_model_widget
 
 
 class DefaultModelWidget(BaseWidget):
     widget_class_ = Ui_default_model_widget
     model_widgets = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.show_items_in_scrollarea()
+
+    def show_items_in_scrollarea(self):
+        session = get_session()
+        models = session.query(AIModel).filter_by(is_default=True).all()
+        print(models)
+        for model_widget in self.model_widgets:
+            model_widget.deleteLater()
+        self.model_widgets = []
+        for index, model in enumerate(models):
+            version = model.version
+            category = model.category
+            pipeline_action = model.pipeline_action
+            pipeline_class = self.settings_manager.get_pipeline_classname(pipeline_action, version, category)
+            model_widget = ModelWidget(
+                path=model.path,
+                branch=model.branch,
+                version=version,
+                category=category,
+                pipeline_action=pipeline_action,
+                pipeline_class=pipeline_class,
+            )
+            model_widget.ui.delete_button.hide()
+            model_widget.ui.edit_button.deleteLater()
+            model_widget.ui.name.setChecked(model.enabled)
+            self.ui.scrollAreaWidgetContents.layout().addWidget(model_widget)
+            self.model_widgets.append(model_widget)
--- src/airunner/widgets/model_manager/templates/default_ui.py
+++ src/airunner/widgets/model_manager/templates/default_ui.py
@@ -12,14 +12,14 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Ui_default_model_widget(object):
     def setupUi(self, default_model_widget):
         default_model_widget.setObjectName("default_model_widget")
-        default_model_widget.resize(400, 300)
+        default_model_widget.resize(400, 299)
         self.gridLayout = QtWidgets.QGridLayout(default_model_widget)
         self.gridLayout.setObjectName("gridLayout")
         self.default_scroll_area = QtWidgets.QScrollArea(parent=default_model_widget)
         self.default_scroll_area.setWidgetResizable(True)
         self.default_scroll_area.setObjectName("default_scroll_area")
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 380, 255))
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 380, 254))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
         self.verticalLayout.setObjectName("verticalLayout")
