da459c88f4b5e9d3a45fa34cc3f59fb884274ab2
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-10-03 11:52:04 -0600

    model manager now scans for models on load

--- src/airunner/widgets/model_manager/custom_widget.py
+++ src/airunner/widgets/model_manager/custom_widget.py
@@ -10,8 +10,15 @@ from airunner.utils import get_session
 
 
 class CustomModelWidget(BaseWidget):
+    initialized = False
     widget_class_ = Ui_custom_model_widget
     model_widgets = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.show_items_in_scrollarea()
+        self.scan_for_models()
+        self.initialized = True
 
     def action_button_clicked_scan_for_models(self):
         self.scan_for_models()
@@ -88,13 +95,14 @@ class CustomModelWidget(BaseWidget):
                 category=model.category,
                 pipeline_action=model.pipeline_action,
                 enabled=model.enabled,
-                default=False
+                is_default=False
             )
             session.add(new_model)
             session.commit()
 
     def show_items_in_scrollarea(self):
-        models = self.settings_manager.models.all()
+        session = get_session()
+        models = session.query(AIModel).filter_by(is_default=False).all()
         for model_widget in self.model_widgets:
             model_widget.deleteLater()
         self.model_widgets = []
@@ -105,8 +113,7 @@ class CustomModelWidget(BaseWidget):
             pipeline_class = self.settings_manager.get_pipeline_classname(
                 pipeline_action, version, category)
 
-            model_widget = ModelWidget()
-            model_widget.set_properties(
+            model_widget = ModelWidget(
                 path=model.path,
                 branch=model.branch,
                 version=version,
@@ -116,46 +123,7 @@ class CustomModelWidget(BaseWidget):
                 # prompts=model.prompts,
             )
 
-            # model_widget = uic.loadUi("widgets/model_manager/templates/model.ui")
-            # model_widget.path.setText(model.path)
-            # model_widget.branch.setText(model.branch)
-            # model_widget.version.setText(version)
-            # model_widget.category.setText(category)
-            # model_widget.pipeline_action.setText(pipeline_action)
-            # if version == "" or category == "":
-            #     continue
-            # model_widget.pipeline_class.setText(pipeline_class)
-            # model_widget.name.setText(model.name)
-
-            key = "default" if model.default else "custom"
-
-            if key == "default":
-                model_widget.ui.delete_button.hide()
-                model_widget.ui.edit_button.deleteLater()
-                model_widget.ui.toolButton.deleteLater()
-            else:
-                model_widget.ui.edit_button.clicked.connect(
-                    partial(
-                        self.handle_edit_model,
-                        model,
-                        index
-                    )
-                )
-                model_widget.ui.delete_button.clicked.connect(
-                    partial(self.handle_delete_model, model)
-                )
-                model_widget.ui.toolButton.clicked.connect(partial(self.toggle_details, model_widget))
-
             model_widget.ui.name.setChecked(model.enabled)
-            model_widget.ui.name.stateChanged.connect(
-                partial(
-                    self.models_changed,
-                    key,
-                    model
-                )
-            )
-
-            self.hide_details(model_widget)
 
             self.ui.scrollAreaWidgetContents.layout().addWidget(
                 model_widget)
@@ -173,12 +141,8 @@ class CustomModelWidget(BaseWidget):
         self.update_generator_model_dropdown()
 
     def update_generator_model_dropdown(self):
-        self.app.refresh_available_models()
-
-    def hide_details(self, model_widget):
-        model_widget.details_are_shown = False
-        for i in range(model_widget.ui.details.layout().count()):
-            model_widget.ui.details.layout().itemAt(i).widget().hide()
+        if self.initialized:
+            self.app.refresh_available_models()
 
     def handle_edit_model(self, model, index):
         print("edit button clicked", index)
@@ -205,15 +169,3 @@ class CustomModelWidget(BaseWidget):
         self.ui.model_form.versions.clear()
         self.ui.model_form.versions.addItems(versions)
         self.ui.model_form.versions.setCurrentText(model.version)
-
-    def toggle_details(self, model_widget):
-        details_are_shown = getattr(model_widget, "details_are_shown", False)
-        if details_are_shown:
-            self.hide_details(model_widget)
-        else:
-            self.show_details(model_widget)
-
-    def show_details(self, model_widget):
-        model_widget.details_are_shown = True
-        for i in range(model_widget.details.layout().count()):
-            model_widget.details.layout().itemAt(i).widget().show()
