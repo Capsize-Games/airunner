af0051348d4dc053cb400c9a97c31c85757768c2
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-10-04 07:45:35 -0600

    Improves lora template with trigger word browser

--- src/airunner/widgets/lora/lora_widget.py
+++ src/airunner/widgets/lora/lora_widget.py
@@ -1,5 +1,6 @@
 from airunner.utils import save_session
 from airunner.widgets.base_widget import BaseWidget
+from airunner.widgets.lora.lora_trigger_word_widget import LoraTriggerWordWidget
 from airunner.widgets.lora.templates.lora_ui import Ui_lora
 
 
@@ -11,13 +12,28 @@ class LoraWidget(BaseWidget):
         super().__init__(*args, **kwargs)
         name = self.lora.name
         enabled = self.lora.enabled
-        self.ui.enabledCheckbox.setText(name)
+        self.ui.enabledCheckbox.setTitle(name)
         self.ui.enabledCheckbox.setChecked(enabled)
-        self.ui.trigger_word.setText(self.lora.trigger_word)
+        self.ui.trigger_word_edit.setText(self.lora.trigger_word)
+        self.create_trigger_word_widgets()
+
+    def action_text_changed_trigger_word(self, val):
+        self.lora.trigger_word = val
+        save_session()
+
+    def create_trigger_word_widgets(self):
+        for i in reversed(range(self.ui.enabledCheckbox.layout().count())):
+            self.ui.enabledCheckbox.layout().itemAt(i).widget().deleteLater()
+        for word in self.lora.trigger_word.split(","):
+            if word.strip() == "":
+                continue
+            widget = LoraTriggerWordWidget(trigger_word=word)
+            self.ui.enabledCheckbox.layout().addWidget(widget)
 
     def action_changed_trigger_words(self, val):
         self.lora.trigger_word = val
         save_session()
+        self.create_trigger_word_widgets()
 
     def action_toggled_lora_enabled(self, val):
         self.lora.enabled = val
