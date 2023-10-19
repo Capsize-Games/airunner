4b509a43299a1fc90d42f4c1a3c211abfe8a4ea6
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-10-19 06:13:11 -0600

    llm widget fixes

--- src/airunner/widgets/llm/message_widget.py
+++ src/airunner/widgets/llm/message_widget.py
@@ -6,7 +6,12 @@ class MessageWidget(BaseWidget):
     widget_class_ = Ui_message_widget
 
     def __init__(self, *args, **kwargs):
+        self.is_bot = kwargs.pop("is_bot")
         self.message = kwargs.pop("message")
         super().__init__(*args, **kwargs)
-        self.ui.name.setText(self.message.name)
-        self.ui.message.setPlainText(self.message.message)
+        self.ui.name.setText(f"{self.message.name}:")
+        self.ui.message.setText(self.message.message)
+        if self.is_bot:
+            self.ui.name.setStyleSheet("color: #0000ff;")
+        else:
+            self.ui.name.setStyleSheet("color: #ff0000;")
