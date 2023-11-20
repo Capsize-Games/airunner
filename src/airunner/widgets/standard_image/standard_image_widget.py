d6e7892fb7922d85babec991c6e2359bd217a310
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-11-20 12:40:07 -0700

    Various updates to standard image widget

--- src/airunner/widgets/standard_image/standard_image_widget.py
+++ src/airunner/widgets/standard_image/standard_image_widget.py
@@ -1,3 +1,6 @@
+import os
+
+
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QLabel
 from PyQt6.QtGui import QPixmap
@@ -8,9 +11,11 @@ from PyQt6.QtWidgets import QDialog
 from PyQt6.QtGui import QImage
 
 from PIL import Image
+from PIL import PngImagePlugin
 
 from airunner.widgets.base_widget import BaseWidget
 from airunner.widgets.standard_image.templates.standard_image_widget_ui import Ui_standard_image_widget
+from airunner.utils import delete_image, load_metadata_from_image, prepare_metadata
 
 
 class StandardImageWidget(BaseWidget):
@@ -19,28 +24,38 @@ class StandardImageWidget(BaseWidget):
     _label = None
     _layout = None
     image_path = None
+    image_label = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.app.image_data.connect(self.handle_image_data)
         self.app.load_image.connect(self.load_image_from_path)
+        # self.ui.controls_container.hide()
+        self.ui.batch_container.hide()
+        self.ui.delete_confirmation.hide()
     
     def handle_image_data(self, data):
-        self.load_image_from_object(data["image"])
+        self.image_path = data["path"]
+        self.image = data["image"]
+        self.load_image_from_object(self.image, self.image_path)
     
-    def load_image_from_path(self, image):
-        image = Image.open(image)
-        self.load_image_from_object(image)
+    def load_image_from_path(self, image_path):
+        print("load_image_from_path", image_path)
+        self.image_path = image_path
+        image = Image.open(image_path)
+        self.load_image_from_object(image=image, image_path=image_path)
     
-    def load_image_from_object(self, image):
+    def load_image_from_object(self, image, image_path):
         if self.app.image_editor_tab_name == "Standard":
-            self.set_pixmap(image=image)
+            self.set_pixmap(image=image, image_path=image_path)
     
     def set_pixmap(self, image_path=None, image=None):
         self.image_path = image_path
         self.image = image
+        self.meta_data = load_metadata_from_image(image)
+        print("SELF.META_DATA", self.meta_data)
         
-        size = self.ui.image_frame.width()
+        size = self.ui.image_frame.width() - 20
 
         pixmap = self._pixmap
         if not pixmap:
@@ -98,7 +113,11 @@ class StandardImageWidget(BaseWidget):
             meta_data["width"] = width
             meta_data["height"] = height
 
-            self.set_table_data(meta_data)
+            #self.set_table_data(meta_data)
+        
+        print("show container")
+        # self.ui.controls_container.show()
+        print("show container2")
     
     def handle_label_clicked(self, event):
         # create a popup window and show the full size image in it
@@ -108,8 +127,9 @@ class StandardImageWidget(BaseWidget):
         self.dialog.setLayout(layout)
 
         if self.image_path:
-            pixmap = QPixmap(self.image_path)
-        elif self.image:
+            self.image = Image.open(self.image_path)
+
+        if self.image:
             raw_data = self.image.tobytes("raw", "RGBA")
             qimage = QImage(
                 raw_data, 
@@ -121,10 +141,15 @@ class StandardImageWidget(BaseWidget):
         label = QLabel()
         label.setPixmap(pixmap)
         layout.addWidget(label)
+        
         self.dialog.show()
     
     def set_table_data(self, data):
+        if "options" in data:
+            data = data["options"]
+
         for k, v in data.items():
+            print(f"setting {k} {v}")
             self.ui.tableWidget.insertRow(self.ui.tableWidget.rowCount())
             self.ui.tableWidget.setItem(self.ui.tableWidget.rowCount()-1, 0, QTableWidgetItem(str(k)))
             self.ui.tableWidget.setItem(self.ui.tableWidget.rowCount()-1, 1, QTableWidgetItem(str(v)))
@@ -137,3 +162,73 @@ class StandardImageWidget(BaseWidget):
     def clear_table_data(self):
         self.ui.tableWidget.clearContents()
         self.ui.tableWidget.setRowCount(0)
+    
+    def image_to_canvas(self):
+        self.app.load_image.emit(self.image_path)
+
+    def delete_image(self):
+        self.ui.delete_confirmation.show()
+
+    def confirm_delete(self):
+        # clear the image from the canvas
+        self._label.setPixmap(QPixmap())
+        # delete the image from image_path
+        os.remove(self.image_path)
+
+    def cancel_delete(self):
+        delete_image(self.image_path)
+        self.ui.delete_confirmation.hide()
+
+    def similar_image_with_prompt(self):
+        """
+        Using the LLM, generate a description of the image
+        """
+        self.app.describe_image(image=self.image, callback=self.handle_prompt_generated)
+    
+    def handle_prompt_generated(self, prompt):
+        print("handle_prompt_generated", prompt)
+        meta_data = load_metadata_from_image(self.image)
+        meta_data["prompt"] = prompt[0]
+        meta_data = prepare_metadata({ "options": meta_data })
+        image = Image.open(self.image_path)
+        image.save(self.image_path, pnginfo=meta_data)
+        self.image = image
+        print("saving meta_data", meta_data)
+        self.meta_data = load_metadata_from_image(self.image)
+        self.similar_image()
+
+    def similar_image(self):
+        meta_data = self.meta_data
+        print("meta_data", meta_data)
+        if "prompt" not in meta_data or meta_data["prompt"] == "" or meta_data["prompt"] is None:
+            return self.similar_image_with_prompt()
+        if "negative_prompt" not in meta_data or meta_data["negative_prompt"] == "" or meta_data["negative_prompt"] is None:
+            meta_data["negative_prompt"] = "verybadimagenegative_v1.3, EasyNegative"
+
+        meta_data["action"] = "txt2img"
+        meta_data["width"] = self.image.width
+        meta_data["height"] = self.image.height
+        meta_data["enable_controlnet"] = True
+        meta_data["controlnet"] = "canny"
+        meta_data["controlnet_conditioning_scale"] = self.settings_manager.image_similarity
+        meta_data["image_guidance_scale"] = 100 * (1000 - self.settings_manager.image_similarity) / 1000.0
+        meta_data["strength"] = 1.0
+        meta_data["enable_input_image"] = True
+        meta_data["use_cropped_image"] = False
+
+        print("GENERATING WITH IMAGE", self.image)
+        print("META_DATA", meta_data)
+        print("*"*80)
+        self.app.generator_tab_widget.current_generator_widget.call_generate(
+            image=self.image,
+            override_data=meta_data
+        )
+    
+    def handle_similar_slider_change(self, value):
+        self.similarity = value
+
+    def similar_batch(self):
+        pass
+
+    def export_image(self):
+        pass
