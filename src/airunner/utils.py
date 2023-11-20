326e190b19daaae0f07caf47037fde2c869ac12e
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-11-20 12:37:01 -0700

    additions for image exporting and deleting

--- src/airunner/utils.py
+++ src/airunner/utils.py
@@ -1,11 +1,13 @@
+import datetime
 import os
+import threading
 import torch
 from PIL import Image
 from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtWidgets import QFileDialog, QApplication, QMainWindow
 from airunner.aihandler.logger import Logger
-
 from airunner.settings import SQLITE_DB_PATH
+from PIL import PngImagePlugin
 
 SESSION = None
 
@@ -274,26 +276,25 @@ def prepare_metadata(data):
     if not settings_manager.metadata_settings.export_metadata or \
             settings_manager.image_export_type != "png":
         return None
-    from PIL import PngImagePlugin
     metadata = PngImagePlugin.PngInfo()
-    options = data["options"]
-    action = data["action"]
+    options = data.get("options", {})
+    action = data.get("action", "txt2img")
     metadata.add_text("action", action)
-    metadata.add_text("prompt", options[f'prompt'])
-    metadata.add_text("negative_prompt", options[f'negative_prompt'])
+    metadata.add_text("prompt", options.get("prompt", ""))
+    metadata.add_text("negative_prompt", options.get("negative_prompt", ""))
     metadata.add_text("strength", str(options.get("strength", 100)))
     metadata.add_text("image_guidance_scale", str(options.get("image_guidance_scale", 100)))
-    metadata.add_text("scale", str(options[f"scale"]))
-    metadata.add_text("seed", str(options[f"seed"]))
-    metadata.add_text("latents_seed", str(options[f"latents_seed"]))
-    metadata.add_text("steps", str(options[f"steps"]))
-    metadata.add_text("ddim_eta", str(options[f"ddim_eta"]))
-    metadata.add_text("n_iter", str(options[f"n_iter"]))
-    metadata.add_text("n_samples", str(options[f"n_samples"]))
-    metadata.add_text("clip_skip", str(options[f"clip_skip"]))
-    for k, v in options[f"model_data"].items():
+    metadata.add_text("scale", str(options.get("scale", 7)))
+    metadata.add_text("seed", str(options.get("seed", 0)))
+    metadata.add_text("latents_seed", str(options.get("latents_seed", 0)))
+    metadata.add_text("steps", str(options.get("steps", 20)))
+    metadata.add_text("ddim_eta", str(options.get("ddim_eta", 0.0001)))
+    metadata.add_text("n_iter", str(options.get("n_iter", 1)))
+    metadata.add_text("n_samples", str(options.get("n_samples", 1)))
+    metadata.add_text("clip_skip", str(options.get("clip_skip", 0)))
+    for k, v in options.get("model_data", {}).items():
         metadata.add_text(f"model_data_{k}", str(v))
-    metadata.add_text("scheduler", str(options[f"scheduler"]))
+    metadata.add_text("scheduler", str(options.get("scheduler", "DPM++ 2M Karras")))
     return metadata
 
 def prepare_controlnet_metadata(data):
@@ -370,7 +371,10 @@ def auto_export_image(
 
         action = data["action"] if data and "action" in data else ""
 
-        file_path = os.path.join(path, action, filename)
+        # date is year-month-day
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+        file_path = os.path.join(path, action, date, filename)
 
         # if path doesn't exist, create it
         if not os.path.exists(os.path.dirname(file_path)):
@@ -462,3 +466,11 @@ def apply_opacity_to_image(image, target_opacity):
     a = a.point(lambda i: target_opacity if i > 0 else 0)
     image.putalpha(a)
     return image
+
+
+lock = threading.Lock()
+
+def delete_image(path):
+    with lock:
+        if os.path.exists(path):
+            os.remove(path)
