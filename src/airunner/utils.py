1754565aeb572b05b9e81d12c38adee2493a4df0
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-11-22 08:43:26 -0700

    apply opacity fix

--- src/airunner/utils.py
+++ src/airunner/utils.py
@@ -476,6 +476,7 @@ def apply_opacity_to_image(image, target_opacity):
     target_opacity = 255 * target_opacity
     if target_opacity == 0:
         target_opacity = 1
+    image = image.convert("RGBA")
     r, g, b, a = image.split()
     a = a.point(lambda i: target_opacity if i > 0 else 0)
     image.putalpha(a)
