c86d10185b426a8534ac08b743bddb423d87babe
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-11-29 17:44:06 -0700

    remove txt2gif

--- src/airunner/widgets/grid_preferences/templates/grid_preferences_ui.py
+++ src/airunner/widgets/grid_preferences/templates/grid_preferences_ui.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/../../src/airunner/widgets/grid_preferences/templates/grid_preferences.ui'
+# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/widgets/grid_preferences/templates/grid_preferences.ui'
 #
 # Created by: PyQt6 UI code generator 6.4.2
 #
