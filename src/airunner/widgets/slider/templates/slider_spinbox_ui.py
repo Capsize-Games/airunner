7c57e92585238e2eebbe640f9b650847bbbf8c16
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-12-17 12:09:17 -0700

    adds tts support and fixes various issues

--- src/airunner/widgets/slider/templates/slider_spinbox_ui.py
+++ src/airunner/widgets/slider/templates/slider_spinbox_ui.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/widgets/slider/templates/slider_spinbox.ui'
+# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/../../src/airunner/widgets/slider/templates/slider_spinbox.ui'
 #
 # Created by: PyQt6 UI code generator 6.4.2
 #
--- src/airunner/windows/update/templates/update_ui.py
+++ src/airunner/windows/update/templates/update_ui.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/windows/update/templates/update.ui'
+# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/../../src/airunner/windows/update/templates/update.ui'
 #
 # Created by: PyQt6 UI code generator 6.4.2
 #
