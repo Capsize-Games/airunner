fc9eaddb6f8312d8aca2bfcc68ac726e849fc5dd
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-01 22:05:55 -0700

    MOdifies lora buttons

--- src/airunner/widgets/lora/templates/lora_trigger_word_ui.py
+++ src/airunner/widgets/lora/templates/lora_trigger_word_ui.py
@@ -19,24 +19,37 @@ class Ui_lora_trigger_word(object):
         self.trigger_word.setObjectName("trigger_word")
         self.horizontalLayout.addWidget(self.trigger_word)
         self.button_to_prompt = QtWidgets.QPushButton(parent=lora_trigger_word)
+        self.button_to_prompt.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         icon = QtGui.QIcon.fromTheme("edit-paste")
         self.button_to_prompt.setIcon(icon)
         self.button_to_prompt.setObjectName("button_to_prompt")
         self.horizontalLayout.addWidget(self.button_to_prompt)
         self.button_to_negative_prompt = QtWidgets.QPushButton(parent=lora_trigger_word)
+        self.button_to_negative_prompt.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         icon = QtGui.QIcon.fromTheme("edit-paste")
         self.button_to_negative_prompt.setIcon(icon)
         self.button_to_negative_prompt.setObjectName("button_to_negative_prompt")
         self.horizontalLayout.addWidget(self.button_to_negative_prompt)
+        self.button_copy = QtWidgets.QPushButton(parent=lora_trigger_word)
+        self.button_copy.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        self.button_copy.setText("")
+        icon = QtGui.QIcon.fromTheme("edit-copy")
+        self.button_copy.setIcon(icon)
+        self.button_copy.setObjectName("button_copy")
+        self.horizontalLayout.addWidget(self.button_copy)
 
         self.retranslateUi(lora_trigger_word)
         self.button_to_prompt.clicked['bool'].connect(lora_trigger_word.action_click_button_to_prompt) # type: ignore
         self.button_to_negative_prompt.clicked.connect(lora_trigger_word.action_click_button_to_negative_prompt) # type: ignore
+        self.button_copy.clicked.connect(lora_trigger_word.action_click_button_copy) # type: ignore
         QtCore.QMetaObject.connectSlotsByName(lora_trigger_word)
 
     def retranslateUi(self, lora_trigger_word):
         _translate = QtCore.QCoreApplication.translate
         lora_trigger_word.setWindowTitle(_translate("lora_trigger_word", "Form"))
         self.trigger_word.setText(_translate("lora_trigger_word", "Trigger Word"))
+        self.button_to_prompt.setToolTip(_translate("lora_trigger_word", "Send trigger word to prompt"))
         self.button_to_prompt.setText(_translate("lora_trigger_word", "Prompt"))
-        self.button_to_negative_prompt.setText(_translate("lora_trigger_word", "Negative Prompt"))
+        self.button_to_negative_prompt.setToolTip(_translate("lora_trigger_word", "Send trigger word to negative prompt"))
+        self.button_to_negative_prompt.setText(_translate("lora_trigger_word", "Negative"))
+        self.button_copy.setToolTip(_translate("lora_trigger_word", "Copy trigger word to clipboard"))
