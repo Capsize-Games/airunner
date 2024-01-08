afe8bc82b14f27d2787aa4bd92cc950e3d425d9b
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-08 16:57:39 -0700

    Many changes

--- src/airunner/filters/color_balance.py
+++ src/airunner/filters/color_balance.py
@@ -5,6 +5,7 @@ from airunner.filters.base_filter import BaseFilter
 
 class ColorBalanceFilter(BaseFilter):
     def apply_filter(self, image, _do_reset):
+        image = image.convert("RGBA")
         red, green, blue, alpha = image.split()
         red = red.point(lambda i: i + (i * self.cyan_red))
         green = green.point(lambda i: i + (i * self.magenta_green))
--- src/airunner/filters/unsharp_mask.py
+++ src/airunner/filters/unsharp_mask.py
@@ -4,5 +4,8 @@ from airunner.filters.base_filter import BaseFilter
 
 
 class UnsharpMask(BaseFilter):
+    def __init__(self, radius, percent, threshold):
+        self.unsharp_mask = UnsharpMask(radius=radius, percent=percent, threshold=threshold)
+
     def apply_filter(self, image, do_reset):
-        return image.filter(UnsharpMask(radius=self.radius))
+        return image.filter(self.unsharp_mask)
