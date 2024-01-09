7d9ba0919ce19216ea8115bd2b1aab76981d003b
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-09 04:40:44 -0700

    Fixes Logger usage in LLM

--- src/airunner/tests/test_get_latest_version.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from airunner.utils import get_latest_version
-
-
-class TestVersion(unittest.TestCase):
-    def test_get_latest_version(self):
-        self.assertEqual(get_latest_version(), "1.12.1")
