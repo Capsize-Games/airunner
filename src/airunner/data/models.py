b16d2802a0e473b712fa298d0a3f3cd7862af863
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-11 11:01:38 -0700

    removes working width from db

--- src/airunner/data/models.py
+++ src/airunner/data/models.py
@@ -555,8 +555,6 @@ class Settings(BaseModel):
     
     current_tool = Column(String, default="")
 
-    working_width = Column(Integer, default=512)
-
     brush_settings_id = Column(Integer, ForeignKey('brush_settings.id'))
     brush_settings = relationship("BrushSettings", back_populates="settings")
 
