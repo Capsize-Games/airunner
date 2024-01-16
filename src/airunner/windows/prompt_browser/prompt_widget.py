393cf05a77f17d8dc77d61ff069408c2761f2498
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-16 13:32:25 -0700

    Many impovements to thread handling and logging

--- src/airunner/windows/prompt_browser/prompt_widget.py
+++ src/airunner/windows/prompt_browser/prompt_widget.py
@@ -7,10 +7,11 @@ class PromptWidget(BaseWidget):
     widget_class_ = Ui_prompt_widget
 
     def __init__(self, *args, **kwargs):
+        self.index = kwargs.pop("index")
         self.prompt_data = kwargs.pop("prompt_data")
         super().__init__(*args, **kwargs)
-        self.ui.prompt.setPlainText(self.prompt_data.prompt)
-        self.ui.negative_prompt.setPlainText(self.prompt_data.negative_prompt)
+        self.ui.prompt.setPlainText(self.prompt_data["prompt"])
+        self.ui.negative_prompt.setPlainText(self.prompt_data["negative_prompt"])
 
     def action_text_changed_prompt(self):
         self.save_prompt()
@@ -19,7 +20,7 @@ class PromptWidget(BaseWidget):
         self.save_prompt()
 
     def action_clicked_button_load(self):
-        self.app.load_prompt(self.prompt_data)
+        self.app.load_saved_stablediffuion_prompt(self.index)
 
     def action_clicked_button_delete(self):
         with session_scope() as session:
@@ -27,7 +28,8 @@ class PromptWidget(BaseWidget):
         self.deleteLater()
 
     def save_prompt(self):
-        with session_scope() as session:
-            session.add(self.prompt_data)
-            self.prompt_data.prompt = self.ui.prompt.toPlainText()
-            self.prompt_data.negative_prompt = self.ui.negative_prompt.toPlainText()
+        self.app.update_saved_stablediffusion_prompt(
+            self.index,
+            self.ui.prompt.toPlainText(), 
+            self.ui.negative_prompt.toPlainText()
+        )
