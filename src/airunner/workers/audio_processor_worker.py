0d7fc4eb9c1ec445476db3039ceb51262f8bc40a
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-24 06:15:04 -0700

    Moves STT audio workers to separate files and removes STT controller

---
+++ src/airunner/workers/audio_processor_worker.py
@@ -0,0 +1,25 @@
+from airunner.aihandler.speech_to_text import SpeechToText
+from airunner.workers.worker import Worker
+
+
+class AudioProcessorWorker(Worker):
+    """
+    This class is responsible for processing audio.
+    It will process audio from the audio_queue and send it to the model.
+    """ 
+    fs = 0
+
+    def __init__(self, prefix):
+        super().__init__(prefix=prefix)
+        self.stt = SpeechToText()
+        self.register("stt_audio_processed", self)
+    
+    def on_stt_audio_processed(self, transcription):
+        self.emit("response_signal", transcription)
+
+    def handle_message(self, audio_data):
+        self.emit("processed_audio", audio_data)
+    
+    def update_properties(self):
+        settings = self.application_settings.value("settings")
+        self.fs = settings["stt_settings"]["fs"]
