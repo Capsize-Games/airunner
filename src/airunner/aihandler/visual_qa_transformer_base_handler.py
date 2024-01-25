6f4eed57b92f7bec21d7df84a44eae30107eab48
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-25 06:07:46 -0700

    Move base handler to separate files

---
+++ src/airunner/aihandler/visual_qa_transformer_base_handler.py
@@ -0,0 +1,85 @@
+import torch
+from transformers import BlipForConditionalGeneration, BlipProcessor
+from airunner.aihandler.transformer_base_handler import TransformerBaseHandler
+
+
+class VisualQATransformerBaseHandler(TransformerBaseHandler):
+    """
+    Visual QA Transformer Base Handler.
+    Uses a processor and model to generate information about a given image.
+    """
+    auto_class_ = BlipForConditionalGeneration
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.prompt = "This is an image of"
+        self.model_path = "Salesforce/blip-image-captioning-large"
+        self.do_sample = False
+        self.num_beams = 5
+        self.max_length = 256
+        self.min_length = 1
+        self.top_p = 0.9
+        self.repetition_penalty = 1.5
+        self.length_penalty = 1.0
+        self.temperature = 1
+
+    def load_processor(self, local_files_only=None):
+        self.logger.info(f"Loading processor {self.model_path}")
+        kwargs = dict(
+            device_map="auto",
+            torch_dtype=torch.float16,
+            local_files_only=self.local_files_only if local_files_only is None else local_files_only,
+        )
+        config = self.quantization_config()
+        if config:
+            kwargs["quantization_config"] = config
+        self.processor = BlipProcessor.from_pretrained(
+            self.model_path,
+            **kwargs
+        )
+        if self.processor:
+            self.logger.info("Processor loaded")
+        else:
+            self.logger.error("Failed to load processor")
+
+    def do_generate(self, prompt, chat_template):
+        image = self.image.convert("RGB")
+        inputs = self.processor(
+            images=image,
+            text=prompt,
+            return_tensors="pt"
+        ).to("cuda")
+
+        try:
+            out = self.model.generate(
+                **inputs,
+                do_sample=self.do_sample,
+                num_beams=self.num_beams,
+                max_length=self.max_length,
+                min_length=self.min_length,
+                top_p=self.top_p,
+                repetition_penalty=self.repetition_penalty,
+                length_penalty=self.length_penalty,
+                temperature=self.temperature,
+            )
+        except AttributeError as e:
+            return ""
+
+        try:
+            generated_text = self.processor.batch_decode(
+                out, skip_special_tokens=True
+            )[0].strip()
+            return generated_text
+        except AttributeError as e:
+            return ""
+
+    def prepare_input_args(self):
+        kwargs = super().prepare_input_args()
+        for key in ["return_result", "skip_special_tokens", "seed"]:
+            kwargs.pop(key)
+        return kwargs
+
+    def model_params(self, local_files_only) -> dict:
+        params = super().model_params(local_files_only)
+        del params["use_cache"]
+        return params
