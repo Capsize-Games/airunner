c9f9d9d2c70c26042b23939dc0a4ab74dd19a02f
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-25 06:07:46 -0700

    Move base handler to separate files

---
+++ src/airunner/aihandler/seq2seq_transformer_base_handler.py
@@ -0,0 +1,10 @@
+from transformers import AutoModelForSeq2SeqLM
+
+from airunner.aihandler.transformer_base_handler import TransformerBaseHandler
+
+
+class Seq2SeqTransformerBaseHandler(TransformerBaseHandler):
+    def do_generate(self, prompt, chat_template):
+        pass
+
+    auto_class_ = AutoModelForSeq2SeqLM
