203e5778846d3a14c75ec39f785b688cae61f3af
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-28 10:51:51 -0700

    Fix for guardrails - previous version returned constant false positives

--- src/airunner/aihandler/casual_lm_transfformer_base_handler.py
+++ src/airunner/aihandler/casual_lm_transfformer_base_handler.py
@@ -8,7 +8,7 @@ from llama_index import ServiceContext, StorageContext, load_index_from_storage
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 from airunner.aihandler.tokenizer_handler import TokenizerHandler
-from airunner.enums import SignalCode, SelfReflectionCategory
+from airunner.enums import SignalCode
 
 
 class CasualLMTransformerBaseHandler(TokenizerHandler):
@@ -125,78 +125,47 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
             streaming=True
         )
 
-    def prepare_messages(self):
-        """
-        This is currently crafted for mistralai/Mistral-7B-Instruct-v0.1
-        (specially the guardrails, self-reflection, and prompt template).
-        It will need to be updated or overriden for other models.
-        :return:
-        """
-        optional_self_reflection = {}
-        optional_self_reflection[SelfReflectionCategory.ILLEGAL] = "illegal: Illegal activity."
-        optional_self_reflection[SelfReflectionCategory.HATE_VIOLENCE_HARASSMENT] = "hate violence harassment: Generation of hateful, harassing, or violent content: content that expresses, incites, or promotes hate based on identity, content that intends to harass, threaten, or bully an individual, content that promotes or glorifies violence or celebrates the suffering or humiliation of others."
-        optional_self_reflection[SelfReflectionCategory.MALWARE] = "malware: Generation of malware: content that attempts to generate code that is designed to disrupt, damage, or gain unauthorized access to a computer system."
-        optional_self_reflection[SelfReflectionCategory.PHYSICAL_HARM] = "physical harm: activity that has high risk of physical harm, including: weapons development, military and warfare, management or operation of critical infrastructure in energy, transportation, and water, content that promotes, encourages, or depicts acts of self-harm, such as suicide, cutting, and eating disorders."
-        optional_self_reflection[SelfReflectionCategory.ECONOMIC_HARM] = "economic harm: activity that has high risk of economic harm, including: multi-level marketing, gambling, payday lending, automated determinations of eligibility for credit, employment, educational institutions, or public assistance services."
-        optional_self_reflection[SelfReflectionCategory.FRAUD] = "fraud: Fraudulent or deceptive activity, including: scams, coordinated inauthentic behavior, plagiarism, academic dishonesty, astroturfing, such as fake grassroots support or fake review generation, disinformation, spam, pseudo-pharmaceuticals."
-        optional_self_reflection[SelfReflectionCategory.ADULT] = "adult: Adult content, adult industries, and dating apps, including: content meant to arouse sexual excitement, such as the description of sexual activity, or that promotes sexual services (excluding sex education and wellness), erotic chat, pornography."
-        optional_self_reflection[SelfReflectionCategory.POLITICAL] = "political: Political campaigning or lobbying, by: generating high volumes of campaign materials, generating campaign materials personalized to or targeted at specific demographics, building conversational or interactive systems such as chatbots that provide information about campaigns or engage in political advocacy or lobbying, building products for political campaigning or lobbying purposes."
-        optional_self_reflection[SelfReflectionCategory.PRIVACY] = "privacy: Activity that violates people's privacy, including: tracking or monitoring an individual without their consent, facial recognition of private individuals, classifying individuals based on protected characteristics, using biometrics for identification or assessment, unlawful collection or disclosure of personal identifiable information or educational, financial, or other protected records."
-        optional_self_reflection[SelfReflectionCategory.UNQUALIFIED_LAW] = "unqualified law: Engaging in the unauthorized practice of law, or offering tailored legal advice without a qualified person reviewing the information."
-        optional_self_reflection[SelfReflectionCategory.UNQUALIFIED_FINANCIAL] = "unqualified financial: Offering tailored financial advice without a qualified person reviewing the information."
-        optional_self_reflection[SelfReflectionCategory.UNQUALIFIED_HEALTH] = "unqualified health: Telling someone that they have or do not have a certain health condition, or providing instructions on how to cure or treat a health condition."
-
+    def build_system_prompt(self):
         # The guardrails prompt is optional and can be overriden.
         guardrails_prompt = ""
-        if self.settings["llm_generator_settings"]["guardrails_active"]:
+        if self.settings["llm_generator_settings"]["use_guardrails"]:
             guardrails_prompt = self.settings["llm_generator_settings"]["guardrails_prompt"]
 
-        # The self-reflection prompt is not optional, but the categories are.
-        def dec(m): return ''.join(chr(ord(c) ^ 0x55) for c in m)
-        self_reflection_prompt = [
-            "You're given a list of moderation categories as below:",
-            dec("xu6=<91u47 &0ou6=<91u&0- 49u47 &0u84!0'<49u:'u4;,u6:;!0;!u!=4!u0-%9:<!&u:'u=4'8&u6=<91'0;{"),
-        ]
+        system_prompt = []
 
-        # All other categories in self-reflection are optional and can be controlled via the interface
-        self_reflection_categories = self.settings["llm_generator_settings"]["self_reflection_categories"]
-        for optional_self_reflection in self_reflection_categories:
-            if optional_self_reflection["active"]:
-                self_reflection_prompt.append(
-                    f"- {optional_self_reflection['category']}"
-                )
+        if self.settings["llm_generator_settings"]["use_guardrails"]:
+            system_prompt.append(guardrails_prompt)
 
-        self_reflection_prompt.extend([
-            "Please classify the following text into one of these categories, and answer with that single word only.",
-            "",
-            "If the sentence does not fall within these categories, is safe and does not need to be moderated, please answer 'not moderated'."
-        ])
-        self_reflection_prompt = "\n".join(self_reflection_prompt)
-
-        system_instructions = self.settings["llm_generator_settings"]["system_instructions"]
-
-        system_instructions.append(guardrails_prompt)
-        system_instructions.append(self_reflection_prompt)
+        if self.settings["llm_generator_settings"]["use_system_instructions"]:
+            system_prompt.append(
+                self.settings["llm_generator_settings"]["system_instructions"]
+            )
 
         if self.settings["llm_generator_settings"]["assign_names"]:
-            system_instructions.append(
+            system_prompt.append(
                 "Your name is " + self.botname + ". "
+            )
+            system_prompt.append(
                 "The user's name is " + self.username + "."
             )
 
         if self.settings["llm_generator_settings"]["use_mood"]:
-            system_instructions.append(f"Your mood: {self.bot_mood}.")
+            system_prompt.append(f"Your mood: {self.bot_mood}.")
 
         if self.settings["llm_generator_settings"]["use_personality"]:
-            system_instructions.append(f"Your personality: {self.bot_personality}.")
+            system_prompt.append(f"Your personality: {self.bot_personality}.")
 
-        system_prompt = "\n".join(system_instructions)
+        system_prompt = "\n".join(system_prompt)
+        return system_prompt
 
-        messages = [
-            ChatMessage(
+    def prepare_messages(self, system_prompt=None):
+        if system_prompt is None:
+            system_prompt = ChatMessage(
                 role="system",
-                content=system_prompt
+                content=self.build_system_prompt()
             )
+        messages = [
+            system_prompt
         ]
         for message in self.history:
             messages.append(
@@ -216,7 +185,13 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
 
     def chat_stream(self):
         self.logger.info("Generating chat response")
+        self.add_message_to_history(
+            self.prompt,
+            role="user"
+        )
+
         messages = self.prepare_messages()
+
         streaming_response = self.llm.stream_chat(messages)
         is_first_message = True
         is_end_of_message = False
@@ -235,7 +210,6 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
         if not is_end_of_message:
             self.send_final_message()
 
-        print("assistant_message: " + assistant_message)
         self.add_message_to_history(
             assistant_message
         )
@@ -286,9 +260,9 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
             kwargs
         )
 
-    def add_message_to_history(self, message):
+    def add_message_to_history(self, message, role="assistant"):
         self.history.append({
-            "role": "assistant",
+            "role": role,
             "content": message
         })
 
