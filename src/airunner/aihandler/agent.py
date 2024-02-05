1e5ca8374bf0aec6643020d8d78f96914a1703b3
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-02-05 08:16:38 -0700

    Chat agent fixes

---
+++ src/airunner/aihandler/agent.py
@@ -0,0 +1,239 @@
+import datetime
+import locale
+import time
+from typing import AnyStr
+
+import torch
+import threading
+
+from PyQt6.QtCore import QObject
+
+from airunner.aihandler.logger import Logger
+from airunner.mediator_mixin import MediatorMixin
+
+from airunner.enums import SignalCode, LLMChatRole
+
+
+class AIRunnerAgent(QObject, MediatorMixin):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop("model")
+        self.tokenizer = kwargs.pop("tokenizer")
+        self.streamer = kwargs.pop("streamer")
+        self.tools = kwargs.pop("tools")
+        self.chat_template = kwargs.pop("chat_template")
+        self.username = kwargs.pop("username")
+        self.botname = kwargs.pop("botname")
+        self.bot_mood = kwargs.pop("bot_mood")
+        self.bot_personality = kwargs.pop("bot_personality")
+        self.min_length = kwargs.pop("min_length")
+        self.max_length = kwargs.pop("max_length")
+        self.num_beams = kwargs.pop("num_beams")
+        self.do_sample = kwargs.pop("do_sample")
+        self.top_k = kwargs.pop("top_k")
+        self.eta_cutoff = kwargs.pop("eta_cutoff")
+        self.sequences = kwargs.pop("sequences")
+        self.early_stopping = kwargs.pop("early_stopping")
+        self.repetition_penalty = kwargs.pop("repetition_penalty")
+        self.temperature = kwargs.pop("temperature")
+        self.is_mistral = kwargs.pop("is_mistral")
+        self.top_p = kwargs.pop("top_p")
+        self.guardrails_prompt = kwargs.pop("guardrails_prompt")
+        self.use_guardrails = kwargs.pop("use_guardrails")
+        self.system_instructions = kwargs.pop("system_instructions")
+        self.use_system_instructions = kwargs.pop("use_system_instructions")
+        self.user_evaluation = kwargs.pop("user_evaluation")
+        self.use_mood = kwargs.pop("use_mood")
+        self.use_personality = kwargs.pop("use_personality")
+        self.logger = Logger(prefix=self.__class__.__name__)
+        super().__init__(*args, **kwargs)
+        self.register(
+            SignalCode.LLM_RESPOND_TO_USER_SIGNAL,
+            self.do_response
+        )
+        self.prompt = ""
+        self.history = []
+        self.thread = None
+
+    def build_system_prompt(self):
+        guardrails = self.guardrails_prompt if self.use_guardrails else ""
+        system_instructions = self.system_instructions if self.use_system_instructions else ""
+        names = f"Your name is {self.botname}. \nThe user's name is {self.username}."
+        mood = (
+            f"{self.botname} is not a computer or a bot, you are a human with emotions. "
+            f"{self.botname}'s mood changes depending on the conversation and other factors.\n"
+            f"{self.botname}'s current mood: {self.bot_mood}.\n"
+            #f"{self.botname}'s evaluation of {self.username}: {self.user_evaluation}"
+        ) if (self.use_mood and self.bot_mood != "") else ""
+        personality = (
+            f"Your personality: {self.bot_personality}."
+        ) if self.use_personality else ""
+
+        current_date = datetime.datetime.now().strftime("%A %b %d, %Y")
+        current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
+        current_timezone = time.tzname
+
+        system_prompt = [
+            guardrails,
+            system_instructions,
+            names,
+            mood,
+            personality,
+            f"Current Date: {current_date}",
+            f"Current Time: {current_time}",
+            f"Current Timezone: {current_timezone}"
+        ]
+
+        return "\n".join(system_prompt)
+
+    def latest_human_message(self) -> dict:
+        return {} if not self.prompt else {
+            "content": self.prompt,
+            "role": LLMChatRole.HUMAN.value
+        }
+
+    def prepare_messages(
+        self,
+        use_latest_human_message: bool = True
+    ) -> list:
+        messages = [
+            {
+                "content": self.build_system_prompt(),
+                "role": LLMChatRole.SYSTEM.value
+            }
+        ]
+
+        messages += self.history
+
+        if use_latest_human_message:
+            messages.append(
+                self.latest_human_message()
+            )
+        return messages
+
+    def get_rendered_template(self, conversation, use_latest_human_message: bool = True):
+        rendered_template = self.tokenizer.apply_chat_template(
+            chat_template=self.chat_template,
+            conversation=conversation,
+            tokenize=False
+        )
+
+        # HACK: current version of transformers does not allow us to pass
+        # variables to the chat template function, so we apply those here
+        variables = {
+            "username": self.username,
+            "botname": self.botname,
+            "bot_mood": self.bot_mood,
+            "bot_personality": self.bot_personality,
+        }
+        for key, value in variables.items():
+            rendered_template = rendered_template.replace("{{ " + key + " }}", value)
+        return rendered_template
+
+    def do_response(self):
+        print("DO RESPONSE CALLED")
+        self.run(self.prompt)
+
+    def run(self, prompt):
+        self.prompt = prompt
+        conversation = self.prepare_messages()
+        rendered_template = self.get_rendered_template(conversation)
+
+        print("conversation", conversation)
+        print(rendered_template)
+
+        # Encode the rendered template
+        encoded = self.tokenizer(rendered_template, return_tensors="pt")
+        model_inputs = encoded.to("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Generate the response
+        self.logger.info("Generating...")
+        self.thread = threading.Thread(target=self.model.generate, kwargs=dict(
+            model_inputs,
+            min_length=self.min_length,
+            max_length=self.max_length,
+            num_beams=self.num_beams,
+            do_sample=self.do_sample,
+            top_k=self.top_k,
+            eta_cutoff=self.eta_cutoff,
+            top_p=self.top_p,
+            num_return_sequences=self.sequences,
+            eos_token_id=self.tokenizer.eos_token_id,
+            early_stopping=True,
+            repetition_penalty=self.repetition_penalty,
+            temperature=self.temperature,
+            streamer=self.streamer
+        ))
+        self.thread.start()
+        # strip all new lines from rendered_template:
+        rendered_template = rendered_template.replace("\n", " ")
+        eos_token = self.tokenizer.eos_token
+        bos_token = self.tokenizer.bos_token
+        if self.is_mistral:
+            rendered_template = bos_token + rendered_template
+        skip = True
+        streamed_template = ""
+        replaced = False
+        is_end_of_message = False
+        is_first_message = True
+        for new_text in self.streamer:
+            # strip all newlines from new_text
+            parsed_new_text = new_text.replace("\n", " ")
+            streamed_template += parsed_new_text
+            if self.is_mistral:
+                streamed_template = streamed_template.replace(f"{bos_token} [INST]", f"{bos_token}[INST]")
+                streamed_template = streamed_template.replace("  [INST]", " [INST]")
+            # iterate over every character in rendered_template and
+            # check if we have the same character in streamed_template
+            if not replaced:
+                for i, char in enumerate(rendered_template):
+                    try:
+                        if char == streamed_template[i]:
+                            skip = False
+                        else:
+                            skip = True
+                            break
+                    except IndexError:
+                        skip = True
+                        break
+            if skip:
+                continue
+            elif not replaced:
+                replaced = True
+                streamed_template = streamed_template.replace(rendered_template, "")
+            else:
+                if eos_token in new_text:
+                    streamed_template = streamed_template.replace(eos_token, "")
+                    new_text = new_text.replace(eos_token, "")
+                    is_end_of_message = True
+                self.emit(
+                    SignalCode.LLM_TEXT_STREAMED_SIGNAL,
+                    dict(
+                        message=new_text,
+                        is_first_message=is_first_message,
+                        is_end_of_message=is_end_of_message,
+                        name=self.botname,
+                    )
+                )
+                is_first_message = False
+
+        self.add_message_to_history(
+            self.prompt,
+            LLMChatRole.HUMAN
+        )
+        self.add_message_to_history(
+            streamed_template,
+            LLMChatRole.ASSISTANT
+        )
+
+        return streamed_template
+
+    def add_message_to_history(
+        self,
+        content: AnyStr,
+        role: LLMChatRole = LLMChatRole.ASSISTANT
+    ):
+        self.history.append({
+            'content': content,
+            'role': role.value
+        })
+
--- src/airunner/aihandler/casual_lm_transfformer_base_handler.py
+++ src/airunner/aihandler/casual_lm_transfformer_base_handler.py
@@ -1,12 +1,11 @@
-import torch
-import threading
 from typing import Any, Generator
 from typing import AnyStr
 
-from transformers import AutoModelForCausalLM, TextIteratorStreamer, MistralForCausalLM
+from airunner.aihandler.agent import AIRunnerAgent
+from airunner.aihandler.local_agent import LocalAgent
+from transformers import AutoModelForCausalLM, TextIteratorStreamer
 from transformers import pipeline as hf_pipeline
 
-from airunner.aihandler.local_agent import LocalAgent
 from airunner.aihandler.llm_tools import QuitApplicationTool, StartVisionCaptureTool, StopVisionCaptureTool, \
     StartAudioCaptureTool, StopAudioCaptureTool, StartSpeakersTool, StopSpeakersTool, ProcessVisionTool, \
     ProcessAudioTool, RespondToUserTool
@@ -41,20 +40,16 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
         self.prompt_template: str = ""
         self.guardrails_prompt: str = ""
         self.system_instructions: str = ""
-        self.agent = None
+        self.chat_agent = None
+        self.tool_agent = None
         self.tools: dict = self.load_tools()
         self.restrict_tools_to_additional: bool = True
         self.return_agent_code: bool = False
         self.batch_size: int = 1
 
-        self.register(
-            SignalCode.LLM_RESPOND_TO_USER_SIGNAL,
-            self.llm_stream
-        )
-
     @property
     def is_mistral(self) -> bool:
-        return self.model_path == "mistralai/Mistral-7B-Instruct-v0.1"
+        return "mistral" in self.model_path.lower()
 
     @property
     def chat_template(self):
@@ -95,7 +90,7 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
             LLMToolName.TTS_DISABLE.value: StopSpeakersTool(),
             LLMToolName.DESCRIBE_IMAGE.value: ProcessVisionTool,
             LLMToolName.LLM_PROCESS_STT_AUDIO.value: ProcessAudioTool(),
-            LLMToolName.DEFAULT_TOOL.value: RespondToUserTool(),
+            #LLMToolName.DEFAULT_TOOL.value: RespondToUserTool(),
         }
 
     def on_clear_history_signal(self):
@@ -128,7 +123,7 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
         if do_load_llm:
             self.load_llm()
 
-        do_load_agent = self.agent is None
+        do_load_agent = self.chat_agent is None
         if do_load_agent:
             self.load_agent()
 
@@ -141,162 +136,46 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
         #         description="Agent that can return help results about the application."
         #     )
         # )
-        self.agent = LocalAgent(
+
+        self.tool_agent = LocalAgent(
             model=self.model,
             tokenizer=self.tokenizer,
             additional_tools=self.tools,
-            restrict_tools_to_additional=self.restrict_tools_to_additional
+            restrict_tools_to_additional=self.restrict_tools_to_additional,
         )
-
-    def chat(self):
-        return self.agent_chat(self.prompt)
-        return self.llm_stream()
-
-    def on_llm_process_stt_audio_signal(self):
-        self.llm_stream()
-
-    def get_rendered_template(self, use_latest_human_message: bool = True):
-        rendered_template = self.tokenizer.apply_chat_template(
+        self.chat_agent = AIRunnerAgent(
+            model=self.model,
+            tokenizer=self.tokenizer,
+            streamer=self.streamer,
+            tools=self.tools,
             chat_template=self.chat_template,
-            conversation=self.prepare_messages(use_latest_human_message),
-            tokenize=False
-        )
-
-        # HACK: current version of transformers does not allow us to pass
-        # variables to the chat template function, so we apply those here
-        variables = {
-            "username": self.username,
-            "botname": self.botname,
-            "bot_mood": self.bot_mood,
-            "bot_personality": self.bot_personality,
-        }
-        for key, value in variables.items():
-            rendered_template = rendered_template.replace("{{ " + key + " }}", value)
-        return rendered_template
-
-    def llm_stream(self):
-        rendered_template = self.get_rendered_template()
-
-        # Encode the rendered template
-        encoded = self.tokenizer(rendered_template, return_tensors="pt")
-        model_inputs = encoded.to("cuda" if torch.cuda.is_available() else "cpu")
-
-        # Generate the response
-        self.logger.info("Generating...")
-        self.thread = threading.Thread(target=self.model.generate, kwargs=dict(
-            model_inputs,
+            username=self.username,
+            botname=self.botname,
+            bot_mood=self.bot_mood,
+            bot_personality=self.bot_personality,
             min_length=self.min_length,
             max_length=self.max_length,
             num_beams=self.num_beams,
-            do_sample=True,
+            do_sample=self.do_sample,
             top_k=self.top_k,
             eta_cutoff=self.eta_cutoff,
-            top_p=self.top_p,
-            num_return_sequences=self.sequences,
-            eos_token_id=self.tokenizer.eos_token_id,
-            early_stopping=True,
+            sequences=self.sequences,
+            early_stopping=self.early_stopping,
             repetition_penalty=self.repetition_penalty,
             temperature=self.temperature,
-            streamer=self.streamer
-        ))
-        self.thread.start()
-        # strip all new lines from rendered_template:
-        rendered_template = rendered_template.replace("\n", " ")
-        if self.is_mistral:
-            rendered_template = "<s>" + rendered_template
-        skip = True
-        streamed_template = ""
-        replaced = False
-        is_end_of_message = False
-        is_first_message = True
-        for new_text in self.streamer:
-            # strip all newlines from new_text
-            parsed_new_text = new_text.replace("\n", " ")
-            streamed_template += parsed_new_text
-            if self.is_mistral:
-                streamed_template = streamed_template.replace("<s> [INST]", "<s>[INST]")
-                streamed_template = streamed_template.replace("  [INST]", " [INST]")
-            # iterate over every character in rendered_template and
-            # check if we have the same character in streamed_template
-            if not replaced:
-                for i, char in enumerate(rendered_template):
-                    try:
-                        if char == streamed_template[i]:
-                            skip = False
-                        else:
-                            skip = True
-                            break
-                    except IndexError:
-                        skip = True
-                        break
-            if skip:
-                continue
-            elif not replaced:
-                replaced = True
-                streamed_template = streamed_template.replace(rendered_template, "")
-            else:
-                if self.is_mistral:
-                    if "</s>" in new_text:
-                        streamed_template = streamed_template.replace("</s>", "")
-                        new_text = new_text.replace("</s>", "")
-                        is_end_of_message = True
-                self.emit(
-                    SignalCode.LLM_TEXT_STREAMED_SIGNAL,
-                    dict(
-                        message=new_text,
-                        is_first_message=is_first_message,
-                        is_end_of_message=is_end_of_message,
-                        name=self.botname,
-                    )
-                )
-                is_first_message = False
-        return streamed_template
-
-    def agent_chat(self, prompt: AnyStr) -> AnyStr:
-        self.logger.info("Chat Stream")
-        res = self.agent.run(
-            prompt
+            is_mistral=self.is_mistral,
+            top_p=self.top_p,
+            guardrails_prompt=self.guardrails_prompt,
+            use_guardrails=self.use_guardrails,
+            system_instructions=self.system_instructions,
+            use_system_instructions=self.use_system_instructions,
+            user_evaluation=self.user_evaluation,
+            use_mood=self.use_mood,
+            use_personality=self.use_personality
         )
 
-        # if not res:
-        #     return "No response"
-        #
-        # if "</s>" in res:
-        #     res = res.replace("</s>", "")
-        #
-        # self.emit(
-        #     SignalCode.LLM_TEXT_STREAMED_SIGNAL,
-        #     dict(
-        #         message=res,
-        #         is_first_message=True,
-        #         is_end_of_message=True,
-        #         name=self.botname,
-        #     )
-        # )
-        # return res
-        return ""
-
-    # def on_llm_respond_to_user_signal(self, message):
-    #     self.logger.info("Responding to user")
-    #     self.prompt = message
-    #     self.llm.stream_chat(
-    #         self.prepare_messages()
-    #     )
-    #
-    #     full_message = self.stream_text(
-    #         response=self.streamer,
-    #         action=LLMAction.CHAT
-    #     )
-    #
-    #     self.add_message_to_history(
-    #         self.prompt,
-    #         LLMChatRole.HUMAN
-    #     )
-    #     self.add_message_to_history(
-    #         full_message,
-    #         LLMChatRole.ASSISTANT
-    #     )
-    #     self.send_final_message()
+    def on_llm_process_stt_audio_signal(self):
+        print("TODO: on_llm_process_stt_audio_signal")
 
     def load_streamer(self):
         self.logger.info("Loading LLM text streamer")
@@ -317,131 +196,14 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
         self.logger.info("Generating response")
         # self.bot_mood = self.update_bot_mood()
         # self.user_evaluation = self.do_user_evaluation()
-        full_message = self.chat()
         #full_message = self.rag_stream()
-        self.add_message_to_history(
-            self.prompt,
-            LLMChatRole.HUMAN
-        )
-        self.add_message_to_history(
-            full_message,
-            LLMChatRole.ASSISTANT
-        )
+
+        # First try running a tool
+        self.tool_agent.run(self.prompt)
+
+        # Then do chat prompt
+        self.chat_agent.run(self.prompt)
         self.send_final_message()
-
-    def build_system_prompt(self):
-        # if action == LLMAction.CHAT:
-        guardrails = self.guardrails_prompt if self.use_guardrails else ""
-        system_instructions = self.system_instructions if self.use_system_instructions else ""
-        names = f"Your name is {self.botname}. \nThe user's name is {self.username}."
-        mood = (
-            f"{self.botname} is not a computer or a bot, you are a human with emotions. "
-            f"{self.botname}'s mood changes depending on the conversation and other factors.\n"
-            f"{self.botname}'s current mood: {self.bot_mood}.\n"
-            f"{self.botname}'s evaluation of {self.username}: {self.user_evaluation}"
-        ) if self.use_mood else ""
-        personality = (
-            f"Your personality: {self.bot_personality}."
-        ) if self.use_personality else ""
-        system_prompt = [
-            guardrails,
-            system_instructions,
-            names,
-            mood,
-            personality,
-        ]
-        # elif action == LLMAction.RAG:
-        #     system_prompt = [
-        #         (
-        #             f"Based on the context, provide an accurate response to the "
-        #             f"user's message."
-        #         )
-        #     ]
-
-        return "\n".join(system_prompt)
-
-    def latest_human_message(self) -> dict:
-        return {} if not self.prompt else {
-            "content": self.prompt,
-            "role": LLMChatRole.HUMAN.value
-        }
-
-    def prepare_messages(
-        self,
-        use_latest_human_message: bool = True
-    ) -> list:
-        messages = [
-            {
-                "content": self.build_system_prompt(),
-                "role": LLMChatRole.SYSTEM.value
-            }
-        ]
-
-        messages += self.history
-
-        if use_latest_human_message:
-            messages.append(
-                self.latest_human_message()
-            )
-
-        return messages
-
-    def standard_text(
-        self,
-        response,
-        action: LLMAction
-    ):
-        response_parser = self.parse_chat_response
-
-        if action == LLMAction.RAG:
-            response_parser = self.parse_rag_response
-
-        content, is_end_of_message, full_message = response_parser(
-            content=response,
-            full_message=""
-        )
-        return content
-
-    def stream_text(
-        self,
-        response: Generator,
-        action: LLMAction,
-        is_first_message: bool = True,
-        full_message: str = "",
-        do_emit_streamed_text_signal: bool = True
-    ):
-        response_parser = self.parse_chat_response
-
-        for chat_response in response:
-            content, is_end_of_message, full_message = response_parser(
-                content=chat_response,
-                full_message=full_message
-            )
-            if do_emit_streamed_text_signal:
-                self.emit_streamed_text_signal(
-                    message=content,
-                    is_first_message=is_first_message,
-                    is_end_of_message=is_end_of_message
-                )
-            is_first_message = False
-        return full_message
-
-    def parse_chat_response(
-        self,
-        content: Any,
-        full_message: str = ""
-    ):
-        content, is_end_of_message = self.is_end_of_message(content)
-        content = content.replace(full_message, "")
-        full_message += content
-        return content, is_end_of_message, full_message
-
-    @staticmethod
-    def is_end_of_message(content: str) -> (str, bool):
-        if "</s>" in content:
-            content = content.replace("</s>", "")
-            return content, True
-        return content, False
 
     def emit_streamed_text_signal(self, **kwargs):
         kwargs["name"] = self.botname
@@ -449,16 +211,6 @@ class CasualLMTransformerBaseHandler(TokenizerHandler):
             SignalCode.LLM_TEXT_STREAMED_SIGNAL,
             kwargs
         )
-
-    def add_message_to_history(
-        self,
-        content: AnyStr,
-        role: LLMChatRole = LLMChatRole.ASSISTANT
-    ):
-        self.history.append({
-            'content': content,
-            'role': role.value
-        })
 
     def send_final_message(self):
         self.emit_streamed_text_signal(
