9e97bc0c647375d01acd548e4754a25656543a33
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-02-10 02:13:55 -0700

    rename canvas_plus_widget to canvas_widget

--- src/airunner/widgets/canvas/templates/canvas_ui.py
+++ src/airunner/widgets/canvas/templates/canvas_ui.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/../../src/airunner/widgets/canvas_plus/templates/canvas_plus.ui'
+# Form implementation generated from reading ui file '/home/joe/Projects/imagetopixel/airunner/src/airunner/../../src/airunner/widgets/canvas/templates/canvas.ui'
 #
 # Created by: PyQt6 UI code generator 6.6.1
 #
