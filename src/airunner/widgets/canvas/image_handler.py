b7d4cb8ecd467e2093039a41acfeae01ffcd42c5
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-02-12 05:32:01 -0700

    Improve canvas code

---
+++ src/airunner/widgets/canvas/image_handler.py
@@ -0,0 +1,72 @@
+from PIL import ImageFilter, Image
+from PyQt6.QtWidgets import QGraphicsPixmapItem
+
+from airunner.mediator_mixin import MediatorMixin
+from airunner.windows.main.settings_mixin import SettingsMixin
+
+
+class ImageHandler(
+    SettingsMixin,
+    MediatorMixin
+):
+    def __init__(self, image=None):
+        MediatorMixin.__init__(self)
+        SettingsMixin.__init__(self)
+        self.image = image
+        self.image_backup = None
+        self.previewing_filter = False
+
+    def load_image(self, image_path: str) -> Image:
+        image = Image.open(image_path)
+        if self.settings["resize_on_paste"]:
+            image.thumbnail((self.settings["is_maximized"],
+                             self.settings["working_height"]), Image.ANTIALIAS)
+        return image
+
+    def save_image(
+        self,
+        image_path: str,
+        image: Image = None,
+        scene_items: list = None
+    ):
+        if image is None and scene_items is not None:
+            for item in scene_items:
+                if isinstance(item, QGraphicsPixmapItem):
+                    image = item.pixmap.toImage()
+                    image.save(image_path)
+        else:
+            image.save(image_path)
+
+    def rotate_image(
+        self,
+        angle: float,
+        image: Image = None
+    ):
+        return image.transpose(angle) if image else image
+
+    def apply_filter(self, _filter_object: ImageFilter.Filter):
+        self.previewing_filter = False
+        self.image_backup = None
+
+    def cancel_filter(self) -> Image:
+        image = None
+        if self.image_backup:
+            image = self.image_backup.copy()
+            self.image_backup = None
+        self.previewing_filter = False
+        return image
+
+    def preview_filter(
+        self,
+        image: Image,
+        filter_object: ImageFilter.Filter
+    ):
+        if not image:
+            return
+        if not self.previewing_filter:
+            self.image_backup = image.copy()
+            self.previewing_filter = True
+        else:
+            image = self.image_backup.copy()
+        filtered_image = filter_object.filter(image)
+        return filtered_image
