0b6c65e965430c1e90b6b969bbe1e87504f968fe
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-02-14 09:15:30 -0700

    Fix snap to grid when erasing / drawing

--- src/airunner/widgets/canvas/custom_view.py
+++ src/airunner/widgets/canvas/custom_view.py
@@ -2,6 +2,7 @@ from PyQt6.QtCore import QPointF
 from PyQt6.QtGui import QMouseEvent
 from PyQt6.QtWidgets import QGraphicsView
 
+from airunner.enums import CanvasToolName
 from airunner.mediator_mixin import MediatorMixin
 from airunner.windows.main.settings_mixin import SettingsMixin
 
@@ -17,10 +18,22 @@ class CustomGraphicsView(
         SettingsMixin.__init__(self)
 
     def adjust_to_grid(self, event: QMouseEvent) -> QMouseEvent:
-        cell_size = self.settings["grid_settings"]["cell_size"]
-        # Calculate the grid cell coordinates
-        grid_x = round(event.pos().x() / cell_size) * cell_size
-        grid_y = round(event.pos().y() / cell_size) * cell_size
+        """
+        This is used to adjust the selection tool to the grid
+        in real time.
+        :param event:
+        :return:
+        """
+        if (
+            self.settings["grid_settings"]["snap_to_grid"] and
+            self.settings["current_tool"] == CanvasToolName.SELECTION
+        ):
+            cell_size = self.settings["grid_settings"]["cell_size"]
+            grid_x = round(event.pos().x() / cell_size) * cell_size
+            grid_y = round(event.pos().y() / cell_size) * cell_size
+        else:
+            grid_x = event.pos().x()
+            grid_y = event.pos().y()
 
         # Create a new event with the adjusted position
         new_event = QMouseEvent(
@@ -34,10 +47,8 @@ class CustomGraphicsView(
 
     def mousePressEvent(self, event: QMouseEvent):
         new_event = self.adjust_to_grid(event)
-        # Pass the new event to the base class's method
         super().mousePressEvent(new_event)
 
     def mouseMoveEvent(self, event: QMouseEvent):
         new_event = self.adjust_to_grid(event)
-        # Pass the new event to the base class's method
         super().mouseMoveEvent(new_event)
