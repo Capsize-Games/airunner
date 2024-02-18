b05d1a09d083027f66645b1ba6a8321951d07951
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-02-18 04:36:10 -0700

    Fix snap to grid

--- src/airunner/utils.py
+++ src/airunner/utils.py
@@ -514,21 +514,21 @@ def stop_profiler(pr):
 def snap_to_grid(x: int, y: int, use_floor: bool = True):
     settings = ServiceLocator.get("get_settings")()
     cell_size = settings["grid_settings"]["cell_size"]
+    if settings["grid_settings"]["snap_to_grid"]:
+        x_is_negative = x < 0
+        y_is_negative = y < 0
 
-    x_is_negative = x < 0
-    y_is_negative = y < 0
-
-    if use_floor:
-        x = abs(x)
-        y = abs(y)
-        x = math.floor(x / cell_size) * cell_size
-        y = math.floor(y / cell_size) * cell_size
-        if x_is_negative:
-            x = -x
-        if y_is_negative:
-            y = -y
-    else:
-        x = round(x / cell_size) * cell_size
-        y = round(y / cell_size) * cell_size
+        if use_floor:
+            x = abs(x)
+            y = abs(y)
+            x = math.floor(x / cell_size) * cell_size
+            y = math.floor(y / cell_size) * cell_size
+            if x_is_negative:
+                x = -x
+            if y_is_negative:
+                y = -y
+        else:
+            x = round(x / cell_size) * cell_size
+            y = round(y / cell_size) * cell_size
 
     return x, y
