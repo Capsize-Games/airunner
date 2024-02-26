3bb35665a1f8c20bee24d746bc629c14c2107358
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-02-26 04:59:45 -0700

    Fix lora loading by version

--- src/airunner/aihandler/mixins/lora_mixin.py
+++ src/airunner/aihandler/mixins/lora_mixin.py
@@ -4,10 +4,17 @@ import os
 class LoraMixin:
     def __init__(self):
         self.loaded_lora = None
+        self.disabled_lora = {}
+        self._available_lora = None
 
     @property
     def available_lora(self):
-        return self.options.get(f"lora", [])
+        if not self._available_lora:
+            self._available_lora = {}
+            available_lora = self.options.get(f"lora", [])
+            for lora in available_lora:
+                self._available_lora[lora["version"]] = lora
+        return self._available_lora
 
     def add_lora_to_pipe(self):
         self.loaded_lora = []
@@ -17,7 +24,9 @@ class LoraMixin:
         model_base_path = self.model_base_path
         lora_path = self.lora_path
         path = os.path.join(model_base_path, lora_path) if lora_path == "lora" else lora_path
-        for lora in self.available_lora:
+        if self.model_version not in self.available_lora:
+            return
+        for lora in self.available_lora[self.model_version]:
             if lora["enabled"] == False:
                 continue
             filepath = None
@@ -29,8 +38,21 @@ class LoraMixin:
             self.load_lora(filepath, lora)
 
     def load_lora(self, checkpoint_path, lora):
+        if self.model_base_path in self.disabled_lora:
+            if checkpoint_path in self.disabled_lora[self.model_base_path]:
+                return
+
         try:
             self.pipe.load_lora_weights(".", weight_name=checkpoint_path)
             self.loaded_lora.append({"name": lora["name"], "scale": lora["scale"]})
         except AttributeError:
             self.logger.warning("This model does not support LORA")
+            self.disable_lora(checkpoint_path)
+        except RuntimeError:
+            self.logger.warning("LORA could not be loaded")
+            self.disable_lora(checkpoint_path)
+
+    def disable_lora(self, checkpoint_path):
+        if self.model_base_path not in self.disabled_lora:
+            self.disabled_lora[self.model_base_path] = []
+        self.disabled_lora[self.model_base_path].append(checkpoint_path)
