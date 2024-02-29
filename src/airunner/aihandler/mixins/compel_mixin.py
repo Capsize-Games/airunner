945042a8eb4b6b05308ed6bafbfb622991d9a9b8
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-02-29 13:24:50 -0700

    Enhancement for drawing

--- src/airunner/aihandler/mixins/compel_mixin.py
+++ src/airunner/aihandler/mixins/compel_mixin.py
@@ -6,36 +6,9 @@ from airunner.utils import clear_memory
 
 class CompelMixin:
     def __init__(self):
-        self._compel_proc = None
-        self._compel_proc = None
-        self._prompt_embeds = None
         self._prompt_embeds = None
         self._negative_prompt_embeds = None
-        self._negative_prompt_embeds = None
-        self._prompt_embeds = None
-        self._negative_prompt_embeds = None
-
-    @property
-    def compel_proc(self):
-        try:
-            if not self._compel_proc:
-                textual_inversion_manager = DiffusersTextualInversionManager(self.pipe)
-                self._compel_proc = Compel(
-                    tokenizer=self.pipe.tokenizer,
-                    text_encoder=self.pipe.text_encoder,
-                    truncate_long_prompts=False,
-                    textual_inversion_manager=textual_inversion_manager,
-                    dtype_for_device_getter=lambda _x: self.data_type,
-                    device=self.pipe.device
-                )
-            return self._compel_proc
-        except Exception as e:
-            self.logger.error(f"Error creating compel proc: {e}")
-            return None
-
-    @compel_proc.setter
-    def compel_proc(self, value):
-        self._compel_proc = value
+        self.compel_proc = None
 
     @property
     def prompt_embeds(self):
@@ -61,23 +34,71 @@ class CompelMixin:
     _current_prompt = ""
     _current_negative_prompt = ""
 
-    def load_prompt_embeds(self):
+    def load_prompt_embeds(self, do_reload=True):
         self.logger.info("Loading prompt embeds")
         prompt = self.prompt if self.prompt else ""
         negative_prompt = self.negative_prompt if self.negative_prompt else ""
-        self.compel_proc = None
+        try:
+            if self.compel_proc is None:
+                textual_inversion_manager = DiffusersTextualInversionManager(self.pipe)
+                self.compel_proc = Compel(
+                    tokenizer=self.pipe.tokenizer,
+                    text_encoder=self.pipe.text_encoder,
+                    truncate_long_prompts=False,
+                    textual_inversion_manager=textual_inversion_manager,
+                    dtype_for_device_getter=lambda _x: self.data_type,
+                    device=self.pipe.device
+                )
+        except Exception as e:
+            self.logger.error(f"Error creating compel proc: {e}")
+            return None
+        compel_proc = self.compel_proc
         clear_memory()
         self._current_prompt = prompt
         self._current_negative_prompt = negative_prompt
 
+        prompt_embeds = None
+        negative_prompt_embeds = None
+
         # check if prompt is string
         if isinstance(prompt, str):
-            prompt_embeds = self.compel_proc.build_conditioning_tensor(prompt)
-            negative_prompt_embeds = self.compel_proc.build_conditioning_tensor(negative_prompt)
+            try:
+                prompt_embeds = compel_proc.build_conditioning_tensor(prompt)
+            except RuntimeError as e:
+                self.logger.error("Error building prompt embeds")
+                self.logger.error(e)
+
+            try:
+                negative_prompt_embeds = compel_proc.build_conditioning_tensor(negative_prompt)
+            except RuntimeError as e:
+                self.logger.error("Error building negative prompt embeds")
+                self.logger.error(e)
         else:
-            prompt_embeds = self.compel_proc(prompt)
-            negative_prompt_embeds = self.compel_proc(negative_prompt)
-        [prompt_embeds, negative_prompt_embeds] = self.compel_proc.pad_conditioning_tensors_to_same_length([prompt_embeds, negative_prompt_embeds])
+            try:
+                prompt_embeds = compel_proc(prompt)
+            except RuntimeError as e:
+                self.logger.error("Error building prompt embeds")
+                self.logger.error(e)
+            try:
+                negative_prompt_embeds = compel_proc(negative_prompt)
+            except RuntimeError as e:
+                self.logger.error("Error building negative prompt embeds")
+                self.logger.error(e)
+
+        if prompt_embeds is None or negative_prompt_embeds is None:
+            if do_reload:
+                return self.load_prompt_embeds(do_reload=False)
+            else:
+                self.logger.error("Prompt embeds are None")
+                return
+
+        [
+            prompt_embeds,
+            negative_prompt_embeds
+        ] = compel_proc.pad_conditioning_tensors_to_same_length([
+            prompt_embeds,
+            negative_prompt_embeds
+        ])
         self.prompt_embeds = prompt_embeds
         self.negative_prompt_embeds = negative_prompt_embeds
 
