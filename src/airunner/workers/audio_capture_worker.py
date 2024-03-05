d67e3f7847644d55e1f57c006e35e0fa6638f2bb
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-03-05 06:34:21 -0700

    audio capture fixes

--- src/airunner/workers/audio_capture_worker.py
+++ src/airunner/workers/audio_capture_worker.py
@@ -16,18 +16,11 @@ class AudioCaptureWorker(Worker):
 
     def __init__(self, prefix):
         super().__init__(prefix)
-        self.recording = []
-        self.running: bool = False
         self.listening: bool = False
-        self.is_recieving_input: bool = False
         self.voice_input_start_time: time.time = None
         stt_settings = self.settings["stt_settings"]
         self.chunk_duration = stt_settings["chunk_duration"]  # duration of chunks in milliseconds
         self.fs = stt_settings["fs"]
-        self.channels = stt_settings["channels"]
-        self.volume_input_threshold = stt_settings["volume_input_threshold"]  # threshold for volume input
-        self.silence_buffer_seconds = stt_settings["silence_buffer_seconds"]  # in seconds
-        self.update_properties()
         self.register(
             SignalCode.STT_STOP_CAPTURE_SIGNAL,
             self.stop_listening
@@ -36,56 +29,57 @@ class AudioCaptureWorker(Worker):
             SignalCode.STT_START_CAPTURE_SIGNAL,
             self.start_listening
         )
-
-    def update_properties(self):
-        stt_settings = self.settings["stt_settings"]
-        self.chunk_duration = stt_settings["chunk_duration"]
-        self.fs = stt_settings["fs"]
-        self.channels = stt_settings["channels"]
-        self.volume_input_threshold = stt_settings["volume_input_threshold"]
-        self.silence_buffer_seconds = stt_settings["silence_buffer_seconds"]
+        fs = self.settings["stt_settings"]["fs"]
+        channels = self.settings["stt_settings"]["channels"]
+        self.stream = sd.InputStream(samplerate=fs, channels=channels)
 
     def start(self):
         self.logger.debug("Starting")
-        self.running = True
+        running = True
         if self.settings["stt_enabled"]:
             self.start_listening()
-        while self.running:
-            while self.listening and self.running:
-                chunk = sd.rec(
-                    int(self.chunk_duration * self.fs),
-                    samplerate=self.fs,
-                    channels=self.channels,
-                    dtype="float32"
-                )
-                sd.wait()
-                if np.max(np.abs(chunk)) > self.volume_input_threshold:  # check if chunk is not silence
-                    self.is_recieving_input = True
-                    self.voice_input_start_time = time.time()
-                else:
-                    # make voice_end_time self.silence_buffer_seconds after voice_input_start_time
-                    if self.voice_input_start_time is not None and time.time() >= self.voice_input_start_time + self.silence_buffer_seconds:
-                        if len(self.recording) > 0:
-                            self.handle_message(b''.join(self.recording))
-                            self.recording = []
-                            self.is_recieving_input = False
-                if self.is_recieving_input:
+        stt_settings = self.settings["stt_settings"]
+        chunk_duration = stt_settings["chunk_duration"]
+        fs = self.settings["stt_settings"]["fs"]
+        volume_input_threshold = self.settings["stt_settings"]["volume_input_threshold"]
+        silence_buffer_seconds = self.settings["stt_settings"]["silence_buffer_seconds"]
+        voice_input_start_time = None
+        recording = []
+        is_receiving_input = False
+        emit = self.emit
+        while running:
+            while self.listening and running:
+                chunk, overflowed = self.stream.read(int(chunk_duration * fs))
+                if np.max(np.abs(chunk)) > volume_input_threshold:  # check if chunk is not silence
+                    is_receiving_input = True
+                    voice_input_start_time = time.time()
+                elif voice_input_start_time is not None:
+                    # make voice_end_time silence_buffer_seconds after voice_input_start_time
+                    end_time = voice_input_start_time + silence_buffer_seconds
+                    if time.time() >= end_time:
+                        if len(recording) > 0:
+                            emit(
+                                SignalCode.AUDIO_CAPTURE_WORKER_RESPONSE_SIGNAL,
+                                b''.join(recording)
+                            )
+                            recording = []
+                            is_receiving_input = False
+                if is_receiving_input:
                     chunk_bytes = np.int16(chunk * 32767).tobytes()  # convert to bytes
-                    self.recording.append(chunk_bytes)
+                    recording.append(chunk_bytes)
 
-            while not self.listening and self.running:
+            while not self.listening and running:
                 QThread.msleep(SLEEP_TIME_IN_MS)
 
     def handle_message(self, message):
-        self.emit(
-            SignalCode.AUDIO_CAPTURE_WORKER_RESPONSE_SIGNAL,
-            message
-        )
+        pass
 
     def start_listening(self):
         self.logger.debug("Start listening")
         self.listening = True
+        self.stream.start()
 
     def stop_listening(self):
         self.logger.debug("Stop listening")
         self.listening = False
+        self.stream.stop()
