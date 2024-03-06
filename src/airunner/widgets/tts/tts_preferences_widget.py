a1d9085ca29bc40517e5d60ae7092b022cc6d3a2
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-03-06 10:49:59 -0700

    templates for more tts settings

---
+++ src/airunner/widgets/tts/tts_preferences_widget.py
@@ -0,0 +1,43 @@
+from airunner.widgets.base_widget import BaseWidget
+from airunner.widgets.tts.templates.tts_preferences_ui import Ui_tts_preferences
+
+
+class TTSPreferencesWidget(BaseWidget):
+    widget_class_ = Ui_tts_preferences
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.initialize_form()
+    
+    def initialize_form(self):
+        elements = [
+            self.ui.enable_tts,
+            self.ui.model_combobox,
+        ]
+
+        for element in elements:
+            element.blockSignals(True)
+
+        tts_model = self.settings["tts_settings"]["model"]
+        self.ui.enable_tts.setChecked(self.settings["tts_settings"]["enable_tts"])
+        self.ui.model_combobox.clear()
+        models = ["Bark", "SpeechT5", "SPD"]
+        self.ui.model_combobox.addItems(models)
+        self.ui.model_combobox.setCurrentText(tts_model)
+        self.ui.bark_preferences.setVisible(tts_model == "Bark")
+        self.ui.speecht5_preferences.setVisible(tts_model == "SpeechT5")
+        self.ui.spd_preferences.setVisible(tts_model == "SPD")
+
+        for element in elements:
+            element.blockSignals(False)
+
+    def enable_tts_changed(self, val):
+        settings = self.settings
+        settings["tts_settings"]["enable_tts"] = val
+        self.settings = settings
+
+    def model_changed(self, val):
+        settings = self.settings
+        settings["tts_settings"]["model"] = val
+        self.settings = settings
+        self.initialize_form()
