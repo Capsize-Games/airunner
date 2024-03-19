5a0239aff1d4024d8a1b3c5cb46467a251b32e6a
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-03-19 09:54:39 -0600

    Fix sliders and scenes

--- src/airunner/widgets/active_grid_settings/active_grid_settings_widget.py
+++ src/airunner/widgets/active_grid_settings/active_grid_settings_widget.py
@@ -31,6 +31,22 @@ class ActiveGridSettingsWidget(BaseWidget):
             SignalCode.APPLICATION_ACTIVE_GRID_AREA_UPDATED: self.update_size
         }
 
+        for k in [
+            "border_opacity_slider_widget",
+            "fill_opacity_slider_widget",
+            "width_slider_widget",
+            "height_slider_widget",
+        ]:
+            getattr(self.ui, k).settings_loaded(self.callback)
+
+    def callback(self, prop, val):
+        settings = self.settings
+        if prop in ["border_opacity", "fill_opacity"]:
+            settings["active_grid_settings"][prop] = val
+        else:
+            settings[prop] = val
+        self.settings = settings
+
     def update_size(self):
         settings = self.settings
         self.ui.width_slider_widget.slider_maximum = settings["working_width"]
