060142e4029fba6c623294513cf1349c724bb739
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-03-24 10:52:29 -0600

    ImprovesPartially working outpaint automask

--- src/airunner/widgets/generator_form/generator_form_widget.py
+++ src/airunner/widgets/generator_form/generator_form_widget.py
@@ -163,61 +163,32 @@ class GeneratorForm(BaseWidget):
     def do_generate_image(self):
         time.sleep(0.1)
 
-        image = convert_base64_to_image(self.settings["canvas_settings"]["image"])
-        if image is not None:
-            # Create a white image of the same size as the input image
-            mask = Image.new('RGB', (self.settings["working_width"], self.settings["working_height"]), 'black')
-            draw = ImageDraw.Draw(mask)
+        if self.settings["generator_settings"]["section"] == GeneratorSection.OUTPAINT.value:
+            image = convert_base64_to_image(self.settings["canvas_settings"]["image"])
+            mask = convert_base64_to_image(self.settings["outpaint_settings"]["image"])
 
-            # Calculate the overlap between the active rectangle and the image
             active_rect = self.active_rect
-            image_overlap_left = max(0, active_rect.left())
-            image_overlap_top = max(0, active_rect.top())
-            image_overlap_right = min(self.settings["working_width"], active_rect.right())
-            image_overlap_bottom = min(self.settings["working_height"], active_rect.bottom())
-
             overlap_left = max(0, active_rect.left())
-            overlap_top = self.settings["working_height"] - active_rect.top()
             overlap_right = min(self.settings["working_width"], active_rect.right())
-            overlap_bottom = self.settings["working_height"] + active_rect.top()
+            overlap_top = max(0, active_rect.top())
+            overlap_bottom = min(self.settings["working_height"], active_rect.bottom())
 
-            # If there is an overlap, draw a black rectangle on the mask at the overlap position
-            if overlap_left < overlap_right and overlap_top < overlap_bottom:
-                print("WE HAVE OVERLAP", overlap_left, overlap_top, overlap_right, overlap_bottom)
-                print(active_rect.left(), active_rect.top(), active_rect.right(), active_rect.bottom())
-                print(self.settings["working_width"], self.settings["working_height"])
-                draw.rectangle((overlap_left, overlap_top, overlap_right, overlap_bottom), fill='white')
+            # Crop the image at the overlap position
+            cropped_image = image.crop(
+                (overlap_left, overlap_top, overlap_right, overlap_bottom))
 
-                # Crop the image at the overlap position
-                cropped_image = image.crop((image_overlap_left, image_overlap_top, image_overlap_right, image_overlap_bottom))
+            # Create a new black image of the same size as the input image
+            new_image = Image.new('RGB', (self.settings["working_width"], self.settings["working_height"]), 'black')
 
-                # Create a new black image of the same size as the input image
-                new_image = Image.new('RGB', (self.settings["working_width"], self.settings["working_height"]), 'black')
+            # Paste the cropped image to the top of the new image
+            new_image.paste(cropped_image, (0, 0))
 
-                # Paste the cropped image to the top of the new image
-                new_image.paste(cropped_image, (0, 0))
-
-                mask.save("mask.png")
-                new_image.save("new_image.png")
-
-                # Emit the signal with the new image and the mask
-                self.emit_signal(SignalCode.DO_GENERATE_SIGNAL, {
-                    "mask_image": mask.convert("RGB"),
-                    "image": new_image.convert("RGB")
-                })
-            else:
-                base64mask = convert_image_to_base64(mask)
-                settings = self.settings
-                settings["canvas_settings"]["mask"] = base64mask
-                self.settings = settings
-
-                if self.settings["generator_settings"]["section"] == GeneratorSection.OUTPAINT.value:
-                    self.emit_signal(SignalCode.DO_GENERATE_SIGNAL, {
-                        "mask_image": mask.convert("RGB"),
-                        "image": image.convert("RGB")
-                    })
-                else:
-                    self.emit_signal(SignalCode.DO_GENERATE_SIGNAL)
+            self.emit_signal(SignalCode.DO_GENERATE_SIGNAL, {
+                "mask_image": mask.convert("RGB"),
+                "image": image.convert("RGB")
+            })
+        else:
+            self.emit_signal(SignalCode.DO_GENERATE_SIGNAL)
 
     def call_generate(self):
         self.emit_signal(SignalCode.LLM_UNLOAD_SIGNAL, {
