ac10ef49bb34ffb2ef2e9ffe86367449c061fb2b
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-03-26 11:33:23 -0600

    add border to image

--- src/airunner/widgets/canvas/custom_scene.py
+++ src/airunner/widgets/canvas/custom_scene.py
@@ -2,7 +2,7 @@ from types import NoneType
 from typing import Optional
 
 import PIL
-from PIL import ImageQt, Image, ImageFilter
+from PIL import ImageQt, Image, ImageFilter, ImageOps
 from PIL.ImageQt import QImage
 from PySide6.QtCore import Qt, QPoint
 from PySide6.QtGui import QEnterEvent, QDragEnterEvent, QDropEvent, QImageReader, QDragMoveEvent
@@ -219,18 +219,24 @@ class CustomScene(
         self.load_image_from_object(image=filtered_image)
 
     def add_image_to_scene(
-        self,
-        image: Image,
-        is_outpaint: bool = False,
-        outpaint_box_rect: QPoint = None
+            self,
+            image: Image,
+            is_outpaint: bool = False,
+            outpaint_box_rect: QPoint = None,
+            border_size: int = 1,  # size of the border
+            border_color: tuple = (255, 0, 0, 255)  # color of the border in RGBA format
     ):
         """
         Adds a given image to the scene
         :param image_data: dict containing the image to be added to the scene
         :param is_outpaint: bool indicating if the image is an outpaint
         :param outpaint_box_rect: QPoint indicating the root point of the image
+        :param border_size: int indicating the size of the border
+        :param border_color: tuple indicating the color of the border
         :return:
         """
+        image = ImageOps.expand(image, border=border_size, fill=border_color)
+
         if is_outpaint:
             image, root_point, pivot_point = self.handle_outpaint(
                 outpaint_box_rect,
