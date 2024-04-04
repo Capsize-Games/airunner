48e9f302af325b1b957b8fe8f56bd5923b1783b5
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-04 12:06:03 -0600

    Fixes scan for models after download complete

--- src/airunner/aihandler/stablediffusion/download_civitai.py
+++ src/airunner/aihandler/stablediffusion/download_civitai.py
@@ -4,10 +4,16 @@ import requests
 from json.decoder import JSONDecodeError
 from PySide6.QtCore import QThread
 from airunner.aihandler.stablediffusion.download_worker import DownloadWorker
+from airunner.enums import SignalCode
+from airunner.mediator_mixin import MediatorMixin
 
 
-class DownloadCivitAI:
+class DownloadCivitAI(
+    MediatorMixin
+):
     def __init__(self):
+        MediatorMixin.__init__(self)
+        super().__init__()
         self.thread = None
         self.worker = None
         self.file_name = None
@@ -35,6 +41,7 @@ class DownloadCivitAI:
         self.worker.moveToThread(self.thread)
 
         # Connect signals
+        self.worker.finished.connect(lambda: self.emit_signal(SignalCode.DOWNLOAD_COMPLETE))
         self.worker.finished.connect(self.thread.quit)
         self.worker.progress.connect(lambda current, total: callback(current, total))
         self.worker.finished.connect(self.worker.deleteLater)
