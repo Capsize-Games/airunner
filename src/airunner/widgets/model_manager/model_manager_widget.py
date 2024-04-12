486f760218c069169804854a65eb89f87d55ba8c
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-12 07:17:45 -0600

    Clean up code for readability

--- src/airunner/widgets/model_manager/model_manager_widget.py
+++ src/airunner/widgets/model_manager/model_manager_widget.py
@@ -1,7 +1,7 @@
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QFileDialog
 
-from airunner.enums import SignalCode, ServiceCode
+from airunner.enums import SignalCode
 from airunner.models.modeldata import ModelData
 from airunner.widgets.model_manager.templates.model_manager_ui import Ui_model_manager
 from airunner.windows.base_window import BaseWindow
