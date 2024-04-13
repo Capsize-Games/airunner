c658143131a15e58333141dc8080705e159414ec
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-13 10:24:36 -0600

    Split utils into separate files

---
+++ src/airunner/utils/get_main_window.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+def get_main_window():
+    app = QApplication.instance()
+    for widget in app.topLevelWidgets():
+        if isinstance(widget, QMainWindow):
+            return widget
