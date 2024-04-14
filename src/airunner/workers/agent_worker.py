1a6eaba05fa70edb85f821675278c87f173b2cc4
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-14 12:58:32 -0600

    Create agent worker to use qthreads

---
+++ src/airunner/workers/agent_worker.py
@@ -0,0 +1,18 @@
+import traceback
+import torch
+from airunner.workers.worker import Worker
+
+
+class AgentWorker(Worker):
+    def handle_message(self, message):
+        input_ids = message["kwargs"].get("input_ids", None)
+        if input_ids is not None:
+            if torch.isnan(input_ids).any():
+                print("Model output contains NaN values.")
+                return None
+        try:
+            message["model"].generate(**message["kwargs"])
+        except Exception as e:
+            print("47 An error occurred in model.generate:")
+            print(str(e))
+            print(traceback.format_exc())
