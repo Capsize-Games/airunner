960333d5c5c4cbeee21109ba547de66211322750
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-15 16:02:01 -0600

    update set_huggingface_environment_variables

--- src/airunner/app.py
+++ src/airunner/app.py
@@ -48,6 +48,9 @@ from airunner.settings import (
     HF_DATASETS_OFFLINE,
     TRANSFORMERS_OFFLINE,
     DIFFUSERS_VERBOSITY,
+    DEFAULT_HF_INFERENCE_ENDPOINT,
+    DEFAULT_HF_HUB_OFFLINE,
+    DEFAULT_HF_ENDPOINT, DEFAULT_HF_DATASETS_OFFLINE, DEFAULT_TRANSFORMERS_OFFLINE,
 )
 
 
@@ -61,16 +64,30 @@ def set_huggingface_environment_variables(
     :param allow_remote_inference:
     :return:
     """
-    allow_downloads = HF_ALLOW_DOWNLOADS if allow_downloads is None else allow_downloads
-
-    if allow_downloads:
-        os.environ["HF_ALLOW_DOWNLOADS"] = "1"
-
+    # The only reason this isn't hard coded is in case they change the
+    # expected value in the future.
     os.environ["HF_HUB_DISABLE_TELEMETRY"] = HF_HUB_DISABLE_TELEMETRY
-    os.environ["HF_HUB_OFFLINE"] = HF_HUB_OFFLINE
+
+    """
+    Conditionally set environment variables which are used 
+    to control the ability of the Hugging Face Hub and other
+    related services to access the internet.
+    """
+    hf_hub_offline = HF_HUB_OFFLINE if not allow_downloads else DEFAULT_HF_HUB_OFFLINE
+    hf_datasets_offline = HF_DATASETS_OFFLINE if not allow_downloads else DEFAULT_HF_DATASETS_OFFLINE
+    transformers_offline = TRANSFORMERS_OFFLINE if not allow_downloads else DEFAULT_TRANSFORMERS_OFFLINE
+    hf_endpoint = HF_ENDPOINT if not allow_downloads else DEFAULT_HF_ENDPOINT
+    hf_inf_endpoint = HF_INFERENCE_ENDPOINT if not allow_downloads else DEFAULT_HF_INFERENCE_ENDPOINT
+    os.environ["HF_ENDPOINT"] = hf_endpoint
+    os.environ["HF_DATASETS_OFFLINE"] = hf_datasets_offline
+    os.environ["TRANSFORMERS_OFFLINE"] = transformers_offline
+    os.environ["HF_INFERENCE_ENDPOINT"] = hf_inf_endpoint
+    os.environ["HF_HUB_OFFLINE"] = hf_hub_offline
+
+    """
+    Set the remaining environment variables for the Hugging Face Hub.
+    """
     os.environ["HF_HOME"] = HF_HOME
-    os.environ["HF_ENDPOINT"] = HF_ENDPOINT
-    os.environ["HF_INFERENCE_ENDPOINT"] = HF_INFERENCE_ENDPOINT
     os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = HF_HUB_DISABLE_PROGRESS_BARS
     os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = HF_HUB_DISABLE_SYMLINKS_WARNING
     os.environ["HF_HUB_DISABLE_EXPERIMENTAL_WARNING"] = HF_HUB_DISABLE_EXPERIMENTAL_WARNING
@@ -81,8 +98,6 @@ def set_huggingface_environment_variables(
     os.environ["HF_HUB_DOWNLOAD_TIMEOUT"] = HF_HUB_DOWNLOAD_TIMEOUT
     os.environ["HF_HUB_ETAG_TIMEOUT"] = HF_HUB_ETAG_TIMEOUT
     os.environ["HF_HUB_DISABLE_IMPLICIT_TOKEN"] = HF_HUB_DISABLE_IMPLICIT_TOKEN
-    os.environ["HF_DATASETS_OFFLINE"] = HF_DATASETS_OFFLINE
-    os.environ["TRANSFORMERS_OFFLINE"] = TRANSFORMERS_OFFLINE
     os.environ["DIFFUSERS_VERBOSITY"] = DIFFUSERS_VERBOSITY
 
 
