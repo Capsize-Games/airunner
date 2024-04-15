0029b7b9ae4f00dfb990f20cf1a3d1ac343c9a8d
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-15 14:01:56 -0600

    Whisper handler refactor
    clean up and reduce code

--- src/airunner/aihandler/stt/whisper_handler.py
+++ src/airunner/aihandler/stt/whisper_handler.py
@@ -1,50 +1,32 @@
 import torch
 from transformers import AutoProcessor, WhisperForConditionalGeneration, AutoFeatureExtractor
 from airunner.aihandler.stt.stt_handler import STTHandler
+from airunner.settings import DEFAULT_STT_HF_PATH
 
 
 class WhisperHandler(STTHandler):
     """
     Handler for the Whisper model from OpenAI.
     """
-    def load_model(self):
-        self.logger.debug("Loading model")
+    def load_component(self, model_path, component_class, component_name):
+        self.logger.debug(f"Loading {component_name}")
         try:
-            self.model = WhisperForConditionalGeneration.from_pretrained(
-                "openai/whisper-tiny.en",
-                local_files_only=True,
-                torch_dtype=torch.bfloat16,
-                device_map=self.device
-            )
-        except OSError as _e:
-            return self.load_model(local_files_only=True)
-        except NotImplementedError as _e:
-            self.logger.error("Failed to load model")
-            self.logger.error(_e)
-            return None
-
-    def load_processor(self):
-        self.logger.debug("Loading processor")
-        try:
-            self.processor = AutoProcessor.from_pretrained(
-                "openai/whisper-tiny.en",
+            return component_class.from_pretrained(
+                model_path,
                 local_files_only=True,
                 torch_dtype=torch.bfloat16,
                 device_map=self.device
             )
         except Exception as e:
-            self.logger.error("Failed to load processor")
+            self.logger.error(f"Failed to load {component_name}")
             self.logger.error(e)
             return None
 
+    def load_model(self):
+        self.model = self.load_component(DEFAULT_STT_HF_PATH, WhisperForConditionalGeneration, "model")
+
+    def load_processor(self):
+        self.processor = self.load_component(DEFAULT_STT_HF_PATH, AutoProcessor, "processor")
+
     def load_feature_extractor(self):
-        try:
-            self.feature_extractor = AutoFeatureExtractor.from_pretrained(
-                "openai/whisper-base",
-                local_files_only=True,
-                torch_dtype=torch.bfloat16,
-                device_map=self.device
-            )
-        except OSError as _e:
-            self.logger.error("Failed to load extractor")
-            return None
+        self.feature_extractor = self.load_component(DEFAULT_STT_HF_PATH, AutoFeatureExtractor, "feature extractor")
