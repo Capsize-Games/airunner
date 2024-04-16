0b007e5c3c99a0d792bdf51cb186800628eb0209
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-16 09:05:53 -0600

    Improves internet disabling for application

--- src/airunner/main.py
+++ src/airunner/main.py
@@ -1,3 +1,14 @@
+"""
+----------------------------------------------------------------
+Import order is crucial for AI Runner to work as expected.
+Do not remove the no_internet_socket import.
+Do not change the order of the imports.
+Importing this module ensures that the internet is completely
+disabled for the AI Runner application.
+See the no_internet_socket module for more information.
+----------------------------------------------------------------
+"""
+from airunner.utils import no_internet_socket
 from airunner.app import App
 
 
