6eb4992853ef4715a5550b4a0cdba66ffd746386
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-17 13:05:20 -0600

    May fixes for security, widgets and setup wizard

--- src/airunner/windows/setup_wizard/model_setup/stable_diffusion_setup/stable_diffusion_setup.py
+++ src/airunner/windows/setup_wizard/model_setup/stable_diffusion_setup/stable_diffusion_setup.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import Slot
 from airunner.windows.setup_wizard.base_wizard import BaseWizard
 from airunner.windows.setup_wizard.model_setup.stable_diffusion_setup.templates.stable_diffusion_model_setup_ui import \
     Ui_stable_diffusion_model_setup
@@ -8,3 +9,13 @@ class StableDiffusionSetupPage(BaseWizard):
 
     def __init__(self, *args):
         super(StableDiffusionSetupPage, self).__init__(*args)
+        self.no_toggled = False
+        self.yes_toggled = False
+
+    @Slot(bool)
+    def no_toggled(self, val: bool):
+        self.no_toggled = val
+
+    @Slot(bool)
+    def yes_toggled(self, val: bool):
+        self.yes_toggled = val
