523d780a208e167090b3795deef21419bf597826
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-19 05:33:38 -0600

    Fix age restriction page design
    Signed-off-by: w4ffl35 <25737761+w4ffl35@users.noreply.github.com>

--- src/airunner/windows/setup_wizard/age_restriction/templates/age_restriction_ui.py
+++ src/airunner/windows/setup_wizard/age_restriction/templates/age_restriction_ui.py
@@ -15,31 +15,41 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QFrame, QGridLayout,
-    QLabel, QScrollArea, QSizePolicy, QSpacerItem,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QFormLayout, QFrame,
+    QGridLayout, QLabel, QScrollArea, QSizePolicy,
+    QSpacerItem, QWidget)
 
 class Ui_age_restriction_warning(object):
     def setupUi(self, age_restriction_warning):
         if not age_restriction_warning.objectName():
             age_restriction_warning.setObjectName(u"age_restriction_warning")
-        age_restriction_warning.resize(915, 605)
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
+        age_restriction_warning.resize(460, 506)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(age_restriction_warning.sizePolicy().hasHeightForWidth())
         age_restriction_warning.setSizePolicy(sizePolicy)
-        age_restriction_warning.setMinimumSize(QSize(915, 0))
+        age_restriction_warning.setMinimumSize(QSize(0, 0))
         self.gridLayout = QGridLayout(age_restriction_warning)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label = QLabel(age_restriction_warning)
-        self.label.setObjectName(u"label")
+        self.formLayout_2 = QFormLayout()
+        self.formLayout_2.setObjectName(u"formLayout_2")
+        self.checkBox = QCheckBox(age_restriction_warning)
+        self.checkBox.setObjectName(u"checkBox")
         font = QFont()
-        font.setPointSize(14)
-        font.setBold(True)
-        self.label.setFont(font)
+        font.setBold(False)
+        self.checkBox.setFont(font)
 
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.formLayout_2.setWidget(0, QFormLayout.LabelRole, self.checkBox)
+
+        self.label_7 = QLabel(age_restriction_warning)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setWordWrap(True)
+
+        self.formLayout_2.setWidget(0, QFormLayout.FieldRole, self.label_7)
+
+
+        self.gridLayout.addLayout(self.formLayout_2, 5, 0, 1, 1)
 
         self.line = QFrame(age_restriction_warning)
         self.line.setObjectName(u"line")
@@ -48,22 +58,21 @@ class Ui_age_restriction_warning(object):
 
         self.gridLayout.addWidget(self.line, 1, 0, 1, 2)
 
+        self.line_3 = QFrame(age_restriction_warning)
+        self.line_3.setObjectName(u"line_3")
+        self.line_3.setFrameShape(QFrame.Shape.HLine)
+        self.line_3.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout.addWidget(self.line_3, 3, 0, 1, 1)
+
         self.scrollArea = QScrollArea(age_restriction_warning)
         self.scrollArea.setObjectName(u"scrollArea")
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 889, 544))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 420, 483))
         self.gridLayout_2 = QGridLayout(self.scrollAreaWidgetContents)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.checkBox = QCheckBox(self.scrollAreaWidgetContents)
-        self.checkBox.setObjectName(u"checkBox")
-        font1 = QFont()
-        font1.setBold(True)
-        self.checkBox.setFont(font1)
-
-        self.gridLayout_2.addWidget(self.checkBox, 8, 0, 1, 1)
-
         self.label_5 = QLabel(self.scrollAreaWidgetContents)
         self.label_5.setObjectName(u"label_5")
         self.label_5.setWordWrap(True)
@@ -72,33 +81,22 @@ class Ui_age_restriction_warning(object):
 
         self.label_3 = QLabel(self.scrollAreaWidgetContents)
         self.label_3.setObjectName(u"label_3")
-        font2 = QFont()
-        font2.setBold(True)
-        font2.setItalic(False)
-        self.label_3.setFont(font2)
+        font1 = QFont()
+        font1.setBold(True)
+        font1.setItalic(False)
+        self.label_3.setFont(font1)
         self.label_3.setWordWrap(True)
 
         self.gridLayout_2.addWidget(self.label_3, 1, 0, 1, 1)
 
         self.label_4 = QLabel(self.scrollAreaWidgetContents)
         self.label_4.setObjectName(u"label_4")
-        font3 = QFont()
-        font3.setPointSize(12)
-        font3.setBold(True)
-        self.label_4.setFont(font3)
+        font2 = QFont()
+        font2.setPointSize(12)
+        font2.setBold(True)
+        self.label_4.setFont(font2)
 
         self.gridLayout_2.addWidget(self.label_4, 0, 0, 1, 1)
-
-        self.line_3 = QFrame(self.scrollAreaWidgetContents)
-        self.line_3.setObjectName(u"line_3")
-        self.line_3.setFrameShape(QFrame.Shape.HLine)
-        self.line_3.setFrameShadow(QFrame.Shadow.Sunken)
-
-        self.gridLayout_2.addWidget(self.line_3, 7, 0, 1, 1)
-
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout_2.addItem(self.verticalSpacer, 6, 0, 1, 1)
 
         self.line_4 = QFrame(self.scrollAreaWidgetContents)
         self.line_4.setObjectName(u"line_4")
@@ -109,19 +107,45 @@ class Ui_age_restriction_warning(object):
 
         self.label_2 = QLabel(self.scrollAreaWidgetContents)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setFont(font1)
+        font3 = QFont()
+        font3.setBold(True)
+        self.label_2.setFont(font3)
 
         self.gridLayout_2.addWidget(self.label_2, 3, 0, 1, 1)
 
-        self.checkBox_2 = QCheckBox(self.scrollAreaWidgetContents)
-        self.checkBox_2.setObjectName(u"checkBox_2")
-        self.checkBox_2.setFont(font1)
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.gridLayout_2.addWidget(self.checkBox_2, 9, 0, 1, 1)
+        self.gridLayout_2.addItem(self.verticalSpacer_2, 5, 0, 1, 1)
 
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
         self.gridLayout.addWidget(self.scrollArea, 2, 0, 1, 1)
+
+        self.formLayout = QFormLayout()
+        self.formLayout.setObjectName(u"formLayout")
+        self.checkBox_2 = QCheckBox(age_restriction_warning)
+        self.checkBox_2.setObjectName(u"checkBox_2")
+        self.checkBox_2.setFont(font3)
+
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.checkBox_2)
+
+        self.label_6 = QLabel(age_restriction_warning)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setWordWrap(True)
+
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.label_6)
+
+
+        self.gridLayout.addLayout(self.formLayout, 8, 0, 1, 1)
+
+        self.label = QLabel(age_restriction_warning)
+        self.label.setObjectName(u"label")
+        font4 = QFont()
+        font4.setPointSize(14)
+        font4.setBold(True)
+        self.label.setFont(font4)
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
 
 
         self.retranslateUi(age_restriction_warning)
@@ -133,14 +157,16 @@ class Ui_age_restriction_warning(object):
 
     def retranslateUi(self, age_restriction_warning):
         age_restriction_warning.setWindowTitle(QCoreApplication.translate("age_restriction_warning", u"Form", None))
-        self.label.setText(QCoreApplication.translate("age_restriction_warning", u"AI Runner is for users of 18 years of age or older", None))
-        self.checkBox.setText(QCoreApplication.translate("age_restriction_warning", u"I am at least 18 years old and agree to the terms and conditions", None))
+        self.checkBox.setText("")
+        self.label_7.setText(QCoreApplication.translate("age_restriction_warning", u"I am at least 18 years old and agree to the terms and conditions", None))
         self.label_5.setText(QCoreApplication.translate("age_restriction_warning", u"This application utilizes advanced AI models capable of generating a wide range of digital images. Users are advised to exercise caution and comply with local laws when creating images. The software might produce content deemed sensitive or inappropriate, which could include illegal content in certain jurisdictions. It is the user's responsibility to ensure that all content generated complies with applicable laws and regulations.\n"
 "\n"
 "Please be aware that AI-generated content could unintentionally replicate copyrighted materials or incorporate features from copyrighted artworks. This has been a subject of legal scrutiny as seen in controversies such as those surrounding technologies like Stable Diffusion. Users must handle the outputs responsibly and are encouraged to critically assess the legality of their creations.", None))
         self.label_3.setText(QCoreApplication.translate("age_restriction_warning", u"This application may generate content that is not suitable for all audiences, including nude images. Please confirm that you are at least 18 years oldbefore proceeding.", None))
         self.label_4.setText(QCoreApplication.translate("age_restriction_warning", u"Age Restriction and User Agreement", None))
         self.label_2.setText(QCoreApplication.translate("age_restriction_warning", u"Content Warning and Legal Disclaimer", None))
-        self.checkBox_2.setText(QCoreApplication.translate("age_restriction_warning", u"I have read and understood the age restrictions, content warnings, and legal disclaimers provided above. I agree to abide by these guidelines", None))
+        self.checkBox_2.setText("")
+        self.label_6.setText(QCoreApplication.translate("age_restriction_warning", u"I have read and understood the age restrictions, content warnings, and legal disclaimers provided above. I agree to abide by these guidelines", None))
+        self.label.setText(QCoreApplication.translate("age_restriction_warning", u"AI Runner is for users of 18 years of age or older", None))
     # retranslateUi
 
