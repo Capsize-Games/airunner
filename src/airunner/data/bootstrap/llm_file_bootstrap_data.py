7623ac933c99f4f3b664a7d55f8ad469f9788d8d
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-19 05:25:26 -0600

    Fix and add new bootstrap data
    Signed-off-by: w4ffl35 <25737761+w4ffl35@users.noreply.github.com>

---
+++ src/airunner/data/bootstrap/llm_file_bootstrap_data.py
@@ -0,0 +1,14 @@
+LLM_FILE_BOOTSTRAP_DATA = {
+    "w4ffl35/Mistral-7B-Instruct-v0.2-safetensors": [
+        "config.json",
+        "generation_config.json",
+        "model-00001-of-00003.safetensors",
+        "model-00002-of-00003.safetensors",
+        "model-00003-of-00003.safetensors",
+        "model.safetensors.index.json",
+        "special_tokens_map.json",
+        "tokenizer.json",
+        "tokenizer.model",
+        "tokenizer_config.json",
+    ]
+}
