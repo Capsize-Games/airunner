6acef25cecca113bb0337ab4a484684902ac309a
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-24 03:41:49 -0600

    use facehuggershield

--- src/airunner/main.py
+++ src/airunner/main.py
@@ -5,28 +5,13 @@ Do not remove the no_internet_socket import.
 Do not change the order of the imports.
 ----------------------------------------------------------------
 """
-from nullscream import install_nullscream
-install_nullscream(blacklist=["huggingface_hub"])
+import facehuggershield.huggingface
 
-################################################################
-# Importing this module ensures that the internet is completely
-# disabled for the AI Runner application.
-################################################################
-from lockdown.network import no_internet_socket
-
-################################################################
-# Importing this restricts access to potentially dangerous OS
-# operations, such as system calls and subprocess invocations.
-################################################################
-from lockdown.os.restrict_os_access import RestrictOSAccess
-restrict_os_access = RestrictOSAccess()
-restrict_os_access.install()
 
 ################################################################
 # Import the main application class for AI Runner.
 ################################################################
 from airunner.app import App
-
 
 if __name__ == "__main__":
     App(restrict_os_access=restrict_os_access)
