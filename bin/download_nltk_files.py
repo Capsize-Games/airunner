cb2a1af0a15638b464422a8f9e0fe20a72e6426d
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-05-10 10:54:38 -0600

    Removes nltk download script this was moved to the installer

--- bin/download_nltk_files.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import nltk
-
-from airunner.settings import NLTK_DOWNLOAD_DIR
-
-nltk.data.path.append(NLTK_DOWNLOAD_DIR)
-
-nltk.download("stopwords", download_dir=NLTK_DOWNLOAD_DIR, quiet=True, halt_on_error=False, raise_on_error=False)
-nltk.download("punkt", download_dir=NLTK_DOWNLOAD_DIR, quiet=True, halt_on_error=False, raise_on_error=False)
