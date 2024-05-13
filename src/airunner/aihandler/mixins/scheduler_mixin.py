14a8893f5c2db8078dce1d0ca7935bb4326cd444
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-05-13 17:53:40 -0600

    Stable Diffusion loading improvements

--- src/airunner/aihandler/mixins/scheduler_mixin.py
+++ src/airunner/aihandler/mixins/scheduler_mixin.py
@@ -20,10 +20,20 @@ class SchedulerMixin:
         self.current_scheduler_name: str = ""
         self.do_change_scheduler: bool = False
         self.scheduler = None
+        self.register(SignalCode.SCHEDULER_LOAD_SIGNAL, self.on_scheduler_load_signal)
+        self.register(SignalCode.SCHEDULER_UNLOAD_SIGNAL, self.on_scheduler_unload_signal)
+        self.register(SignalCode.SCHEDULER_LOAD_SIGNAL, self.on_scheduler_load_signal)
+        self.register(SignalCode.SCHEDULER_UNLOAD_SIGNAL, self.on_scheduler_unload_signal)
 
     @property
     def scheduler_section(self):
         return self.sd_request.generator_settings.section
+
+    def on_scheduler_load_signal(self, _data: dict = None):
+        self.load_scheduler()
+
+    def on_scheduler_unload_signal(self, _data: dict = None):
+        self.unload_scheduler()
 
     def clear_scheduler(self):
         self.logger.debug("Clearing scheduler")
@@ -32,95 +42,39 @@ class SchedulerMixin:
         self.do_change_scheduler = True
         self.scheduler = None
 
+    @property
+    def __scheduler_path(self) -> str:
+        return os.path.expanduser(
+            os.path.join(
+                self.settings["path_settings"]["feature_extractor_model_path"],
+                f"openai/clip-vit-large-patch14/preprocessor_config.json"
+            )
+        )
+
     def load_scheduler(self, force_scheduler_name=None, config=None):
-        self.logger.info(f"load_scheduler called with {force_scheduler_name}")
-        if self.is_sd_xl_turbo:
-            self.change_model_status(ModelType.SCHEDULER, ModelStatus.UNLOADED, "")
-            return None
-
-        if (
-            not force_scheduler_name and
-            self.scheduler and
-            not self.do_change_scheduler and
-            self.settings["generator_settings"]["scheduler"] == self.sd_request.generator_settings.scheduler
-        ):
-            self.logger.info(f"Scheduler already loaded for {self.sd_request.generator_settings.scheduler}")
-            return self.scheduler
-
-        self.current_scheduler_name = force_scheduler_name if force_scheduler_name else self.sd_request.generator_settings.scheduler
-        self.logger.debug("Loading scheduler")
-
-        scheduler_name = force_scheduler_name if force_scheduler_name else self.scheduler_name
-
-        if not force_scheduler_name and scheduler_name not in AVAILABLE_SCHEDULERS_BY_ACTION[self.scheduler_section]:
-            scheduler_name = AVAILABLE_SCHEDULERS_BY_ACTION[self.scheduler_section][0]
-
-        self.change_model_status(ModelType.SCHEDULER, ModelStatus.LOADING, scheduler_name)
-
-        scheduler_class_name = self.schedulers[scheduler_name]
-        scheduler_class = getattr(diffusers, scheduler_class_name)
-
+        scheduler_name = force_scheduler_name if force_scheduler_name else self.settings["generator_settings"]["scheduler"]
         kwargs = {
             "subfolder": "scheduler",
             "local_files_only": True,
         }
-
-        if self.current_model_branch:
-            kwargs["variant"] = self.current_model_branch
-
-        if config:
-            config = dict(config)
-            if scheduler_name == Scheduler.DPM_PP_2M_K.value:
-                config["use_karras_sigmas"] = True
-
-            algorithm_type = config.get("algorithm_type", None)
-            if scheduler_name == Scheduler.DPM_PP_2M_SDE_K.value:
-                algorithm_type = SchedulerAlgorithm.SDE_DPM_SOLVER_PLUS_PLUS
-            elif scheduler_name == Scheduler.DPM_2M_SDE_K.value:
-                algorithm_type = SchedulerAlgorithm.SDE_DPM_SOLVER
-            elif scheduler_name.startswith("DPM"):
-                if scheduler_name.find("++") != -1:
-                    algorithm_type = SchedulerAlgorithm.DPM_SOLVER_PLUS_PLUS
-                else:
-                    algorithm_type = SchedulerAlgorithm.DPM_SOLVER
-            if algorithm_type is not None:
-                config["algorithm_type"] = algorithm_type.value
-
-            self.scheduler = scheduler_class.from_config(config)
-        else:
-            if scheduler_name == Scheduler.DPM_PP_2M_K.value:
-                kwargs["use_karras_sigmas"] = True
-            if scheduler_name.startswith("DPM"):
-                if scheduler_name.find("++") != -1:
-                    algorithm_type = SchedulerAlgorithm.DPM_SOLVER_PLUS_PLUS
-                else:
-                    algorithm_type = SchedulerAlgorithm.DPM_SOLVER
-                kwargs["algorithm_type"] = algorithm_type.value
-
-            try:
-                self.logger.debug(
-                    f"Loading scheduler `{scheduler_name}` "
-                    f"from pretrained path `{self.model_path}`"
-                )
-                self.scheduler = scheduler_class.from_pretrained(
-                    os.path.expanduser(
-                        os.path.join(
-                            self.settings["path_settings"]["feature_extractor_model_path"],
-                            f"{self.feature_extractor_path}/preprocessor_config.json"
-                        )
-                    ),
-                    **kwargs
-                )
-                self.change_model_status(ModelType.SCHEDULER, ModelStatus.LOADED, scheduler_name)
-            except Exception as e:
-                self.change_model_status(ModelType.SCHEDULER, ModelStatus.FAILED, scheduler_name)
-                self.logger.error(
-                    f"Unable to load scheduler {scheduler_name} "
-                    f"from {self.sd_request.generator_settings.model}"
-                )
-                self.logger.error(e)
-
-        return self.scheduler
+        for scheduler in self.settings["schedulers"]:
+            if scheduler["display_name"] == scheduler_name:
+                scheduler_name = Scheduler[scheduler["name"]]
+                scheduler_class_name = SCHEDULER_CLASSES[scheduler_name]
+                scheduler_class = getattr(diffusers, scheduler_class_name)
+                try:
+                    self.change_model_status(ModelType.SCHEDULER, ModelStatus.LOADING, self.__scheduler_path)
+                    self.scheduler = scheduler_class.from_pretrained(
+                        self.__scheduler_path,
+                        **kwargs
+                    )
+                    self.change_model_status(ModelType.SCHEDULER, ModelStatus.READY, self.__scheduler_path)
+                    self.current_scheduler_name = scheduler_name
+                    self.logger.debug(f"Loaded scheduler {scheduler_name}")
+                except Exception as e:
+                    self.logger.error(f"Failed to load scheduler {scheduler_name}: {e}")
+                    self.change_model_status(ModelType.SCHEDULER, ModelStatus.ERROR, self.__scheduler_path)
+                return scheduler
 
     def unload_scheduler(self):
         self.logger.debug("Unloading scheduler")
@@ -149,3 +103,13 @@ class SchedulerMixin:
             self.do_change_scheduler = True
         else:
             self.do_change_scheduler = False
+
+    def apply_scheduler_to_pipe(self):
+        if self.pipe and self.pipe.scheduler:
+            self.pipe.scheduler = self.scheduler
+            self.change_model_status(ModelType.SCHEDULER, ModelStatus.LOADED, self.__scheduler_path)
+
+    def remove_scheduler_from_pipe(self):
+        if self.pipe and self.pipe.scheduler:
+            self.pipe.scheduler = None
+            self.change_model_status(ModelType.SCHEDULER, ModelStatus.READY, self.__scheduler_path)
