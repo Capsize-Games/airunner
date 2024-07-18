d63281dd479d706e0bf3e8084118cd934b939b03
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-07-18 10:14:17 -0600

    Fixes embeddings path

--- src/airunner/windows/main/embedding_mixin.py
+++ src/airunner/windows/main/embedding_mixin.py
@@ -44,6 +44,7 @@ class EmbeddingMixin:
     def get_embeddings(self, message: dict = None):
         name_filter = message.get("name_filter") if message is not None else ""
         embeddings = []
+
         for embedding in self.settings["embeddings"]:
             if name_filter == "":
                 embeddings.append(embedding)
@@ -73,7 +74,7 @@ class EmbeddingMixin:
                 return
 
     def scan_for_embeddings(self, _message: dict):
-        embeddings_path = self.settings["path_settings"]["embeddings_model_path"]
+        embeddings_path = os.path.expanduser(self.settings["path_settings"]["embeddings_model_path"])
         if os.path.exists(embeddings_path):
             for root, dirs, _ in os.walk(embeddings_path):
                 for directory in dirs:
