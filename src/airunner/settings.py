a343ef423a90a2586bb7cdc1bb118aeae51a5ab6
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-08-24 18:02:38 -0600

    fixes agent handling, tts issues

--- src/airunner/settings.py
+++ src/airunner/settings.py
@@ -80,7 +80,7 @@ PROMPT_FOR_ONLINE_ACCESS = True
 # These logs are not stored and are used for development
 # purposes only.
 ####################################################################
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.ERROR
 
 ####################################################################
 # Default models for the core application
