8c2a9de69313d6f228a9e3831da664d93908721d
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-08-25 10:12:56 -0600

    move to facehuggershield v0.1.11

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
         "tomesd==0.1.3",
 
         # Security
-        "facehuggershield==0.1.10",
+        "facehuggershield==0.1.11",
         
         # Llama index
         "llama-index",
