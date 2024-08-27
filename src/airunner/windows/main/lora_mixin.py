881fa8a75c5c5277927bd6b67e6910dc051532b5
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-08-27 09:39:47 -0600

    Fixes #824 SDXL Lora
    improves lora scanning
    makes lora scanning work with new directory structure

--- src/airunner/windows/main/lora_mixin.py
+++ src/airunner/windows/main/lora_mixin.py
@@ -2,34 +2,16 @@ from airunner.enums import SignalCode
 
 
 class LoraMixin:
-    def on_add_lora_signal(self, params: dict):
+    def __init__(self):
         settings = self.settings
-        name = params["name"]
-        path = params["path"]
-        # ensure we have a unique name and path combo
-        for index, lora in enumerate(settings["lora"]):
-            if not lora:
-                del settings["lora"][index]
-                continue
-            if lora["name"] == name and lora["path"] == path:
-                return
-        lora = {
-            'name': params.get("name", ""),
-            'path': params.get("path", ""),
-            'scale': params.get("scale", 1),
-            'enabled': params.get("enabled", True),
-            'loaded': params.get("loaded", False),
-            'trigger_word': params.get("trigger_word", ""),
-            'version': params.get("version", "SD 1.5"),
-        }
-        settings["lora"].append(lora)
+        settings["lora"] = scan_path_for_lora(settings["lora"])
         self.settings = settings
-        return lora
 
     def on_update_lora_signal(self, lora: dict):
         settings = self.settings
-        for index, _lora in enumerate(self.settings["lora"]):
-            if _lora["name"] == lora["name"] and _lora["path"] == lora["path"]:
-                settings["lora"][index] = lora
-                self.settings = settings
-                return
+        for version, current_lora in self.settings["lora"].items():
+            for index, _lora in enumerate(current_lora):
+                if _lora["name"] == lora["name"] and _lora["path"] == lora["path"]:
+                    settings["lora"][version][index] = lora
+                    self.settings = settings
+                    return
