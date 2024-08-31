e2706f36d91473fd8e0c82bb249052420cd239d4
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-08-30 18:12:15 -0600

    sd request improvement and fixes for compel

--- src/airunner/aihandler/mixins/compel_mixin.py
+++ src/airunner/aihandler/mixins/compel_mixin.py
@@ -62,6 +62,7 @@ class CompelMixin:
                 self.compel_proc = Compel(
                     **parameters
                 )
+                self.compel_proc.to(self.device)
         except Exception as e:
             self.logger.error(f"Error creating compel proc: {e}")
             return None
