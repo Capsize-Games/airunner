e411ee34f6a4b9c00e6f2d1ae8a950825f73c7ca
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-08-31 16:38:24 -0600

    toolbar design tweaks

--- src/airunner/widgets/controlnet/templates/controlnet_settings_widget_ui.py
+++ src/airunner/widgets/controlnet/templates/controlnet_settings_widget_ui.py
@@ -16,8 +16,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QGridLayout,
-    QLabel, QScrollArea, QSizePolicy, QSpacerItem,
-    QVBoxLayout, QWidget)
+    QLabel, QSizePolicy, QSpacerItem, QVBoxLayout,
+    QWidget)
 
 from airunner.widgets.slider.slider_widget import SliderWidget
 
@@ -28,8 +28,32 @@ class Ui_controlnet_settings_widget(object):
         controlnet_settings_widget.resize(1004, 622)
         self.gridLayout_2 = QGridLayout(controlnet_settings_widget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.gridLayout_2.setHorizontalSpacing(0)
         self.gridLayout_2.setVerticalSpacing(10)
         self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.widget_2 = QWidget(controlnet_settings_widget)
+        self.widget_2.setObjectName(u"widget_2")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget_2.sizePolicy().hasHeightForWidth())
+        self.widget_2.setSizePolicy(sizePolicy)
+        self.widget_2.setMinimumSize(QSize(0, 37))
+        self.gridLayout_3 = QGridLayout(self.widget_2)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.gridLayout_3.setContentsMargins(10, 10, 10, 0)
+        self.label = QLabel(self.widget_2)
+        self.label.setObjectName(u"label")
+        font = QFont()
+        font.setPointSize(11)
+        font.setBold(True)
+        self.label.setFont(font)
+
+        self.gridLayout_3.addWidget(self.label, 0, 0, 1, 1)
+
+
+        self.gridLayout_2.addWidget(self.widget_2, 0, 0, 1, 1)
+
         self.line = QFrame(controlnet_settings_widget)
         self.line.setObjectName(u"line")
         self.line.setFrameShape(QFrame.Shape.HLine)
@@ -37,17 +61,19 @@ class Ui_controlnet_settings_widget(object):
 
         self.gridLayout_2.addWidget(self.line, 1, 0, 1, 1)
 
-        self.scrollArea = QScrollArea(controlnet_settings_widget)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 1002, 560))
-        self.gridLayout = QGridLayout(self.scrollAreaWidgetContents)
+        self.widget_3 = QWidget(controlnet_settings_widget)
+        self.widget_3.setObjectName(u"widget_3")
+        self.gridLayout = QGridLayout(self.widget_3)
         self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setHorizontalSpacing(0)
         self.gridLayout.setVerticalSpacing(10)
         self.gridLayout.setContentsMargins(10, 0, 10, 0)
-        self.widget = QWidget(self.scrollAreaWidgetContents)
+        self.controlnet = QComboBox(self.widget_3)
+        self.controlnet.setObjectName(u"controlnet")
+
+        self.gridLayout.addWidget(self.controlnet, 0, 0, 1, 1)
+
+        self.widget = QWidget(self.widget_3)
         self.widget.setObjectName(u"widget")
         self.widget.setMinimumSize(QSize(0, 0))
         self.verticalLayout = QVBoxLayout(self.widget)
@@ -88,41 +114,12 @@ class Ui_controlnet_settings_widget(object):
 
         self.gridLayout.addWidget(self.widget, 1, 0, 1, 1)
 
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        self.verticalSpacer = QSpacerItem(20, 479, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.gridLayout.addItem(self.verticalSpacer, 3, 0, 1, 1)
-
-        self.controlnet = QComboBox(self.scrollAreaWidgetContents)
-        self.controlnet.setObjectName(u"controlnet")
-
-        self.gridLayout.addWidget(self.controlnet, 0, 0, 1, 1)
-
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-
-        self.gridLayout_2.addWidget(self.scrollArea, 2, 0, 1, 1)
-
-        self.widget_2 = QWidget(controlnet_settings_widget)
-        self.widget_2.setObjectName(u"widget_2")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.widget_2.sizePolicy().hasHeightForWidth())
-        self.widget_2.setSizePolicy(sizePolicy)
-        self.widget_2.setMinimumSize(QSize(0, 37))
-        self.gridLayout_3 = QGridLayout(self.widget_2)
-        self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.gridLayout_3.setContentsMargins(10, 10, 10, 0)
-        self.label = QLabel(self.widget_2)
-        self.label.setObjectName(u"label")
-        font = QFont()
-        font.setPointSize(11)
-        font.setBold(True)
-        self.label.setFont(font)
-
-        self.gridLayout_3.addWidget(self.label, 0, 0, 1, 1)
+        self.gridLayout.addItem(self.verticalSpacer, 2, 0, 1, 1)
 
 
-        self.gridLayout_2.addWidget(self.widget_2, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.widget_3, 2, 0, 1, 1)
 
 
         self.retranslateUi(controlnet_settings_widget)
@@ -133,8 +130,8 @@ class Ui_controlnet_settings_widget(object):
 
     def retranslateUi(self, controlnet_settings_widget):
         controlnet_settings_widget.setWindowTitle(QCoreApplication.translate("controlnet_settings_widget", u"Form", None))
+        self.label.setText(QCoreApplication.translate("controlnet_settings_widget", u"Controlnet", None))
         self.controlnet_conditioning_scale.setProperty("label_text", QCoreApplication.translate("controlnet_settings_widget", u"Conditioning Scale", None))
         self.strength_slider.setProperty("label_text", QCoreApplication.translate("controlnet_settings_widget", u"Strength", None))
-        self.label.setText(QCoreApplication.translate("controlnet_settings_widget", u"Controlnet", None))
     # retranslateUi
 
