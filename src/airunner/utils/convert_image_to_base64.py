51e4f1b0667febe3c552af885c1215b6d4d57b8c
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-09-02 11:01:04 -0600

    Modify error message for easier debugging

--- src/airunner/utils/convert_image_to_base64.py
+++ src/airunner/utils/convert_image_to_base64.py
@@ -10,7 +10,7 @@ def convert_image_to_base64(image: Image) -> str:
         image.save(img_byte_arr, format='PNG')
     except AttributeError as e:
         logger = Logger(prefix="convert_image_to_base64")
-        logger.error("Something went wrong with image conversion to base64")
+        logger.error(f"Something went wrong with image conversion to base64: {e}")
         return ""
     img_byte_arr = img_byte_arr.getvalue()
     image_base64 = base64.encodebytes(img_byte_arr).decode('ascii')
