ca1c1b54fcba292928ecbda85c9f12e3630953de
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-09-08 19:25:15 -0600

    Fixes filters

--- src/airunner/filters/saturation.py
+++ src/airunner/filters/saturation.py
@@ -3,11 +3,9 @@ from airunner.filters.base_filter import BaseFilter
 
 
 class SaturationFilter(BaseFilter):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.factor = None
-
     def apply_filter(self, image, do_reset):
-        # limit self.factor to 2 decimal places
-        self.factor = round(self.factor, 2)
-        return ImageEnhance.Color(image).enhance(self.factor)
+        # Transform self.factor from [0, 100] to [-1, 2]
+        factor = (self.factor - 50) / 25 + 1
+        # Limit self.factor to 2 decimal places
+        factor = round(factor, 2)
+        return ImageEnhance.Color(image).enhance(factor)
