67f280f847faab689e0bb44a028a90ca72d66eea
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-09-12 17:22:12 -0600

    remove print statement

--- src/airunner/utils/open_file_path.py
+++ src/airunner/utils/open_file_path.py
@@ -1,9 +1,6 @@
 from PySide6.QtWidgets import QFileDialog
 
-
-def open_file_path(label="Import Image", directory="", file_type="Image Files (*.png *.jpg *.jpeg)"):
+def open_file_path(parent=None, label="Import Image", directory="", file_type="Image Files (*.png *.jpg *.jpeg)"):
     return QFileDialog.getOpenFileName(
-        None, label, directory, file_type
+        parent, label, directory, file_type
     )
-
-
