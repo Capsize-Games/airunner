bb7c7ab7133ffa9f3b29986e09ec57e01739f835
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-09-19 12:11:41 -0600

    fix llm history loading

--- src/airunner/widgets/llm/llm_history_widget.py
+++ src/airunner/widgets/llm/llm_history_widget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QVBoxLayout, QPushButton, QSpacerItem, QSizePolicy, QHBoxLayout
+from PySide6.QtWidgets import QVBoxLayout, QPushButton, QSpacerItem, QSizePolicy, QHBoxLayout, QWidget
 
 from airunner.enums import SignalCode
 from airunner.widgets.base_widget import BaseWidget
@@ -11,6 +11,7 @@ class LLMHistoryWidget(BaseWidget):
     def __init__(self, *args, **kwargs):
         super(LLMHistoryWidget, self).__init__(*args, **kwargs)
         self.database_handler = AgentDatabaseHandler()
+        self.spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
     def showEvent(self, event):
         super(LLMHistoryWidget, self).showEvent(event)
@@ -18,7 +19,26 @@ class LLMHistoryWidget(BaseWidget):
 
     def load_conversations(self):
         conversations = self.database_handler.get_all_conversations()
-        layout = QVBoxLayout()
+        layout = self.ui.gridLayout_2
+
+        if layout is None:
+            self.logger.error("Layout is None")
+
+        # clear all widgets from the layout
+        try:
+            layout.removeItem(self.spacer)
+        except:
+            pass
+
+        if layout:
+            for i in reversed(range(layout.count())):
+                widget = layout.itemAt(i).widget()
+                if widget:
+                    widget.setParent(None)
+
+        if layout is None:
+            layout = QVBoxLayout(self.ui.scrollAreaWidgetContents)
+            self.ui.scrollAreaWidgetContents.setLayout(layout)
 
         for conversation in conversations:
             h_layout = QHBoxLayout()
@@ -28,11 +48,13 @@ class LLMHistoryWidget(BaseWidget):
             delete_button.clicked.connect(lambda _, widget=h_layout, c=conversation: self.on_delete_conversation(widget, c))
             h_layout.addWidget(button)
             h_layout.addWidget(delete_button)
-            layout.addLayout(h_layout)
+
+            container_widget = QWidget()
+            container_widget.setLayout(h_layout)
+            layout.addWidget(container_widget)
 
         # Add a vertical spacer at the end
-        spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-        layout.addItem(spacer)
+        layout.addItem(self.spacer)
 
         self.ui.conversations_scroll_area.setLayout(layout)
 
