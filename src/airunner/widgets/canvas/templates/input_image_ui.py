c9edae8f3c22ae1d90b19b72b4f79eec1a35b4b9
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-09-29 20:14:22 -0600

    adds scroll area to input image so that tool tab collapses properly

--- src/airunner/widgets/canvas/templates/input_image_ui.py
+++ src/airunner/widgets/canvas/templates/input_image_ui.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QFrame, QGraphicsView,
     QGridLayout, QHBoxLayout, QLabel, QPushButton,
-    QSizePolicy, QSpacerItem, QWidget)
+    QScrollArea, QSizePolicy, QSpacerItem, QWidget)
 
 from airunner.widgets.controlnet.controlnet_settings_widget import ControlnetSettingsWidget
 from airunner.widgets.slider.slider_widget import SliderWidget
@@ -26,20 +26,39 @@ class Ui_input_image(object):
     def setupUi(self, input_image):
         if not input_image.objectName():
             input_image.setObjectName(u"input_image")
-        input_image.resize(656, 1097)
+        input_image.resize(605, 920)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(input_image.sizePolicy().hasHeightForWidth())
         input_image.setSizePolicy(sizePolicy)
         self.gridLayout = QGridLayout(input_image)
+        self.gridLayout.setSpacing(0)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.controlnet_settings = ControlnetSettingsWidget(input_image)
-        self.controlnet_settings.setObjectName(u"controlnet_settings")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.scrollArea = QScrollArea(input_image)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 603, 918))
+        self.gridLayout_2 = QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.gridLayout_2.setHorizontalSpacing(0)
+        self.gridLayout_2.setVerticalSpacing(10)
+        self.gridLayout_2.setContentsMargins(10, 10, 10, 10)
+        self.line = QFrame(self.scrollAreaWidgetContents)
+        self.line.setObjectName(u"line")
+        self.line.setFrameShape(QFrame.Shape.HLine)
+        self.line.setFrameShadow(QFrame.Shadow.Sunken)
 
-        self.gridLayout.addWidget(self.controlnet_settings, 4, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.line, 1, 0, 1, 1)
 
-        self.strength_slider_widget = SliderWidget(input_image)
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout_2.addItem(self.verticalSpacer, 3, 0, 1, 1)
+
+        self.strength_slider_widget = SliderWidget(self.scrollAreaWidgetContents)
         self.strength_slider_widget.setObjectName(u"strength_slider_widget")
         sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
         sizePolicy1.setHorizontalStretch(0)
@@ -53,20 +72,20 @@ class Ui_input_image(object):
         self.strength_slider_widget.setProperty("spinbox_single_step", 0.010000000000000)
         self.strength_slider_widget.setProperty("spinbox_page_step", 0.100000000000000)
 
-        self.gridLayout.addWidget(self.strength_slider_widget, 5, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.strength_slider_widget, 6, 0, 1, 1)
 
-        self.image_container = QGraphicsView(input_image)
+        self.image_container = QGraphicsView(self.scrollAreaWidgetContents)
         self.image_container.setObjectName(u"image_container")
         sizePolicy.setHeightForWidth(self.image_container.sizePolicy().hasHeightForWidth())
         self.image_container.setSizePolicy(sizePolicy)
         self.image_container.setMinimumSize(QSize(512, 512))
 
-        self.gridLayout.addWidget(self.image_container, 2, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.image_container, 2, 0, 1, 1)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setSpacing(10)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.use_grid_image_as_input_checkbox = QCheckBox(input_image)
+        self.use_grid_image_as_input_checkbox = QCheckBox(self.scrollAreaWidgetContents)
         self.use_grid_image_as_input_checkbox.setObjectName(u"use_grid_image_as_input_checkbox")
         sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy2.setHorizontalStretch(0)
@@ -76,21 +95,21 @@ class Ui_input_image(object):
 
         self.horizontalLayout.addWidget(self.use_grid_image_as_input_checkbox)
 
-        self.enable_checkbox = QCheckBox(input_image)
+        self.enable_checkbox = QCheckBox(self.scrollAreaWidgetContents)
         self.enable_checkbox.setObjectName(u"enable_checkbox")
         sizePolicy2.setHeightForWidth(self.enable_checkbox.sizePolicy().hasHeightForWidth())
         self.enable_checkbox.setSizePolicy(sizePolicy2)
 
         self.horizontalLayout.addWidget(self.enable_checkbox)
 
-        self.import_button = QPushButton(input_image)
+        self.import_button = QPushButton(self.scrollAreaWidgetContents)
         self.import_button.setObjectName(u"import_button")
         icon = QIcon(QIcon.fromTheme(u"folder"))
         self.import_button.setIcon(icon)
 
         self.horizontalLayout.addWidget(self.import_button)
 
-        self.delete_button = QPushButton(input_image)
+        self.delete_button = QPushButton(self.scrollAreaWidgetContents)
         self.delete_button.setObjectName(u"delete_button")
         icon1 = QIcon(QIcon.fromTheme(u"user-trash"))
         self.delete_button.setIcon(icon1)
@@ -98,23 +117,21 @@ class Ui_input_image(object):
         self.horizontalLayout.addWidget(self.delete_button)
 
 
-        self.gridLayout.addLayout(self.horizontalLayout, 6, 0, 1, 1)
+        self.gridLayout_2.addLayout(self.horizontalLayout, 4, 0, 1, 1)
 
-        self.line = QFrame(input_image)
-        self.line.setObjectName(u"line")
-        self.line.setFrameShape(QFrame.Shape.HLine)
-        self.line.setFrameShadow(QFrame.Shadow.Sunken)
-
-        self.gridLayout.addWidget(self.line, 1, 0, 1, 1)
-
-        self.label = QLabel(input_image)
+        self.label = QLabel(self.scrollAreaWidgetContents)
         self.label.setObjectName(u"label")
 
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
 
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        self.controlnet_settings = ControlnetSettingsWidget(self.scrollAreaWidgetContents)
+        self.controlnet_settings.setObjectName(u"controlnet_settings")
 
-        self.gridLayout.addItem(self.verticalSpacer, 3, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.controlnet_settings, 5, 0, 1, 1)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.gridLayout.addWidget(self.scrollArea, 0, 0, 1, 1)
 
 
         self.retranslateUi(input_image)
