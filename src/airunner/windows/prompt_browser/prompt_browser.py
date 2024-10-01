fb47454d3bfc2935546102f47b683a7a37e262bc
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-10-01 07:51:43 -0600

    fixes prompt browser widget functionality

--- src/airunner/windows/prompt_browser/prompt_browser.py
+++ src/airunner/windows/prompt_browser/prompt_browser.py
@@ -7,6 +7,8 @@ class PromptBrowser(BaseWindow):
     template_class_ = Ui_prompt_browser
 
     def initialize_window(self):
+        self.setWindowTitle("Prompt Browser")
+
         for saved_prompt in self.saved_prompts:
             widget = PromptWidget(saved_prompt=saved_prompt)
             self.ui.scrollAreaWidgetContents.layout().addWidget(widget)
