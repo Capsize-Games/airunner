1dc53e5a96e7e2698c8864879b586eb1d8bca077
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-10-03 07:44:38 -0600

    fixes #893 improves STT loading simplifies worker manager

--- src/airunner/worker_manager.py
+++ src/airunner/worker_manager.py
@@ -1,19 +1,10 @@
-import numpy as np
 from PySide6.QtCore import QObject, Signal
 
-from airunner.enums import SignalCode, EngineResponseCode, WorkerType
+from airunner.enums import WorkerType
 from airunner.mediator_mixin import MediatorMixin
 from airunner.windows.main.settings_mixin import SettingsMixin
 from airunner.aihandler.logger import Logger
 from airunner.utils.create_worker import create_worker
-
-
-
-class Message:
-    def __init__(self, *args, **kwargs):
-        self.name = kwargs.get("name")
-        self.message = kwargs.get("message")
-        self.conversation = kwargs.get("conversation")
 
 
 class WorkerManager(QObject, MediatorMixin, SettingsMixin):
@@ -31,47 +22,19 @@ class WorkerManager(QObject, MediatorMixin, SettingsMixin):
         disable_llm: bool = False,
         disable_tts: bool = False,
         disable_stt: bool = False,
-        agent_options: dict = None,
-        **kwargs
+        agent_options: dict = None
     ):
         MediatorMixin.__init__(self)
         SettingsMixin.__init__(self)
         super().__init__()
-        self.llm_loaded: bool = False
-        self.sd_loaded: bool = False
-        self.message = ""
-        self.current_message = ""
-        self.do_process_queue = None
-        self.do_process_queue = None
         self.logger = Logger(prefix=self.__class__.__name__)
-        self.is_capturing_image = False
-        signals = [
-            (SignalCode.STT_HEAR_SIGNAL, self.on_hear_signal),
-            (SignalCode.ENGINE_STOP_PROCESSING_QUEUE_SIGNAL, self.on_engine_stop_processing_queue_signal),
-            (SignalCode.ENGINE_START_PROCESSING_QUEUE_SIGNAL, self.on_engine_start_processing_queue_signal),
-            (SignalCode.LOG_STATUS_SIGNAL, self.on_status_signal),
-            (SignalCode.AUDIO_CAPTURE_WORKER_RESPONSE_SIGNAL, self.on_AudioCaptureWorker_response_signal),
-
-            (SignalCode.STT_LOAD_SIGNAL, self.on_stt_load_signal),
-            (SignalCode.STT_UNLOAD_SIGNAL, self.on_stt_unload_signal),
-            (SignalCode.STT_START_CAPTURE_SIGNAL, self.on_stt_start_capture_signal),
-            (SignalCode.STT_STOP_CAPTURE_SIGNAL, self.on_stt_stop_capture_signal),
-            (SignalCode.AUDIO_CAPTURE_WORKER_RESPONSE_SIGNAL, self.on_stt_process_audio_signal),
-
-
-            (SignalCode.LLM_TEXT_STREAMED_SIGNAL, self.on_llm_text_streamed_signal),
-        ]
-        for signal in signals:
-            self.register(signal[0], signal[1])
-
-        self.sd_worker = None
-        self.sd_state = None
-        self.llm_request_worker = None
+        self._sd_worker = None
+        self._llm_request_worker = None
         self._llm_generate_worker = None
-        self.tts_generator_worker = None
-        self.tts_vocalizer_worker = None
-        self.stt_audio_capture_worker = None
-        self.stt_audio_processor_worker = None
+        self._tts_generator_worker = None
+        self._tts_vocalizer_worker = None
+        self._stt_audio_capture_worker = None
+        self._stt_audio_processor_worker = None
 
         self.agent_options = agent_options
 
@@ -87,88 +50,16 @@ class WorkerManager(QObject, MediatorMixin, SettingsMixin):
         if not disable_stt:
             self.register_stt_workers()
 
-    def on_llm_text_streamed_signal(self, data: dict):
-        try:
-            if self.application_settings.tts_enabled:
-                self.do_tts_request(data["message"], data["is_end_of_message"])
-        except TypeError as e:
-            self.logger.error(f"Error in on_llm_text_streamed_signal: {e}")
-        self.emit_signal(SignalCode.APPLICATION_ADD_BOT_MESSAGE_TO_CONVERSATION, data)
-
     def register_sd_workers(self):
-        from airunner.workers.sd_worker import SDWorker
-        self.sd_worker = SDWorker()
+        self._sd_worker = create_worker(WorkerType.SDWorker)
 
     def register_llm_workers(self, agent_options):
-        self.llm_generate_worker = create_worker(
-            WorkerType.LLMGenerateWorker,
-            agent_options=agent_options
-        )
+        self._llm_generate_worker = create_worker(WorkerType.LLMGenerateWorker, agent_options=agent_options)
 
     def register_tts_workers(self):
-        self.tts_generator_worker = create_worker(WorkerType.TTSGeneratorWorker)
-        self.tts_vocalizer_worker = create_worker(WorkerType.TTSVocalizerWorker)
+        self._tts_generator_worker = create_worker(WorkerType.TTSGeneratorWorker)
+        self._tts_vocalizer_worker = create_worker(WorkerType.TTSVocalizerWorker)
 
     def register_stt_workers(self):
-        self.stt_audio_capture_worker = create_worker(WorkerType.AudioCaptureWorker)
-        self.stt_audio_processor_worker = create_worker(WorkerType.AudioProcessorWorker)
-
-    def on_stt_load_signal(self):
-        self.stt_audio_processor_worker.on_load_signal()
-        self.emit_signal(SignalCode.STT_START_CAPTURE_SIGNAL)
-
-    def on_stt_unload_signal(self):
-        self.emit_signal(SignalCode.STT_STOP_CAPTURE_SIGNAL)
-        self.stt_audio_processor_worker.on_unload_signal()
-
-    def on_stt_start_capture_signal(self):
-        self.stt_audio_capture_worker.on_start_capture_signal()
-
-    def on_stt_stop_capture_signal(self):
-        self.stt_audio_capture_worker.on_stop_capture_signal()
-
-    def on_stt_process_audio_signal(self, message):
-        self.stt_audio_processor_worker.add_to_queue(message)
-
-    def handle_error(self, error_message):
-        print(f"Error: {error_message}")
-
-    def do_response(self, response):
-        """
-        Handle a response from the application by putting it into
-        a response worker queue.
-        """
-        self.emit_signal(SignalCode.ENGINE_RESPONSE_WORKER_RESPONSE_SIGNAL, {
-            'code': EngineResponseCode.IMAGE_GENERATED,
-            'message': response
-        })
-
-    def on_engine_stop_processing_queue_signal(self):
-        self.do_process_queue = False
-
-    def on_engine_start_processing_queue_signal(self):
-        self.do_process_queue = True
-
-    def on_hear_signal(self, message):
-        """
-        This is a slot function for the hear_signal.
-        The hear signal is triggered from the speech_to_text.listen function.
-        """
-        print("HEARD", message)
-
-    def on_AudioCaptureWorker_response_signal(self, message: dict):
-        item: np.ndarray = message["item"]
-        self.logger.debug("Heard signal")
-        self.stt_audio_capture_worker.add_to_queue(item)
-
-    def on_status_signal(self, message: dict):
-        self.logger.debug(message)
-
-    def on_sd_image_generated_signal(self, message):
-        self.emit_signal(SignalCode.SD_IMAGE_GENERATED_SIGNAL, message)
-
-    def unload_stablediffusion(self):
-        """
-        Unload the Stable Diffusion model from memory.
-        """
-        self.emit_signal(SignalCode.SD_UNLOAD_SIGNAL)
+        self._stt_audio_capture_worker = create_worker(WorkerType.AudioCaptureWorker)
+        self._stt_audio_processor_worker = create_worker(WorkerType.AudioProcessorWorker)
--- src/airunner/workers/audio_processor_worker.py
+++ src/airunner/workers/audio_processor_worker.py
@@ -1,3 +1,5 @@
+import threading
+
 from airunner.aihandler.stt.whisper_handler import WhisperHandler
 from airunner.enums import SignalCode
 from airunner.workers.worker import Worker
@@ -13,20 +15,33 @@ class AudioProcessorWorker(Worker):
     def __init__(self, prefix):
         super().__init__(prefix=prefix, signals = (
             (SignalCode.APPLICATION_SETTINGS_CHANGED_SIGNAL, self.update_properties),
+            (SignalCode.STT_LOAD_SIGNAL, self.on_stt_load_signal),
+            (SignalCode.STT_UNLOAD_SIGNAL, self.on_stt_unload_signal),
+            (SignalCode.AUDIO_CAPTURE_WORKER_RESPONSE_SIGNAL, self.on_stt_process_audio_signal),
         ))
-        self.stt = WhisperHandler()
+        self._stt = WhisperHandler()
 
-    def on_load_signal(self):
-        print("on load tts")
-        self.stt.load()
+    def on_stt_load_signal(self):
+        if self._stt:
+            threading.Thread(target=self._stt_load).start()
+
+    def on_stt_unload_signal(self):
+        if self._stt:
+            threading.Thread(target=self._stt_unload).start()
+
+    def _stt_load(self):
+        self._stt.load()
         self.emit_signal(SignalCode.STT_START_CAPTURE_SIGNAL)
 
-    def on_unload_signal(self):
+    def _stt_unload(self):
         self.emit_signal(SignalCode.STT_STOP_CAPTURE_SIGNAL)
-        self.stt.unload()
+        self._stt.unload()
+
+    def on_stt_process_audio_signal(self, message):
+        self.add_to_queue(message)
 
     def handle_message(self, audio_data):
-        self.stt.on_process_audio({
+        self._stt.on_process_audio({
             "message": audio_data
         })
     
