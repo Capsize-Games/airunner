6190711cde301a5e9810d458558702ff3b91e3b0
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-10-05 06:42:41 -0600

    improved collapsing of grid

--- src/airunner/windows/main/templates/main_window_ui.py
+++ src/airunner/windows/main/templates/main_window_ui.py
@@ -417,7 +417,7 @@ class Ui_MainWindow(object):
         self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
         self.canvas_widget_2 = CanvasWidget(self.canvas_widget)
         self.canvas_widget_2.setObjectName(u"canvas_widget_2")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.MinimumExpanding)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.MinimumExpanding)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.canvas_widget_2.sizePolicy().hasHeightForWidth())
