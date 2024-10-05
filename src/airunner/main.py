6efa231182b16b30b06fbebc7b76cc62101ffffc
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-10-05 08:34:58 -0600

    run alembic upgrades when main.py is run

--- src/airunner/main.py
+++ src/airunner/main.py
@@ -23,7 +23,18 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 ################################################################
 from airunner.app import App
 
+################################################################
+# Import Alembic modules to run migrations.
+################################################################
+from alembic.config import Config
+from alembic import command
+
+def run_alembic_upgrade():
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
 if __name__ == "__main__":
+    run_alembic_upgrade()
     App(
         restrict_os_access=None,
         defendatron=facehuggershield.huggingface.defendatron
