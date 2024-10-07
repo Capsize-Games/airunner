15a675e0d054498551c55ba41a85873f08023927
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-10-07 07:58:54 -0600

    improves image exports and fixes metadata export options

--- src/airunner/widgets/canvas/canvas_widget.py
+++ src/airunner/widgets/canvas/canvas_widget.py
@@ -8,7 +8,6 @@ from airunner.widgets.base_widget import BaseWidget
 from airunner.widgets.canvas.mixins.clipboard_handler_mixin import ClipboardHandlerMixin
 from airunner.widgets.canvas.mixins.image_handler_mixin import ImageHandlerMixin
 from airunner.widgets.canvas.templates.canvas_ui import Ui_canvas
-from airunner.workers.image_data_worker import ImageDataWorker
 
 
 class CanvasWidget(
@@ -39,14 +38,9 @@ class CanvasWidget(
         self.drag_pos: QPoint = Optional[None]
         self._grid_settings = {}
         self._active_grid_settings = {}
-        self.image_data_worker = None
-        self.canvas_resize_worker = None
         self.signal_handlers = {
             SignalCode.CANVAS_UPDATE_CURSOR: self.on_canvas_update_cursor_signal,
             SignalCode.CANVAS_APPLY_FILTER_SIGNAL: self.apply_filter,
-        }
-        self.worker_class_map = {
-            "image_data_worker": ImageDataWorker,
         }
 
     @property
@@ -104,6 +98,3 @@ class CanvasWidget(
             "force_draw": force_draw
         })
         self.ui.canvas_container_size = self.ui.canvas_container.viewport().size()
-
-    def save_image(self, image_path, image=None):
-        self.save_image(image_path, image)
