1659ba6ea4a3261a1ebff6493f69af3f4df98952
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-10-13 13:57:28 -0600

    fixes #894 model load balancing

--- src/airunner/workers/audio_processor_worker.py
+++ src/airunner/workers/audio_processor_worker.py
@@ -22,9 +22,13 @@ class AudioProcessorWorker(Worker):
         ))
 
     def start_worker_thread(self):
-        self._stt = WhisperHandler()
+        self._initialize_stt_handler()
         if self.application_settings.stt_enabled:
             self._stt.load()
+
+    def _initialize_stt_handler(self):
+        if self._stt is None:
+            self._stt = WhisperHandler()
 
     def on_stt_load_signal(self):
         if self._stt:
@@ -34,13 +38,22 @@ class AudioProcessorWorker(Worker):
         if self._stt:
             threading.Thread(target=self._stt_unload).start()
 
+    def unload(self):
+        self._stt_unload()
+
+    def load(self):
+        self._initialize_stt_handler()
+        self._stt_load()
+
     def _stt_load(self):
-        self._stt.load()
-        self.emit_signal(SignalCode.STT_START_CAPTURE_SIGNAL)
+        if self._stt:
+            self._stt.load()
+            self.emit_signal(SignalCode.STT_START_CAPTURE_SIGNAL)
 
     def _stt_unload(self):
         self.emit_signal(SignalCode.STT_STOP_CAPTURE_SIGNAL)
-        self._stt.unload()
+        if self._stt:
+            self._stt.unload()
 
     def on_stt_process_audio_signal(self, message):
         self.add_to_queue(message)
