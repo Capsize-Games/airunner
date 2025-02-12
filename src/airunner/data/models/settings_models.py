7ee19d3ed9ae7641862ed8a1f4bf18c06a7e22c4
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-02-12 08:51:00 -0700

    add columns to User model

--- src/airunner/data/models/settings_models.py
+++ src/airunner/data/models/settings_models.py
@@ -417,6 +417,13 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String, nullable=False)
+    zipcode = Column(String, nullable=True)
+    location_display_name = Column(String, nullable=True)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    temperature_unit = Column(String, nullable=True, default="fahrenheit")
+    wind_speed_unit = Column(String, nullable=True, default="mph")
+    precipitation_unit = Column(String, nullable=True, default="inch")
 
 
 class TargetFiles(Base):
