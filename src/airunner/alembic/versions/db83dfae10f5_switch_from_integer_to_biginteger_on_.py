caf617f1a3c8f14817a7fa806e62f0345b756495
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-06 15:26:01 -0700

    remove unused import

--- src/airunner/alembic/versions/db83dfae10f5_switch_from_integer_to_biginteger_on_.py
+++ src/airunner/alembic/versions/db83dfae10f5_switch_from_integer_to_biginteger_on_.py
@@ -10,7 +10,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.engine.reflection import Inspector
-from sqlalchemy.engine import Engine
 
 # revision identifiers, used by Alembic.
 revision: str = 'db83dfae10f5'
