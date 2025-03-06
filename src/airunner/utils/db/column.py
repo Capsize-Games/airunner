a21d69fc086fccef68cb7598c13c16e3bcda933c
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-06 15:24:00 -0700

    adds more database utilities

--- src/airunner/utils/db/column.py
+++ src/airunner/utils/db/column.py
@@ -2,7 +2,6 @@ from alembic import op
 import sqlalchemy as sa
 from typing import List
 from airunner.utils.db.engine import get_inspector
-from airunner.utils.db.engine import is_sqlite
 
 
 def get_columns(cls) -> List[str]:
@@ -13,6 +12,7 @@ def get_columns(cls) -> List[str]:
 
 def column_exists(cls, column_name):
     return column_name in get_columns(cls)
+
 
 def add_column(cls, col):
     available_columns = cls.__table__.columns.keys()
@@ -32,17 +32,11 @@ def drop_column(cls, col):
         inspector = get_inspector()
         foreign_keys = inspector.get_foreign_keys(cls.__tablename__)
 
-        if is_sqlite():
-            with op.batch_alter_table(cls.__tablename__) as batch_op:
-                for fk in foreign_keys:
-                    if col in fk['constrained_columns']:
-                        batch_op.drop_constraint(fk['name'], type_='foreignkey')
-                batch_op.drop_column(col)
-        else:
+        with op.batch_alter_table(cls.__tablename__, recreate='always') as batch_op:
             for fk in foreign_keys:
                 if col in fk['constrained_columns']:
-                    op.drop_constraint(fk['name'], cls.__tablename__, type_='foreignkey')
-            op.drop_column(cls.__tablename__, col)
+                    batch_op.drop_constraint(fk['name'], type_='foreignkey')
+            batch_op.drop_column(col)
     else:
         print(f"Column '{col}' does not exist, skipping drop.")
 
@@ -62,19 +56,88 @@ def alter_column(
         print(f"Column '{col_a}' already has the same type as '{col_b}', skipping modify.")
         return
 
-    if is_sqlite():
-        with op.batch_alter_table(cls.__tablename__) as batch_op:
-            batch_op.alter_column(
-                col_a.name,
-                existing_type=getattr(cls, col_a.name).type,
-                type_=col_b.type,
-                nullable=col_b.nullable
-            )
-    else:
-        op.alter_column(
-            cls.__tablename__, 
+    with op.batch_alter_table(cls.__tablename__, recreate='always') as batch_op:
+        batch_op.alter_column(
             col_a.name,
             existing_type=getattr(cls, col_a.name).type,
             type_=col_b.type,
             nullable=col_b.nullable
         )
+
+
+def add_column_with_fk(
+    cls,
+    column_name: str, 
+    column_type: sa.Column, 
+    fk_table: str, 
+    fk_column: str, 
+    fk_name: str
+) -> None:
+    """
+    Adds a column with a foreign key constraint to a table.
+    
+    :param table_name: Name of the table to alter.
+    :param column_name: Name of the new column.
+    :param column_type: SQLAlchemy column type for the new column.
+    :param fk_table: Name of the foreign key table.
+    :param fk_column: Name of the foreign key column.
+    :param fk_name: Name of the foreign key constraint.
+    """
+    if not column_exists(cls, column_name):
+        with op.batch_alter_table(cls.__tablename__, recreate='always') as batch_op:
+            batch_op.add_column(sa.Column(column_name, column_type))
+            batch_op.create_foreign_key(fk_name, fk_table, [column_name], [fk_column])
+
+
+def drop_column_with_fk(
+    cls, 
+    column_name: str, 
+    fk_name: str
+) -> None:
+    """
+    Drops a column with a foreign key constraint from a table.
+    
+    :param table_name: Name of the table to alter.
+    :param column_name: Name of the column to drop.
+    :param fk_name: Name of the foreign key constraint.
+    """
+    if column_exists(cls, column_name):
+        inspector = get_inspector()
+        foreign_keys = inspector.get_foreign_keys(cls.__tablename__)
+        fk_exists = any(fk['name'] == fk_name for fk in foreign_keys)
+
+        with op.batch_alter_table(cls.__tablename__, recreate='always') as batch_op:
+            if fk_exists:
+                batch_op.drop_constraint(fk_name, type_='foreignkey')
+            batch_op.drop_column(column_name)
+    else:
+        print(f"Column '{column_name}' does not exist, skipping drop.")
+
+
+def safe_alter_column(table_name: str, column_name: str, new_type: sa.types.TypeEngine, existing_type: sa.types.TypeEngine, nullable: bool):
+    try:
+        op.alter_column(table_name, column_name, type_=new_type, existing_type=existing_type, nullable=nullable)
+    except sa.exc.OperationalError:
+        pass
+
+
+def set_default_and_create_fk(table_name, column_name, ref_table_name, ref_column_name, default_value):
+    bind = op.get_bind()
+    
+    # Set default value for existing rows
+    op.execute(f"UPDATE {table_name} SET {column_name} = {default_value} WHERE {column_name} IS NULL OR {column_name} NOT IN (SELECT {ref_column_name} FROM {ref_table_name})")
+
+    # Alter column to set default value and not nullable
+    if bind.dialect.name == 'sqlite':
+        with op.batch_alter_table(table_name, recreate='always') as batch_op:
+            batch_op.alter_column(column_name,
+                existing_type=sa.INTEGER(),
+                nullable=False,
+                server_default=sa.text(str(default_value)))
+            batch_op.create_foreign_key(f'fk_{table_name}_{column_name}', ref_table_name, [column_name], [ref_column_name])
+    else:
+        op.alter_column(table_name, column_name,
+            existing_type=sa.INTEGER(),
+            nullable=False,
+            server_default=sa.text(str(default_value)))
+        op.create_foreign_key(f'fk_{table_name}_{column_name}', table_name, ref_table_name, [column_name], [ref_column_name])
--- src/airunner/utils/db/engine.py
+++ src/airunner/utils/db/engine.py
@@ -5,9 +5,6 @@ import sqlalchemy as sa
 def get_connection():
     return op.get_bind()
 
-def is_sqlite() -> bool:
-    return get_connection().dialect.name == 'sqlite'
-
 def get_inspector():
     conn = get_connection()
     return sa.inspect(conn)
