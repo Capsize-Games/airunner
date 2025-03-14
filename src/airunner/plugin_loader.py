bf95d15ad4fa7ff414334917f3dfe4e3139cb04e
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-14 09:13:32 -0600

    Move web browser to plugin examples

--- src/airunner/plugin_loader.py
+++ src/airunner/plugin_loader.py
@@ -12,17 +12,26 @@ class PluginLoader:
         if not os.path.exists(self.plugin_dir):
             os.makedirs(self.plugin_dir)
 
-        for filename in os.listdir(self.plugin_dir):
-            if filename.endswith(".py"):
-                plugin_path = os.path.join(self.plugin_dir, filename)
-                module_name = os.path.splitext(filename)[0]
-                spec = importlib.util.spec_from_file_location(module_name, plugin_path)
-                module = importlib.util.module_from_spec(spec)
-                sys.modules[module_name] = module
-                spec.loader.exec_module(module)
+        for foldername in os.listdir(self.plugin_dir):
+            plugin_path = os.path.join(self.plugin_dir, foldername, "plugin.py")
+            path = os.path.join(self.plugin_dir, foldername)
 
-                # Check if the module has a `Plugin` class
-                if hasattr(module, "Plugin"):
-                    plugins.append(module.Plugin())
+            # Append the plugin directory to sys.path temporarily
+            if os.path.isfile(plugin_path):
+                original_sys_path = sys.path.copy()
+                sys.path.append(path)
+
+                try:
+                    module_name = f"plugin_{foldername}"  # Use a unique module name
+                    spec = importlib.util.spec_from_file_location(module_name, plugin_path)
+                    module = importlib.util.module_from_spec(spec)
+                    spec.loader.exec_module(module)
+
+                    # Check if the module has a `Plugin` class
+                    if hasattr(module, "Plugin"):
+                        plugins.append(module.Plugin())
+                finally:
+                    # Restore the original sys.path to avoid conflicts
+                    sys.path = original_sys_path
 
         return plugins
