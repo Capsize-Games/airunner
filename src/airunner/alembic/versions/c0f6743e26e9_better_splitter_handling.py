1d641681c535cb106555bf1d779ed07424db2c0c
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-14 06:02:33 -0600

    Add splitter settings table

--- src/airunner/alembic/versions/c0f6743e26e9_better_splitter_handling.py
+++ src/airunner/alembic/versions/c0f6743e26e9_better_splitter_handling.py
@@ -6,28 +6,18 @@ Revises: 7fb526dc074c
 Create Date: 2025-03-11 10:02:25.695702
 
 """
-from typing import Sequence, Union
-
-from alembic import op
-import sqlalchemy as sa
-
+from typing import Union
+from airunner.utils.db import add_table, drop_table
+from airunner.data.models import SplitterSetting
 
 # revision identifiers, used by Alembic.
 revision: str = 'c0f6743e26e9'
 down_revision: Union[str, None] = '7fb526dc074c'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table('splitter_settings',
-    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
-    sa.Column('name', sa.String(), nullable=True),
-    sa.Column('splitter_settings', sa.LargeBinary(), nullable=True),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('name')
-    )
+    add_table(SplitterSetting)
 
 
 def downgrade() -> None:
-    op.drop_table('splitter_settings')
+    drop_table(SplitterSetting)
