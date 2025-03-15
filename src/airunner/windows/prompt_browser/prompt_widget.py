e4147ea0f0ad117c744f95f7888eea6420ca576b
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-15 15:34:51 -0600

    Fix prompt browser

--- src/airunner/windows/prompt_browser/prompt_widget.py
+++ src/airunner/windows/prompt_browser/prompt_widget.py
@@ -24,10 +24,7 @@ class PromptWidget(BaseWidget):
         self.ui.secondary_prompt.blockSignals(False)
         self.ui.secondary_negative_prompt.blockSignals(False)
 
-    def action_text_changed_prompt(self):
-        self.save_prompt()
-
-    def action_text_changed_negative_prompt(self):
+    def action_text_changed(self):
         self.save_prompt()
 
     def action_clicked_button_load(self):
@@ -45,4 +42,16 @@ class PromptWidget(BaseWidget):
         self.saved_prompt.negative_prompt = self.ui.negative_prompt.toPlainText()
         self.saved_prompt.secondary_prompt = self.ui.secondary_prompt.toPlainText()
         self.saved_prompt.secondary_negative_prompt = self.ui.secondary_negative_prompt.toPlainText()
-        self.update_saved_prompt(self.saved_prompt)
+        self.save()
+    
+    def save(self):
+        new_saved_prompt = SavedPrompt.objects.filter_by(
+            id=self.saved_prompt.id
+        ).first()
+        if new_saved_prompt:
+            for key in self.saved_prompt.__dict__.keys():
+                if key != "_sa_instance_state":
+                    setattr(new_saved_prompt, key, getattr(self.saved_prompt, key))
+            new_saved_prompt.save()
+        else:
+            self.saved_prompt.save()
