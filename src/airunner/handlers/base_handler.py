258f3aed6d2fcb5e1b33ab7746d9fca499a2324f
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-21 07:36:51 -0600

    rearrange code

--- src/airunner/handlers/base_handler.py
+++ src/airunner/handlers/base_handler.py
@@ -6,16 +6,13 @@ from airunner.mediator_mixin import MediatorMixin
 from airunner.utils.get_torch_device import get_torch_device
 from airunner.windows.main.settings_mixin import SettingsMixin
 
-# Get the metaclass of QObject
 QObjectMeta = type(QObject)
 
 
-# Create a metaclass that combines ABCMeta and QObject's metaclass
 class CombinedMeta(QObjectMeta, ABCMeta):
     pass
 
 
-# Use the combined metaclass
 class BaseHandler(
     MediatorMixin,
     SettingsMixin,
