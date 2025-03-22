1d64b100de08cca468d47de56731603c350b023e
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-22 07:24:26 -0600

    made worker import cleaner

--- src/airunner/widgets/lora/lora_container_widget.py
+++ src/airunner/widgets/lora/lora_container_widget.py
@@ -8,7 +8,7 @@ from airunner.utils.models.scan_path_for_items import scan_path_for_lora
 from airunner.widgets.base_widget import BaseWidget
 from airunner.widgets.lora.lora_widget import LoraWidget
 from airunner.widgets.lora.templates.lora_container_ui import Ui_lora_container
-from airunner.workers.directory_watcher import DirectoryWatcher
+from airunner.workers import DirectoryWatcher
 
 from airunner.data.models import Lora
 
--- src/airunner/windows/main/main_window.py
+++ src/airunner/windows/main/main_window.py
@@ -51,13 +51,15 @@ from airunner.settings import (
 from airunner.styles_mixin import StylesMixin
 from airunner.utils.image.convert_image_to_binary import convert_image_to_binary
 from airunner.utils.create_worker import create_worker
-from airunner.workers.audio_capture_worker import AudioCaptureWorker
-from airunner.workers.audio_processor_worker import AudioProcessorWorker
-from airunner.workers.llm_generate_worker import LLMGenerateWorker
-from airunner.workers.mask_generator_worker import MaskGeneratorWorker
-from airunner.workers.sd_worker import SDWorker
-from airunner.workers.tts_generator_worker import TTSGeneratorWorker
-from airunner.workers.tts_vocalizer_worker import TTSVocalizerWorker
+from airunner.workers import (
+    AudioCaptureWorker,
+    AudioProcessorWorker,
+    LLMGenerateWorker,
+    MaskGeneratorWorker,
+    SDWorker,
+    TTSGeneratorWorker,
+    TTSVocalizerWorker,
+)
 from airunner.utils.get_version import get_version
 from airunner.utils.widgets.save_splitter_settings import save_splitter_settings
 from airunner.utils.widgets.load_splitter_settings import load_splitter_settings
