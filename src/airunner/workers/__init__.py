6bbe5826e219f7718e4b92da36129941e219d03e
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-22 07:24:26 -0600

    made worker import cleaner

---
+++ src/airunner/workers/__init__.py
@@ -0,0 +1,28 @@
+from airunner.workers.agent_worker import AgentWorker
+from airunner.workers.audio_capture_worker import AudioCaptureWorker
+from airunner.workers.audio_processor_worker import AudioProcessorWorker
+from airunner.workers.directory_watcher import DirectoryWatcher
+from airunner.workers.llm_generate_worker import LLMGenerateWorker
+from airunner.workers.mask_generator_worker import MaskGeneratorWorker
+from airunner.workers.model_scanner_worker import ModelScannerWorker
+from airunner.workers.sd_worker import SDWorker
+from airunner.workers.tts_generator_worker import TTSGeneratorWorker
+from airunner.workers.tts_vocalizer_worker import TTSVocalizerWorker
+from airunner.workers.watch_state_worker import WatchStateWorker
+from airunner.workers.worker import Worker
+
+
+__all__ = [
+    "AgentWorker",
+    "AudioCaptureWorker",
+    "AudioProcessorWorker",
+    "DirectoryWatcher",
+    "LLMGenerateWorker",
+    "MaskGeneratorWorker",
+    "ModelScannerWorker",
+    "SDWorker",
+    "TTSGeneratorWorker",
+    "TTSVocalizerWorker",
+    "WatchStateWorker",
+    "Worker",
+]
