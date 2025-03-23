d519761ca9112688274251ff464ebed5e22d63d3
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-23 11:03:07 -0600

    decouple pyside6 from handlers

---
+++ src/airunner/handlers/stablediffusion/rect.py
@@ -0,0 +1,16 @@
+@dataclass
+class Rect:
+    x: int
+    y: int
+    width: int
+    height: int
+
+    def left(self) -> int:
+        return self.x
+    
+    def top(self) -> int:
+        return self.y
+
+    def translate(self, x: int, y: int):
+        self.x += x
+        self.y += y
