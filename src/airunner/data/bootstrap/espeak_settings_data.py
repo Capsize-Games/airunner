fea89a0d320f3b5042bf0110de78a3b66efb5b2e
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-24 14:58:05 -0600

    Moved non-application settings data to bootstrap dir

---
+++ src/airunner/data/bootstrap/espeak_settings_data.py
@@ -0,0 +1,26 @@
+ESPEAK_SETTINGS_DATA = {
+    "voices": {
+        "Male": [
+            "m1", "m2", "m3",
+        ],
+        "Female": [
+            "f1", "f2", "f3",
+        ],
+    },
+    "rate": {
+        "min": -100,
+        "max": 100,
+        "default": 0
+    },
+    "pitch": {
+        "min": -100,
+        "max": 100,
+        "default": 0
+    },
+    "volume": {
+        "min": 0,
+        "max": 100,
+        "default": 100
+    },
+    "punctuation_modes": ["none", "all", "some"],
+}
