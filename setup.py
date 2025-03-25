d02645e3603d4c74b9d53513e66b9f58fe098fa1
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-25 12:04:23 -0600

    add missing setting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="airunner",
-    version="3.3.17",
+    version="3.3.18",
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
