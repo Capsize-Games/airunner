33d7efc994290c2c1364e68f757c0b1a4f9a4da5
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-25 01:54:19 -0600

    remove unused file

--- src/airunner/windows/theme_editor.py
+++
@@ -1,9 +0,0 @@
-from airunner.windows.base_window import BaseWindow
-
-
-class ThemeEditor(BaseWindow):
-    template_name = "theme_editor"
-    window_title = "Theme Editor"
-
-    def initialize_window(self):
-        pass
