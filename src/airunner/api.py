c6a13918910323f5a04695c20460784fcd8d5635
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-27 13:52:55 -0600

    move emit_signal to api class

--- src/airunner/api.py
+++ src/airunner/api.py
@@ -2,11 +2,22 @@ from typing import Optional
 
 from airunner.app import App
 from airunner.handlers.llm.llm_request import LLMRequest
+from airunner.enums import SignalCode, LLMActionType
 
 
 class API(App):    
-    def send_llm_request(self, prompt: str, request: Optional[LLMRequest] = None):
-        self.app.main_window.send_llm_request(prompt, request)
+    def send_llm_request(self, prompt: str, llm_request: Optional[LLMRequest] = None):
+        self.app.emit_signal(
+            SignalCode.LLM_TEXT_GENERATE_REQUEST_SIGNAL,
+            {
+                "llm_request": True,
+                "request_data": {
+                    "action": LLMActionType.CHAT,
+                    "prompt": prompt,
+                    "llm_request": llm_request or LLMRequest.from_default()
+                }
+            }
+        )
     
     def quit(self):
         self.app.quit()
