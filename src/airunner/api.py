d706031c7ace112904404c50b28966479863e128
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-28 18:11:04 -0600

    adds send_tts_request method to API

--- src/airunner/api.py
+++ src/airunner/api.py
@@ -1,11 +1,17 @@
 from typing import Optional
 
 from airunner.app import App
-from airunner.handlers.llm.llm_request import LLMRequest
-from airunner.enums import SignalCode, LLMActionType
+from airunner.handlers.llm import (
+    LLMRequest, 
+    LLMResponse
+)
+from airunner.enums import (
+    SignalCode, 
+    LLMActionType
+)
 
 
-class API(App):    
+class API(App):
     def send_llm_request(self, prompt: str, llm_request: Optional[LLMRequest] = None):
         self.emit_signal(
             SignalCode.LLM_TEXT_GENERATE_REQUEST_SIGNAL,
@@ -18,3 +24,13 @@ class API(App):
                 }
             }
         )
+    
+    def send_tts_request(self, response: LLMResponse):
+        self.emit_signal(SignalCode.LLM_TEXT_STREAMED_SIGNAL, {
+            "response": LLMResponse(
+                message=response.message,
+                is_first_message=response.is_first_message,
+                is_end_of_message=response.is_last_message,
+                name=response.name,
+            )
+        })
