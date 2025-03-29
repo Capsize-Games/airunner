ad2dfb61ae165cc951a2ffcb7a0b2afce8d3c1b2
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-29 15:59:31 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="airunner",
-    version="3.4.1",
+    version="3.4.2",
     author="Capsize LLC",
     description="A Stable Diffusion GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
