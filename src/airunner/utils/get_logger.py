5c7cf62d854ef650e24353af6be15feff761b1b4
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-03-30 07:50:05 -0600

    Fix logging messages

--- src/airunner/utils/get_logger.py
+++ src/airunner/utils/get_logger.py
@@ -14,32 +14,55 @@ class Logger:
             logger.handlers.clear()
 
         handler = logging.StreamHandler()
-        # Get the name of the calling module
-        frame = inspect.currentframe().f_back.f_back
-        module_name = frame.f_globals["__name__"]
         handler.setFormatter(logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - ' + module_name + ' - %(funcName)s - %(lineno)d - %(message)s'))
+            '%(asctime)s - %(name)s - %(levelname)s - %(caller_module)s - %(caller_function)s - %(caller_lineno)d - %(message)s')
+        )
+
         logger.addHandler(handler)
 
         # Disable propagation to the root logger
         logger.propagate = False
         self.logger = logger
+        self.name = name
+    
+    def _get_caller_info(self):
+        """Get the caller module name, function name and line number."""
+        frame = inspect.currentframe().f_back.f_back  # Skip this function and the logging method
+        module_name = self.name
+        func_name = ""
+        lineno = 0
+        
+        candidate = frame.f_globals.get("__name__", "")
+        module_name = candidate
+        func_name = frame.f_code.co_name
+        lineno = frame.f_lineno
+        
+        return {
+            'caller_module': module_name, 
+            'caller_function': func_name, 
+            'caller_lineno': lineno
+        }
     
     def debug(self, message: str, *args, **kwargs):
-        self.logger.debug(message)
+        extra = self._get_caller_info()
+        self.logger.debug(message, extra=extra, *args, **kwargs)
     
     def error(self, message: str, *args, **kwargs):
         traceback.print_stack()
-        self.logger.error(message)
+        extra = self._get_caller_info()
+        self.logger.error(message, extra=extra, *args, **kwargs)
 
     def info(self, message: str, *args, **kwargs):
-        self.logger.info(message)
+        extra = self._get_caller_info()
+        self.logger.info(message, extra=extra, *args, **kwargs)
 
     def warning(self, message: str, *args, **kwargs):
-        self.logger.warning(message)
+        extra = self._get_caller_info()
+        self.logger.warning(message, extra=extra, *args, **kwargs)
 
     def critical(self, message: str, *args, **kwargs):
-        self.logger.critical(message)
+        extra = self._get_caller_info()
+        self.logger.critical(message, extra=extra, *args, **kwargs)
 
 
 def get_logger(name: str, level: int = logging.DEBUG) -> Logger:
