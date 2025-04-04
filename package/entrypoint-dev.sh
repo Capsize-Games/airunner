17e7e1da24dd5d58311771b2e7b670d783e4e466
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-04 09:30:36 -0600

    Add x display support

--- package/entrypoint-dev.sh
+++ package/entrypoint-dev.sh
@@ -5,8 +5,17 @@ set -e
 if [ ! -d "/home/appuser/.local/lib/python3.10/site-packages/airunner.egg-link" ]; then
   echo "Installing airunner package in development mode..."
   pip install --user -e .[gui,linux,dev,art,llm,llm_weather,tts]
-  pip install --user --upgrade timm==1.0.15
+  pip install --user -U timm
+fi
+
+# Check if DISPLAY is set, if not, start xvfb
+if [ -z "$DISPLAY" ]; then
+  echo "No X display found, starting a virtual one with Xvfb..."
+  Xvfb :99 -screen 0 1024x768x24 &
+  export DISPLAY=:99
+  sleep 2 # Give Xvfb time to start
 fi
 
 # Execute the command passed to docker
+echo "Executing command: $@"
 exec "$@"
