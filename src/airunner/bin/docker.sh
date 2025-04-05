6b8fac4d46f7dc3c44ceae208ecd16195d7433b2
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-05 06:17:39 -0600

    better paths

--- src/airunner/bin/docker.sh
+++ src/airunner/bin/docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "HOST_HOME=$HOME" > .env
+
 # Get user command
 if [ "$#" -eq 0 ]; then
   echo "No command provided. Starting an interactive shell..."
-  docker-compose -f ./package/docker-compose-dev.yml up -d && docker exec -it airunner_dev bash
+  docker-compose --env-file .env -f ./package/docker-compose-dev.yml up -d && docker exec -it airunner_dev bash
 else
   echo "Executing command: $@"
-    docker-compose -f ./package/docker-compose-dev.yml up -d && docker exec -it airunner_dev "$@"
+    docker-compose --env-file .env -f ./package/docker-compose-dev.yml up -d && docker exec -it airunner_dev "$@"
 fi
