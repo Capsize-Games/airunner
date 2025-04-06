6abedbe35e7468e29895b9ba04b8e94f04a0a2cc
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-05 21:05:51 -0600

    Fixes #1256: agents return responses twice

--- src/airunner/handlers/llm/agent/tools/chat_engine_tool.py
+++ src/airunner/handlers/llm/agent/tools/chat_engine_tool.py
@@ -6,52 +6,46 @@ from typing import (
 import openai
 
 from llama_index.core.tools.types import (
-    AsyncBaseTool, 
+    AsyncBaseTool,
     ToolMetadata,
-    ToolOutput
+    ToolOutput,
 )
 from llama_index.core.langchain_helpers.agents.tools import (
-    IndexToolConfig, 
-    LlamaIndexTool
+    IndexToolConfig,
+    LlamaIndexTool,
 )
 
 from airunner.handlers.llm.agent.chat_engine import ReactAgentEngine
 from airunner.handlers.llm.agent.chat_engine import (
     RefreshContextChatEngine,
-    RefreshSimpleChatEngine
+    RefreshSimpleChatEngine,
 )
 from airunner.handlers.llm.llm_request import LLMRequest
 from airunner.gui.windows.main.settings_mixin import SettingsMixin
 from airunner.utils.application.mediator_mixin import MediatorMixin
 
 
-class ChatEngineTool(
-    AsyncBaseTool,
-    SettingsMixin,
-    MediatorMixin
-):
+class ChatEngineTool(AsyncBaseTool, SettingsMixin, MediatorMixin):
     """Chat tool.
-    
+
     A tool for chatting with the LLM.
     """
-    
+
     def __init__(
         self,
         chat_engine: Union[
-            RefreshSimpleChatEngine, 
-            RefreshContextChatEngine, 
-            ReactAgentEngine
+            RefreshSimpleChatEngine, RefreshContextChatEngine, ReactAgentEngine
         ],
         metadata: ToolMetadata,
         resolve_input_errors: bool = True,
         agent=None,
+        do_handle_response: bool = True,
         *args: Any,
-        **kwargs: Any
+        **kwargs: Any,
     ):
+        self.do_handle_response: bool = do_handle_response
         self.chat_engine: Union[
-            RefreshSimpleChatEngine, 
-            RefreshContextChatEngine,
-            ReactAgentEngine
+            RefreshSimpleChatEngine, RefreshContextChatEngine, ReactAgentEngine
         ] = chat_engine
         if not chat_engine:
             raise ValueError("Chat engine must be provided.")
@@ -65,15 +59,14 @@ class ChatEngineTool(
     def from_defaults(
         cls,
         chat_engine: Union[
-            RefreshSimpleChatEngine, 
-            RefreshContextChatEngine, 
-            ReactAgentEngine
+            RefreshSimpleChatEngine, RefreshContextChatEngine, ReactAgentEngine
         ],
         name: Optional[str] = None,
         description: Optional[str] = None,
         return_direct: bool = False,
         resolve_input_errors: bool = True,
-        agent=None
+        agent=None,
+        do_handle_response: bool = True,
     ) -> "ChatEngineTool":
         name = name or "chat_engine_tool"
         description = description or """Useful for chatting with the LLM."""
@@ -85,35 +78,31 @@ class ChatEngineTool(
             chat_engine=chat_engine,
             metadata=metadata,
             resolve_input_errors=resolve_input_errors,
-            agent=agent
+            agent=agent,
+            do_handle_response=do_handle_response,
         )
-    
+
     @property
     def metadata(self) -> ToolMetadata:
         return self._metadata
-    
-    def call(
-        self, 
-        *args: Any, 
-        **kwargs: Any
-    ) -> ToolOutput:
+
+    def call(self, *args: Any, **kwargs: Any) -> ToolOutput:
         query_str = self._get_query_str(*args, **kwargs)
         llm_request = kwargs.get("llm_request", LLMRequest.from_default())
         if hasattr(self.chat_engine.llm, "llm_request"):
             self.chat_engine.llm.llm_request = llm_request
-        
+
         response = ""
 
         if not self._do_interrupt:
             do_not_display = kwargs.get("do_not_display", False)
             chat_history = kwargs.get("chat_history", [])
-            params = { 
-                "chat_history": chat_history 
-            } if len(chat_history) > 0 else {}
+            params = (
+                {"chat_history": chat_history} if len(chat_history) > 0 else {}
+            )
 
             streaming_response = self.chat_engine.stream_chat(
-                query_str, 
-                **params
+                query_str, **params
             )
 
             is_first_message = True
@@ -122,12 +111,15 @@ class ChatEngineTool(
                     if self._do_interrupt:
                         break
                     response += token
-                    if response != "Empty Response":
+                    if (
+                        response != "Empty Response"
+                        and self.do_handle_response
+                    ):
                         self.agent.handle_response(
-                            token, 
-                            is_first_message, 
+                            token,
+                            is_first_message,
                             do_not_display=do_not_display,
-                            do_tts_reply=llm_request.do_tts_reply
+                            do_tts_reply=llm_request.do_tts_reply,
                         )
                     is_first_message = False
             except openai.APIError as e:
