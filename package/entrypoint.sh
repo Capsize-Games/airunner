bce0127b5ecb8268118816aae3daf5abe3894a70
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-09 13:40:46 -0600

    fixes dev build

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 # Diagnostic information for X11 setup
 echo "===== X11 Setup Diagnostic Information ====="
@@ -19,12 +20,41 @@ else
   echo "X connection failed"
 fi
 
-# Verify that the log file is writable
-LOG_FILE="/home/appuser/.local/share/airunner/airunner.log"
-if [ ! -w "$LOG_FILE" ]; then
-  echo "Error: Log file $LOG_FILE is not writable. Please check permissions."
-  exit 1
-fi
+pip install --no-cache-dir pip setuptools wheel --upgrade
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
+# # Check if OpenVoice exists
+# if [ ! -d "OpenVoice" ]; then
+#   git clone https://github.com/myshell-ai/OpenVoice.git
+# fi
+# cd OpenVoice
+# pip install .
+# cd ..
+# rm -rf OpenVoice
+
+# # Check if MeloTTS exists
+# if [ ! -d "MeloTTS" ]; then
+#   git clone https://github.com/myshell-ai/MeloTTS.git
+# fi
+# cd MeloTTS
+# git checkout v0.1.2
+# pip install .
+# cd ..
+# rm -rf MeloTTS
+
+# echo "Downloading unidic..."
+# python3 -m unidic download
+# echo "Unidic download complete."
+# exit 0
+
+# Install python packages at runtime
+pip install --no-cache-dir -e .[nvidia,gui,linux,dev,art,llm,llm_weather,tts] \
+ -U langchain-community \
+ -U mediapipe
+pip install -U timm
+python3 -c "from accelerate.utils import write_basic_config; write_basic_config(mixed_precision='fp16')"
+python3 -c "import nltk; nltk.download('punkt')"
+rm -rf .cache/pip
 
 # Modify the script to handle interactive sessions properly
 if [ "$#" -eq 0 ]; then
