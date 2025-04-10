2826da04473138dd6436b3c4554df9f8a70d6c45
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-10 10:47:11 -0600

    fixes for linux build

--- package/pyinstaller/build.sh
+++ package/pyinstaller/build.sh
@@ -8,38 +8,37 @@ echo "============================================"
 echo "Build airunner for linux"
 echo "============================================"
 echo ""
-python3 -m PyInstaller --log-level=INFO --noconfirm /app/airunner.spec 2>&1 | tee build.log
+DEV_ENV=0 AIRUNNER_ENVIRONMENT="prod" PYTHONOPTIMIZE=0 python3 -m PyInstaller --log-level=INFO --noconfirm /app/package/pyinstaller/airunner.spec 2>&1 | tee build.log
 echo ""
 echo "============================================"
 echo "Copy timm to dist"
 echo "============================================"
 echo ""
-cp -R /home/appuser/.local/lib/python3.10/site-packages/timm ./dist/airunner/
+cp -R /home/appuser/.local/lib/python3.10/site-packages/timm /app/dist/airunner/
 echo ""
 echo "============================================"
 echo "Copy libtorch_cuda_linalg.so to dist"
 echo "============================================"
 echo ""
-cp /home/appuser/.local/lib/python3.10/site-packages/torch/lib/libtorch_cuda_linalg.so ./dist/airunner/
+cp /home/appuser/.local/lib/python3.10/site-packages/torch/lib/libtorch_cuda_linalg.so /app/dist/airunner/
 echo ""
 echo "============================================"
 echo "Copy setup.py to dist"
 echo "============================================"
 echo ""
-cp /app/setup.py ./dist/airunner/
+cp /app/setup.py /app/dist/airunner/
 echo ""
 echo "============================================"
 echo "Copy pillow to dist"
 echo "============================================"
 echo ""
-cp -R /home/appuser/.local/lib/python3.10/site-packages/pillow-10.4.0.dist-info ./dist/airunner/
+cp -R /home/appuser/.local/lib/python3.10/site-packages/pillow-10.4.0.dist-info /app/dist/airunner/
 echo ""
 echo "============================================"
 echo "Deploying airunner to itch.io"
 echo "============================================"
 echo ""
 chown -R 1000:1000 dist
-LATEST_TAG=$(grep -oP '(?<=version=).*(?=,)' /app/airunner/setup.py | tr -d '"')
+LATEST_TAG=$(grep -oP '(?<=version=).*(?=,)' /app/setup.py | tr -d '"')
 echo "Latest tag: $LATEST_TAG"
-wget https://dl.itch.ovh/butler/linux-amd64/head/butler && chmod +x butler
-./butler push ./dist/airunner capsizegames/ai-runner:ubuntu --userversion $LATEST_TAG
+/home/appuser/butler push /app/dist/airunner capsizegames/ai-runner:ubuntu --userversion $LATEST_TAG
