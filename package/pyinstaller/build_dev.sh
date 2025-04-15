bee2754e30da92349b115dd6b840af91ca7b9c88
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-15 08:24:05 -0600

    Adds package files for dev builds

---
+++ package/pyinstaller/build_dev.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+cd /app
+echo "============================================"
+echo "Build airunner for linux"
+echo "============================================"
+echo ""
+echo "Running PyInstaller"
+DEV_ENV=0 AIRUNNER_ENVIRONMENT="prod" PYTHONOPTIMIZE=0 python3 -m PyInstaller --log-level=INFO --noconfirm /app/package/pyinstaller/airunner.spec 2>&1 | tee build.log
+echo ""
+echo "============================================"
+echo "Copy setup.py to dist"
+echo "============================================"
+echo ""
+cp /app/setup.py /app/dist/airunner/_internal/airunner/
+echo ""
+echo "============================================"
+echo "Deploying airunner to itch.io"
+echo "============================================"
+echo ""
+chown -R 1000:1000 dist
+
+# CHECK IF DEV ENVIRONMENT
+if [ "$DEV_ENV" -eq 1 ]; then
+  echo "Dev environment detected. Skipping deployment."
+  exit 0
+fi
+LATEST_TAG=$(grep -oP '(?<=version=).*(?=,)' /app/setup.py | tr -d '"')
+echo "Latest tag: $LATEST_TAG"
+/home/appuser/butler/butler push /app/dist/airunner capsizegames/ai-runner:ubuntu --userversion $LATEST_TAG
