784f63c3b635f4f334df1e09be07276db2c016b7
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-20 09:32:29 -0600

    simplify imports

--- src/airunner/gui/widgets/nodegraph/node_graph_widget.py
+++ src/airunner/gui/widgets/nodegraph/node_graph_widget.py
@@ -13,70 +13,31 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtCore import Qt
 
-from airunner.gui.widgets.nodegraph.nodes.agent_action_node import (
-    AgentActionNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.base_workflow_node import (
-    BaseWorkflowNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.image_generation_node import (
-    ImageGenerationNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.prompt_node import (
-    PromptNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.textbox_node import (
-    TextboxNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.random_number_node import (
-    RandomNumberNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.number_node import (
-    NumberNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.float_node import (
-    FloatNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.boolean_node import (
-    BooleanNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.llm_request_node import (
-    LLMRequestNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.image_request_node import (
-    ImageRequestNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.run_llm_node import (
-    RunLLMNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.image_display_node import (
-    ImageDisplayNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.start_node import StartNode
-from airunner.gui.widgets.nodegraph.nodes.branch_node import (
-    BranchNode,
-)
 
-# Import the new loop nodes
-from airunner.gui.widgets.nodegraph.nodes.for_each_loop_node import (
+from airunner.gui.widgets.nodegraph.nodes import (
+    AgentActionNode,
+    BaseWorkflowNode,
+    ImageGenerationNode,
+    PromptNode,
+    TextboxNode,
+    RandomNumberNode,
+    NumberNode,
+    FloatNode,
+    BooleanNode,
+    LLMRequestNode,
+    ImageRequestNode,
+    RunLLMNode,
+    ImageDisplayNode,
+    StartNode,
+    BranchNode,
     ForEachLoopNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.for_loop_node import (
     ForLoopNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.while_loop_node import (
     WhileLoopNode,
-)
-from airunner.gui.widgets.nodegraph.nodes.reverse_for_each_loop_node import (
     ReverseForEachLoopNode,
 )
 
-from airunner.gui.widgets.nodegraph.add_port_dialog import (
-    AddPortDialog,
-)
-from airunner.gui.widgets.nodegraph.custom_node_graph import (
-    CustomNodeGraph,
-)
+from airunner.gui.widgets.nodegraph.add_port_dialog import AddPortDialog
+from airunner.gui.widgets.nodegraph.custom_node_graph import CustomNodeGraph
 
 # Import database models and managers
 from airunner.data.models.workflow import Workflow
