1aa62f075dab4ab7b57279b915317435f1b3b5b8
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-23 05:19:29 -0600

    add generate image node to graph

---
+++ src/airunner/gui/widgets/nodegraph/nodes/generate_image_node.py
@@ -0,0 +1,49 @@
+from typing import Dict
+
+from airunner.gui.widgets.nodegraph.nodes.base_workflow_node import (
+    BaseWorkflowNode,
+)
+from airunner.handlers.stablediffusion.stable_diffusion_model_manager import (
+    StableDiffusionModelManager,
+)
+from airunner.handlers.stablediffusion.image_request import ImageRequest
+from airunner.handlers.stablediffusion.image_response import ImageResponse
+from airunner.enums import SignalCode
+
+
+class GenerateImageNode(BaseWorkflowNode):
+    """
+    Node for generating images using a specified Stable Diffusion model
+    """
+
+    NODE_NAME = "Generate Image"
+
+    def __init__(self):
+        self.signal_handlers = {}
+        super().__init__()
+        self.image_request_port = self.add_input(
+            "image_request", display_name=True
+        )
+        self.image_response_port = self.add_output(
+            "image_response", display_name=True
+        )
+
+    def _generate_image(self, image_request: ImageRequest):
+        self.emit_signal(
+            SignalCode.DO_GENERATE_SIGNAL,
+            {
+                "image_request": image_request,
+            },
+        )
+
+    def _on_image_generated(self, data: Dict):
+        print("ON IMAGE GENERATED CALLBACK HERE", data)
+
+    def execute(self, input_data: Dict):
+        image_request = input_data.get("image_request", None)
+        print("*" * 100)
+        print("image_request", image_request)
+        if image_request is not None:
+            image_request.callback = self._on_image_generated
+            self._generate_image(image_request)
+        return {}
