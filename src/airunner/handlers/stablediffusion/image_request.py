dc66a1b2a0a1e2d28de80107e36cafb16ed35e36
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-29 08:14:55 -0600

    fixes microconditioning for sdxl

--- src/airunner/handlers/stablediffusion/image_request.py
+++ src/airunner/handlers/stablediffusion/image_request.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Optional
 from dataclasses import dataclass, asdict
 
 from airunner.settings import AIRUNNER_DEFAULT_SCHEDULER
-from airunner.enums import ImagePreset
+from airunner.enums import ImagePreset, QualityEffects
 
 
 @dataclass
@@ -26,16 +26,18 @@ class ImageRequest:
     strength: int = 0.5
     n_samples: int = 1
     clip_skip: int = 0
-    crops_coord_top_left: Dict = None
+    crops_coords_top_left: Dict = None
     original_size: Dict = None
     target_size: Dict = None
     negative_original_size: Dict = None
     negative_target_size: Dict = None
+    negative_crops_coords_top_left: Dict = None
     lora_scale: float = 1.0
     width: int = 512
     height: int = 512
     callback: Optional[callable] = None
     image_preset: ImagePreset = ImagePreset.NONE
+    quality_effects: QualityEffects = QualityEffects.STANDARD
     node_id: Optional[str] = None
 
     additional_prompts: Optional[List[Dict[str, str]]] = None
