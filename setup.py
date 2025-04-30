93ac637e33c004dcd0725ca7a3c739fc0036ada0
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-04-30 07:16:04 -0600

    bump version

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ for k, v in extras_require.items():
 
 setup(
     name="airunner",
-    version="4.3.2",
+    version="4.3.3",
     author="Capsize LLC",
     description="Run local opensource AI models (Stable Diffusion, LLMs, TTS, STT, chatbots) in a lightweight Python GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
