8e861bedd3d1130c5a8315928588cc3591b4b686
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-05-01 09:16:27 -0600

    better compel abstraction

---
+++ src/airunner/handlers/stablediffusion/controlnet_request.py
@@ -0,0 +1,13 @@
+from PIL.Image import Image
+from typing import Dict, List, Optional
+from dataclasses import dataclass, asdict
+
+from airunner.settings import AIRUNNER_DEFAULT_SCHEDULER
+from airunner.enums import ImagePreset, QualityEffects
+from airunner.handlers.stablediffusion.image_request import ImageRequest
+
+
+@dataclass
+class ControlnetRequest:
+    image: Optional[Image] = None
+    strength: float = 0.5
