886fa4aada0229a6c551456afd63929a85873b8a
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-05-01 18:44:46 -0600

    fixes image to image settings strength slider

--- src/airunner/data/models/image_to_image_settings.py
+++ src/airunner/data/models/image_to_image_settings.py
@@ -4,9 +4,10 @@ from airunner.data.models.base import BaseModel
 
 
 class ImageToImageSettings(BaseModel):
-    __tablename__ = 'image_to_image_settings'
+    __tablename__ = "image_to_image_settings"
     id = Column(Integer, primary_key=True, autoincrement=True)
     image = Column(LargeBinary, nullable=True)
     enabled = Column(Boolean, default=False)
     use_grid_image_as_input = Column(Boolean, default=False)
     lock_input_image = Column(Boolean, default=False)
+    strength = Column(Integer, default=0)
