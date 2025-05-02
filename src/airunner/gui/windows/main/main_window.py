2e320e7632334962bf461410e45deb99a8120b65
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2025-05-02 08:07:39 -0600

    improve slot / signal names

--- src/airunner/gui/windows/main/main_window.py
+++ src/airunner/gui/windows/main/main_window.py
@@ -1075,7 +1075,6 @@ class MainWindow(
         )
 
     def on_toggle_tts(self, data: Dict = None, val=None):
-        print("ON TOGGLE TTS", data, val)
         if val is None:
             val = data.get(
                 "enabled", not self.application_settings.tts_enabled
