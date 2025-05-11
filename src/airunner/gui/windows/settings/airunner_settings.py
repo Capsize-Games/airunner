cdb49efd26e4c4e99671c0923b0bdbba659643ff
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-05-11 04:40:23 -0600

    use api to fire update stylesheet signal

--- src/airunner/gui/windows/settings/airunner_settings.py
+++ src/airunner/gui/windows/settings/airunner_settings.py
@@ -364,11 +364,11 @@ class SettingsWindow(BaseWindow):
         elif name == "dark_mode":
             checked = item.checkState() == Qt.CheckState.Checked
             self.update_application_settings("dark_mode_enabled", checked)
-            self.emit_signal(SignalCode.REFRESH_STYLESHEET_SIGNAL)
+            self.api.refresh_stylesheet(dark_mode=checked)
         elif name == "override_system_theme":
             checked = item.checkState() == Qt.CheckState.Checked
             self.update_application_settings("override_system_theme", checked)
-            self.emit_signal(SignalCode.REFRESH_STYLESHEET_SIGNAL)
+            self.api.refresh_stylesheet(override_system_theme=checked)
         elif name == "check_for_updates":
             checked = item.checkState() == Qt.CheckState.Checked
             self.update_application_settings("latest_version_check", checked)
