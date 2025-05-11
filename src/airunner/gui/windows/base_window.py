a0b749d90ed464de14fff430e237d79430189ca2
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-05-11 09:41:08 -0600

    prevent multiple settings windows

--- src/airunner/gui/windows/base_window.py
+++ src/airunner/gui/windows/base_window.py
@@ -1,11 +1,6 @@
-import os
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QDialog
 from airunner.utils.application.mediator_mixin import MediatorMixin
-from airunner.settings import (
-    AIRUNNER_DARK_THEME_NAME,
-    AIRUNNER_LIGHT_THEME_NAME,
-)
 from airunner.gui.styles.styles_mixin import StylesMixin
 from airunner.gui.windows.main.ai_model_mixin import AIModelMixin
 from airunner.gui.windows.main.settings_mixin import SettingsMixin
