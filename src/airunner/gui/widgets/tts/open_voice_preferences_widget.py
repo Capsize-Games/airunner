eb0de47f0b6813e13cbafc9c4d94ea3712370950
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-05-15 17:26:30 -0600

    Openvoice loading improvements
    - Add mp3 path to openvoice settings
    - Remove obsolete openvoice enable flag

--- src/airunner/gui/widgets/tts/open_voice_preferences_widget.py
+++ src/airunner/gui/widgets/tts/open_voice_preferences_widget.py
@@ -1,4 +1,6 @@
 from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QFileDialog
+
 from airunner.gui.widgets.base_widget import BaseWidget
 from airunner.gui.widgets.tts.templates.open_voice_preferences_ui import (
     Ui_open_voice_preferences,
@@ -16,6 +18,35 @@ class OpenVoicePreferencesWidget(BaseWidget):
         if not self._item:
             self._item = OpenVoiceSettings.objects.create()
         super().__init__(*args, **kwargs)
+        self.ui.voice_sample_path.setText(self._item.reference_speaker_path)
+
+    @Slot()
+    def on_browse_voice_sample_path_button_clicked(self):
+        """Open a file dialog to select a voice sample path."""
+        file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            self.tr("Select Voice Sample Path"),
+            "",
+            self.tr("Audio Files (*.wav *.mp3 *.ogg);;All Files (*)"),
+        )
+        if file_path:
+            self.ui.voice_sample_path.setText(file_path)
+            open_voice_settings = OpenVoiceSettings.objects.get(self._id)
+            if not open_voice_settings:
+                return
+            OpenVoiceSettings.objects.update(
+                self._id, reference_speaker_path=file_path
+            )
+
+    @Slot(str)
+    def on_voice_sample_path_textChanged(self, txt: str):
+        open_voice_settings = OpenVoiceSettings.objects.get(self._id)
+        if not open_voice_settings:
+            return
+        if open_voice_settings.reference_speaker_path != txt:
+            OpenVoiceSettings.objects.update(
+                self._id, reference_speaker_path=txt
+            )
 
     def initialize_ui(self):
         # initialize comboboxes
