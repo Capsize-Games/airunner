58c74b1d920f0883e7b6a2c4cd39b52dedd29ddc
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-05-15 20:08:15 -0600

    add openvoice bootstrap data files

---
+++ src/airunner/data/bootstrap/openvoice_bootstrap_data.py
@@ -0,0 +1,63 @@
+OPENVOICE_FILES = {
+    "bert-base-multilingual-uncased": {
+        "files": [
+            "config.json",
+            "model.safetensors",
+            "vocab.txt",
+            "tokenizer_config.json",
+            "special_tokens_map.json",
+        ]
+    },
+    "bert-base-uncased": {
+        "files": [
+            "config.json",
+            "model.safetensors",
+            "vocab.txt",
+            "tokenizer_config.json",
+            "special_tokens_map.json",
+        ]
+    },
+    "dbmdz/bert-base-french-europeana-cased": {
+        "files": [
+            "config.json",
+            "pytorch_model.bin",
+            "tokenizer_config.json",
+            "vocab.txt",
+            "special_tokens_map.json",
+        ]
+    },
+    "dccuchile/bert-base-spanish-wwm-uncased": {
+        "files": [
+            "config.json",
+            "model.safetensors",
+            "vocab.txt",
+            "tokenizer_config.json",
+            "special_tokens_map.json",
+        ]
+    },
+    "kykim/bert-kor-base": {
+        "files": [
+            "config.json",
+            "model.safetensors",
+            "vocab.txt",
+            "tokenizer_config.json",
+            "special_tokens_map.json",
+        ]
+    },
+    "myshell-ai/MelTTS-English-v3": {
+        "files": [
+            "checkpoint.pth",
+            "config.json",
+            "hps.json",
+        ]
+    },
+    "tohoku-nlp/bert-base-japanese-v3": {
+        "files": [
+            "config.json",
+            "model.safetensors",
+            "vocab.txt",
+            "tokenizer_config.json",
+            "special_tokens_map.json",
+        ]
+    },
+}
