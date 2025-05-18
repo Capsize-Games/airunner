7e7ca5b8466ff17c2cd1fbfcc0afdbafcee3f553
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-05-18 13:32:29 -0600

    reduces openvoice code

--- src/airunner/vendor/melo/text/language_base.py
+++ src/airunner/vendor/melo/text/language_base.py
@@ -14,6 +14,21 @@ class LanguageBase:
     CMU_DICT_PATH = os.path.join(current_file_path, "cmudict.rep")
     CACHE_PATH = os.path.join(current_file_path, "cmudict_cache.pickle")
 
+    @staticmethod
+    def distribute_phone(n_phone, n_word):
+        phones_per_word = [0] * n_word
+        for task in range(n_phone):
+            min_tasks = min(phones_per_word)
+            min_index = phones_per_word.index(min_tasks)
+            phones_per_word[min_index] += 1
+        return phones_per_word
+
+    @staticmethod
+    def unicode_normalize(text):
+        import unicodedata
+
+        return unicodedata.normalize("NFKC", text)
+
     def __init__(self):
         self._tokenizer = None
         self._bert_model = None
