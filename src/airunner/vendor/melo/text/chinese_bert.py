da9e697744f7158dba0610dca780fe3d5995a83a
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-05-18 11:59:03 -0600

    moves to classes

--- src/airunner/vendor/melo/text/chinese_bert.py
+++ src/airunner/vendor/melo/text/chinese_bert.py
@@ -2,19 +2,19 @@ import torch
 import sys
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 
-
-# model_id = 'hfl/chinese-roberta-wwm-ext-large'
-local_path = "./bert/chinese-roberta-wwm-ext-large"
-
+from airunner.api import API
 
 tokenizers = {}
 models = {}
 
-def get_bert_feature(text, word2ph, device=None, model_id='hfl/chinese-roberta-wwm-ext-large'):
+
+def get_bert_feature(text, word2ph, device=None, model_id=None):
+    model_id = model_id or "hfl/chinese-roberta-wwm-ext-large"
+    model_id = API().paths[model_id]
     if model_id not in models:
-        models[model_id] = AutoModelForMaskedLM.from_pretrained(
-            model_id
-        ).to(device)
+        models[model_id] = AutoModelForMaskedLM.from_pretrained(model_id).to(
+            device
+        )
         tokenizers[model_id] = AutoTokenizer.from_pretrained(model_id)
     model = models[model_id]
     tokenizer = tokenizers[model_id]
@@ -34,8 +34,6 @@ def get_bert_feature(text, word2ph, device=None, model_id='hfl/chinese-roberta-w
             inputs[i] = inputs[i].to(device)
         res = model(**inputs, output_hidden_states=True)
         res = torch.cat(res["hidden_states"][-3:-2], -1)[0].cpu()
-    # import pdb; pdb.set_trace()
-    # assert len(word2ph) == len(text) + 2
     word2phone = word2ph
     phone_level_feature = []
     for i in range(len(word2phone)):
@@ -44,64 +42,3 @@ def get_bert_feature(text, word2ph, device=None, model_id='hfl/chinese-roberta-w
 
     phone_level_feature = torch.cat(phone_level_feature, dim=0)
     return phone_level_feature.T
-
-
-if __name__ == "__main__":
-    import torch
-
-    word_level_feature = torch.rand(38, 1024)  # 12个词,每个词1024维特征
-    word2phone = [
-        1,
-        2,
-        1,
-        2,
-        2,
-        1,
-        2,
-        2,
-        1,
-        2,
-        2,
-        1,
-        2,
-        2,
-        2,
-        2,
-        2,
-        1,
-        1,
-        2,
-        2,
-        1,
-        2,
-        2,
-        2,
-        2,
-        1,
-        2,
-        2,
-        2,
-        2,
-        2,
-        1,
-        2,
-        2,
-        2,
-        2,
-        1,
-    ]
-
-    # 计算总帧数
-    total_frames = sum(word2phone)
-    print(word_level_feature.shape)
-    print(word2phone)
-    phone_level_feature = []
-    for i in range(len(word2phone)):
-        print(word_level_feature[i].shape)
-
-        # 对每个词重复word2phone[i]次
-        repeat_feature = word_level_feature[i].repeat(word2phone[i], 1)
-        phone_level_feature.append(repeat_feature)
-
-    phone_level_feature = torch.cat(phone_level_feature, dim=0)
-    print(phone_level_feature.shape)  # torch.Size([36, 1024])
---
+++ src/airunner/vendor/melo/text/language_base.py
@@ -0,0 +1,84 @@
+import torch
+import os
+from g2p_en import G2p
+
+from airunner.api import API
+
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+
+class LanguageBase:
+    model_path = ""
+    model_path_bert = ""
+
+    def __init__(self):
+        self._tokenizer = None
+        self.eng_dict = self.get_dict()
+        self.current_file_path = os.path.dirname(__file__)
+        self.cmu_dict_path = os.path.join(
+            self.current_file_path, "cmudict.rep"
+        )
+        self.cache_path = os.path.join(
+            self.current_file_path, "cmudict_cache.pickle"
+        )
+
+    @property
+    def bert_model_path(self) -> str:
+        model_id = model_id or self.model_path_bert
+        model_id = API().paths[model_id]
+
+    @property
+    def bert_model(self):
+        if not self._bert_model:
+            self._bert_model = AutoModelForMaskedLM.from_pretrained(
+                self.bert_model_path
+            ).to(self.device)
+        return self._bert_model
+
+    @property
+    def bert_tokenizer(self):
+        if not self._bert_tokenizer:
+            self._bert_tokenizer = AutoTokenizer.from_pretrained(
+                self.bert_model_path
+            )
+
+    @property
+    def call(self):
+        if self._g2p is None:
+            self._g2p = G2p()
+        return self._g2p
+
+    @property
+    def model_id(self):
+        return API().paths[self.model_path]
+
+    @property
+    def tokenizer(self):
+        if self._tokenizer is None:
+            self._tokenizer = AutoTokenizer.from_pretrained(self.model_id)
+        return self._tokenizer
+
+    @tokenizer.setter
+    def tokenizer(self, value):
+        self._tokenizer = value
+
+    def call(self, text, pad_start_end=True, tokenized=None):
+        raise NotImplementedError(
+            "g2p method must be implemented in the subclass"
+        )
+
+    def get_bert_feature(self, text, word2ph):
+        with torch.no_grad():
+            inputs = self.bert_tokenizer(text, return_tensors="pt")
+            for i in inputs:
+                inputs[i] = inputs[i].to(device)
+            res = self.bert_model(**inputs, output_hidden_states=True)
+            res = torch.cat(res["hidden_states"][-3:-2], -1)[0].cpu()
+        assert inputs["input_ids"].shape[-1] == len(word2ph)
+        word2phone = word2ph
+        phone_level_feature = []
+        for i in range(len(word2phone)):
+            repeat_feature = res[i].repeat(word2phone[i], 1)
+            phone_level_feature.append(repeat_feature)
+        phone_level_feature = torch.cat(phone_level_feature, dim=0)
+        return phone_level_feature.T
