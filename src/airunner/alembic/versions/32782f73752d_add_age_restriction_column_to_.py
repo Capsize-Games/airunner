4d1ccdd6400c90921ca2b09cf2103670151533bc
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-05-22 00:13:20 -0600

    store useragreement in db to prevent showing multiple time during setup

---
+++ src/airunner/alembic/versions/32782f73752d_add_age_restriction_column_to_.py
@@ -0,0 +1,28 @@
+"""add age restriction column to application settings'
+
+
+Revision ID: 32782f73752d
+Revises: 1ca474ca1d8f
+Create Date: 2025-05-21 23:58:42.539763
+
+"""
+
+from typing import Sequence, Union
+
+from airunner.data.models.application_settings import ApplicationSettings
+from airunner.utils.db import add_column, drop_column
+
+
+# revision identifiers, used by Alembic.
+revision: str = "32782f73752d"
+down_revision: Union[str, None] = "1ca474ca1d8f"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    add_column(ApplicationSettings, "age_agreement_checked")
+
+
+def downgrade() -> None:
+    drop_column(ApplicationSettings, "age_agreement_checked")
