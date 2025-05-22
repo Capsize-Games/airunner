a7b97b386cc059876728dfdc3d8e2fa420f1489a
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-05-22 00:13:20 -0600

    store useragreement in db to prevent showing multiple time during setup

--- src/airunner/gui/windows/setup_wizard/model_setup/stable_diffusion_setup/stable_diffusion_license.py
+++ src/airunner/gui/windows/setup_wizard/model_setup/stable_diffusion_setup/stable_diffusion_license.py
@@ -1,7 +1,17 @@
-from airunner.gui.windows.setup_wizard.user_agreement.agreement_page import AgreementPage
-from airunner.gui.windows.setup_wizard.model_setup.stable_diffusion_setup.templates.stable_diffusion_license_ui import Ui_stable_diffusion_license
+from PySide6.QtCore import Slot
+from airunner.gui.windows.setup_wizard.user_agreement.agreement_page import (
+    AgreementPage,
+)
+from airunner.gui.windows.setup_wizard.model_setup.stable_diffusion_setup.templates.stable_diffusion_license_ui import (
+    Ui_stable_diffusion_license,
+)
 
 
 class StableDiffusionLicense(AgreementPage):
     class_name_ = Ui_stable_diffusion_license
     setting_key = "stable_diffusion_agreement_checked"
+
+    @Slot(bool)
+    def agreement_clicked(self, val):
+        super().agreement_clicked(val)
+        self.update_application_settings(self.setting_key, val)
