97737d84ce415f66a2f3705584480bb4947644ed
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-05-22 00:13:20 -0600

    store useragreement in db to prevent showing multiple time during setup

--- src/airunner/gui/windows/setup_wizard/user_agreement/user_agreement.py
+++ src/airunner/gui/windows/setup_wizard/user_agreement/user_agreement.py
@@ -1,8 +1,17 @@
-from airunner.gui.windows.setup_wizard.user_agreement.agreement_page import AgreementPage
-from airunner.gui.windows.setup_wizard.user_agreement.templates.user_agreement_ui import Ui_user_agreement
+from PySide6.QtCore import Slot
+from airunner.gui.windows.setup_wizard.user_agreement.agreement_page import (
+    AgreementPage,
+)
+from airunner.gui.windows.setup_wizard.user_agreement.templates.user_agreement_ui import (
+    Ui_user_agreement,
+)
 
 
 class UserAgreement(AgreementPage):
     class_name_ = Ui_user_agreement
     setting_key = "user_agreement_checked"
 
+    @Slot(bool)
+    def agreement_clicked(self, val):
+        super().agreement_clicked(val)
+        self.update_application_settings(self.setting_key, val)
