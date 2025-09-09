2fd46109845f9022f6978c9aa9460c8a9721d64d
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-09-09 13:05:32 -0600

    reduce version

--- setup.py
+++ setup.py
@@ -163,7 +163,7 @@ for k, v in extras_require.items():
 
 setup(
     name="airunner",
-    version="5.0.4",
+    version="5.0.0",
     author="Capsize LLC",
     description="Run local opensource AI models (Stable Diffusion, LLMs, TTS, STT, chatbots) in a lightweight Python GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
