c5671cffa8d9b3fc1a548f9d97ec6400873ae591
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-12-05 09:26:01 -0700

    Adds image-to-image support for Z-Image

--- src/airunner/components/art/managers/stablediffusion/mixins/sd_generation_preparation_mixin.py
+++ src/airunner/components/art/managers/stablediffusion/mixins/sd_generation_preparation_mixin.py
@@ -113,7 +113,9 @@ class SDGenerationPreparationMixin:
             data.update({"width": width, "height": height})
 
         if self.is_img2img:
-            image = self.img2img_image
+            # Use image from image_request if available (passed from GUI),
+            # otherwise fall back to img2img_image property
+            image = self.image_request.image if self.image_request.image is not None else self.img2img_image
             if (
                 data["num_inference_steps"]
                 < AIRUNNER_MIN_NUM_INFERENCE_STEPS_IMG2IMG
