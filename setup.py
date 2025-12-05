22e6fefd3e47b92bf559426f2efab496178bc627
Joe Curlee (w4ffl35) <25737761+w4ffl35@users.noreply.github.com>
2025-12-05 08:44:56 -0700

    bump version

--- setup.py
+++ setup.py
@@ -187,7 +187,7 @@ for k, v in extras_require.items():
 
 setup(
     name="airunner",
-    version="5.3.1",
+    version="5.3.2",
     author="Capsize LLC",
     description="Run local opensource AI models (Stable Diffusion, LLMs, TTS, STT, chatbots) in a lightweight Python GUI",
     long_description=open("README.md", "r", encoding="utf-8").read(),
